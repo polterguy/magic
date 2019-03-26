564013e16a6ee0130af0880f21307422b18355bf
Thomas Hansen <thomas@gaiasoul.com>
2019-03-26 10:39:25 +0200

    Allowing for plugins to configure services

---
+++ contracts/magic.contracts.common/IConfigureServices.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace magic.contracts.common
+{
+    public interface IConfigureServices
+    {
+        void Configure(IServiceCollection services);
+    }
+}
