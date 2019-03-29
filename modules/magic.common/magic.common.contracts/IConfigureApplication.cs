fb4e7af63d06b38208125ec5c0fa1e85b024e105
Thomas Hansen <thomas@gaiasoul.com>
2019-03-29 14:42:16 +0200

    Allowing for injecting IApplicationBuilder startup initialization logic

---
+++ modules/magic.common/magic.common.contracts/IConfigureApplication.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using Microsoft.AspNetCore.Builder;
+
+namespace magic.common.contracts
+{
+    public interface IConfigureApplication
+    {
+        void Configure(IApplicationBuilder app);
+    }
+}
