819089b2c6fa473f41ec4b277276c4026f6f9156
thomas@ainiro.io <thomas@gaiasoul.com>
2019-04-02 16:24:25 +0300

    Slightly more intelligent error handling and getting logging to work

--- magic.backend/Program.cs
+++ magic.backend/Program.cs
@@ -3,16 +3,33 @@
  * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
  */
 
+using log4net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Xml;
+using System.Reflection;
 
 namespace magic.backend
 {
     public class Program
     {
+        static readonly ILog _logger = LogManager.GetLogger(typeof(Program));
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var log4netConfig = new XmlDocument();
+            log4netConfig.Load(File.OpenRead("log4net.config"));
+
+            var repo = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+            log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
+
+            _logger.Info("Starting application");
+
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
