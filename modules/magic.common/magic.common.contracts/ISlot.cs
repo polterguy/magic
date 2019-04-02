c085c5e8f5b54d2805fd03092445bd7dc1c533e2
thomas@ainiro.io <thomas@gaiasoul.com>
2019-04-02 12:24:36 +0300

    Basic signals and slots implementation - Needs to be tested!

---
+++ modules/magic.common/magic.common.contracts/ISlot.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using Newtonsoft.Json.Linq;
+
+namespace magic.common.contracts
+{
+    public interface ISlot
+    {
+        void Signal(JObject input);
+    }
+}
