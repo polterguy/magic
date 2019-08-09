f398ece909a7602aedbe4493d4021f4b5e309f18
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-09 17:58:26 +0300

    Hyperlambda parser implementation

---
+++ lib/magic.hyperlambda/Parser.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using magic.node;
+
+namespace magic.hyperlambda
+{
+    public sealed class Parser
+    {
+        public IEnumerable<Node> Parse(string content)
+        {
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+            {
+                foreach (var idx in Parse(stream))
+                {
+                    yield return idx;
+                }
+            }
+        }
+
+        public IEnumerable<Node> Parse(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                while(!reader.EndOfStream)
+                {
+                    var buffer = GetNode(reader);
+                    if (buffer != null)
+                        yield return buffer;
+                }
+            }
+        }
+
+        #region [ -- Private helper methods -- ]
+
+        Node GetNode(StreamReader reader)
+        {
+            var name = GetToken(reader, out bool colonName);
+            if (name == null)
+                return null;
+
+            var value = GetToken(reader, out bool colonValue);
+            if (!colonName && value == "")
+                value = null;
+
+            if (colonValue)
+            {
+                // Current value is actually a type.
+                var type = value;
+                // TODO: Read value!
+                return new Node(name.ToString(), value);
+            }
+            else
+            {
+                return new Node(name.ToString(), value);
+            }
+        }
+
+        string GetToken(StreamReader reader, out bool colon)
+        {
+            var builder = new StringBuilder();
+            colon = false;
+            while(!reader.EndOfStream)
+            {
+                var idx = (char)reader.Read();
+                switch(idx)
+                {
+                    case ':':
+                        colon = true;
+                        return builder.ToString();
+                    case '\r':
+                        if (reader.Read() != '\n')
+                            throw new ApplicationException("Syntax error in Hyperlambda file. Wrong CR/LF sequence found.");
+                        return builder.ToString();
+                    case '\n':
+                        return builder.ToString();
+                    default:
+                        builder.Append(idx);
+                        break;
+                }
+            }
+            return builder.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ lib/magic.tests/tests/HyperlambdaTests.cs
@@ -0,0 +1,57 @@
+/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System.Linq;
+using Xunit;
+using magic.hyperlambda;
+
+namespace magic.tests.tests
+{
+    public class HyperlambdaTests
+    {
+        [Fact]
+        public void ParseString_01()
+        {
+            var result = new Parser().Parse("foo");
+            Assert.Single(result);
+            Assert.Equal("foo", result.First().Name);
+            Assert.Null(result.First().Value);
+            Assert.Empty(result.First().Children);
+        }
+
+        [Fact]
+        public void ParseString_02()
+        {
+            var result = new Parser().Parse("foo:");
+            Assert.Single(result);
+            Assert.Equal("foo", result.First().Name);
+            Assert.Equal("", result.First().Value);
+            Assert.Empty(result.First().Children);
+        }
+
+        [Fact]
+        public void ParseString_03()
+        {
+            var result = new Parser().Parse("foo:bar");
+            Assert.Single(result);
+            Assert.Equal("foo", result.First().Name);
+            Assert.Equal("bar", result.First().Value);
+            Assert.Empty(result.First().Children);
+        }
+
+        [Fact]
+        public void ParseString_04()
+        {
+            var result = new Parser().Parse("foo1:bar1\r\nfoo2:bar2");
+            Assert.Equal(2, result.Count());
+            Assert.Equal("foo1", result.First().Name);
+            Assert.Equal("bar1", result.First().Value);
+            Assert.Empty(result.First().Children);
+            Assert.Equal("foo2", result.Skip(1).First().Name);
+            Assert.Equal("bar2", result.Skip(1).First().Value);
+            Assert.Empty(result.Skip(1).First().Children);
+        }
+    }
+}
