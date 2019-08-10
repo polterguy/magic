66c0991034a944c09ccc6ca4899ddf4fbced61df
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-10 14:30:38 +0300

    Hyperlambda parser done
    But no support for comments

--- lib/magic.hyperlambda/Parser.cs
+++ lib/magic.hyperlambda/Parser.cs
@@ -6,68 +6,113 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Linq;
 using System.Globalization;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using magic.node;
 
 namespace magic.hyperlambda
 {
     public sealed class Parser
     {
-        public IEnumerable<Node> Parse(string content)
+        Node _root = new Node();
+
+        public Parser(string hyperlambda, Encoding encoding = null)
         {
-            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+            using (var reader = new StreamReader(new MemoryStream((encoding ?? Encoding.UTF8).GetBytes(hyperlambda))))
             {
-                foreach (var idx in Parse(stream))
-                {
-                    yield return idx;
-                }
+                Parse(reader);
             }
         }
 
-        public IEnumerable<Node> Parse(Stream stream)
+        public Node Lambda()
         {
-            using (var reader = new StreamReader(stream))
-            {
-                while(!reader.EndOfStream)
-                {
-                    var buffer = GetNode(reader);
-                    if (buffer != null)
-                        yield return buffer;
-                }
-            }
+            return _root;
         }
 
         #region [ -- Private helper methods -- ]
 
-        Node GetNode(StreamReader reader)
+        void Parse(StreamReader _reader)
         {
-            Node retVal = null;
+            var currentParent = _root;
+            var tokenizer = new Tokenizer(_reader);
+            var en = tokenizer.GetTokens().GetEnumerator();
 
-            var name = GetToken(reader, out bool colonName);
-            if (name == null)
-                return null;
+            Node idxNode = null;
+            string previous = null;
+            int level = 0;
 
-            var value = GetToken(reader, out bool colonValue);
-            if (!colonName && value == "")
+            while (en.MoveNext())
             {
-                retVal = new Node(name, null);
-            }
-            else if (colonValue)
-            {
-                // Current value is actually a type.
-                var actualValue = GetToken(reader, out colonValue);
-                var convertedValue = ConvertValue(actualValue, value);
-                retVal = new Node(name.ToString(), convertedValue);
-            }
-            else
-            {
-                retVal = new Node(name, value);
-            }
+                var token = en.Current;
+                switch (token)
+                {
+                    case ":":
+                        if (idxNode == null)
+                        {
+                            idxNode = new Node();
+                            currentParent.Add(idxNode);
+                        }
 
-            // TODO: Check for child nodes here.
-            return retVal;
+                        if (idxNode.Value == null)
+                            idxNode.Value = "";
+                        previous = token;
+                        break;
+
+                    case "\n":
+                        idxNode = null; // Making sure we create a new node on next iteration.
+                        previous = token;
+                        break;
+
+                    default:
+
+                        // Checking if token is a scope declaration.
+                        if (idxNode == null &&
+                            token.StartsWith(" ", StringComparison.CurrentCulture) &&
+                            token.Distinct().Count() == 1)
+                        {
+                            // We have a scope declaration.
+                            int newLevel = token.Length / 2;
+                            if (newLevel > level + 1)
+                            {
+                                // Syntax error in Hyperlambda, too many consecutive SP characters.
+                                throw new ApplicationException("Too many spaces found in Hyperlambda content");
+                            }
+                            if (newLevel == level + 1)
+                            {
+                                // Children collection opens up.
+                                currentParent = currentParent.Children.Last();
+                                level = newLevel;
+                            }
+                            else
+                            {
+                                // Propagating upwards in ancestor hierarchy.
+                                while (level-- > newLevel)
+                                {
+                                    currentParent = currentParent.Parent;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (previous == "\n")
+                                currentParent = _root; // Special case for no spaces, and previous was CR.
+
+                            if (idxNode == null)
+                            {
+                                idxNode = new Node();
+                                currentParent.Add(idxNode);
+                            }
+                            if (idxNode.Name == "")
+                                idxNode.Name = token;
+                            else if (idxNode.Value == null || "".Equals(idxNode.Value))
+                                idxNode.Value = token;
+                            else
+                                idxNode.Value = ConvertValue(token, idxNode.Get<string>());
+                        }
+                        previous = token;
+                        break;
+                }
+            }
         }
 
         object ConvertValue(string value, string type)
@@ -86,37 +131,9 @@ namespace magic.hyperlambda
                     return DateTime.ParseExact(value, "yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture);
                 case "guid":
                     return new Guid(value);
-                case "regex":
-                    return new Regex(value);
                 default:
                     throw new ApplicationException($"Unknown type declaration found in Hyperlambda '{type}'");
             }
-        }
-
-        string GetToken(StreamReader reader, out bool colon)
-        {
-            var builder = new StringBuilder();
-            colon = false;
-            while(!reader.EndOfStream)
-            {
-                var idx = (char)reader.Read();
-                switch(idx)
-                {
-                    case ':':
-                        colon = true;
-                        return builder.ToString();
-                    case '\r':
-                        if (reader.Read() != '\n')
-                            throw new ApplicationException("Syntax error in Hyperlambda file. Wrong CR/LF sequence found.");
-                        return builder.ToString();
-                    case '\n':
-                        return builder.ToString();
-                    default:
-                        builder.Append(idx);
-                        break;
-                }
-            }
-            return builder.ToString();
         }
 
         #endregion
--- lib/magic.tests/tests/HyperlambdaTests.cs
+++ lib/magic.tests/tests/HyperlambdaTests.cs
@@ -14,7 +14,14 @@ namespace magic.tests.tests
         [Fact]
         public void ParseString_01()
         {
-            var result = new Parser().Parse("foo");
+            var result = new Parser("").Lambda().Children.ToList();
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void ParseString_02()
+        {
+            var result = new Parser("foo").Lambda().Children.ToList();
             Assert.Single(result);
             Assert.Equal("foo", result.First().Name);
             Assert.Null(result.First().Value);
@@ -22,9 +29,9 @@ namespace magic.tests.tests
         }
 
         [Fact]
-        public void ParseString_02()
+        public void ParseString_03()
         {
-            var result = new Parser().Parse("foo:");
+            var result = new Parser("foo:").Lambda().Children.ToList();
             Assert.Single(result);
             Assert.Equal("foo", result.First().Name);
             Assert.Equal("", result.First().Value);
@@ -32,9 +39,9 @@ namespace magic.tests.tests
         }
 
         [Fact]
-        public void ParseString_03()
+        public void ParseString_04()
         {
-            var result = new Parser().Parse("foo:bar");
+            var result = new Parser("foo:bar").Lambda().Children.ToList();
             Assert.Single(result);
             Assert.Equal("foo", result.First().Name);
             Assert.Equal("bar", result.First().Value);
@@ -42,38 +49,68 @@ namespace magic.tests.tests
         }
 
         [Fact]
-        public void ParseString_04()
-        {
-            var result = new Parser().Parse("foo1:bar1\r\nfoo2:bar2");
-            Assert.Equal(2, result.Count());
-            Assert.Equal("foo1", result.First().Name);
-            Assert.Equal("bar1", result.First().Value);
-            Assert.Empty(result.First().Children);
-            Assert.Equal("foo2", result.Skip(1).First().Name);
-            Assert.Equal("bar2", result.Skip(1).First().Value);
-            Assert.Empty(result.Skip(1).First().Children);
-        }
-
-        [Fact]
         public void ParseString_05()
         {
-            var result = new Parser().Parse("foo1:bar1\nfoo2:bar2");
-            Assert.Equal(2, result.Count());
-            Assert.Equal("foo1", result.First().Name);
-            Assert.Equal("bar1", result.First().Value);
+            var result = new Parser("foo:int:5").Lambda().Children.ToList();
+            Assert.Single(result);
+            Assert.Equal("foo", result.First().Name);
+            Assert.Equal(5, result.First().Value);
             Assert.Empty(result.First().Children);
-            Assert.Equal("foo2", result.Skip(1).First().Name);
-            Assert.Equal("bar2", result.Skip(1).First().Value);
-            Assert.Empty(result.Skip(1).First().Children);
         }
 
         [Fact]
         public void ParseString_06()
         {
-            var result = new Parser().Parse("foo:int:5");
+            var result = new Parser("foo1:bar1\r\nfoo2:bar2").Lambda().Children.ToList();
+            Assert.Equal(2, result.Count());
+            Assert.Equal("foo1", result.First().Name);
+            Assert.Equal("bar1", result.First().Value);
+            Assert.Empty(result.First().Children);
+            Assert.Equal("foo2", result.Skip(1).First().Name);
+            Assert.Equal("bar2", result.Skip(1).First().Value);
+            Assert.Empty(result.Skip(1).First().Children);
+        }
+
+        [Fact]
+        public void ParseString_07()
+        {
+            var result = new Parser("foo\r\n  bar").Lambda().Children.ToList();
             Assert.Single(result);
             Assert.Equal("foo", result.First().Name);
-            Assert.Equal(5, result.First().Value);
+            Assert.Null(result.First().Value);
+            Assert.Single(result.First().Children);
+            Assert.Equal("bar", result.First().Children.First().Name);
+            Assert.Null(result.First().Children.First().Value);
+        }
+
+        [Fact]
+        public void ParseString_08()
+        {
+            var result = new Parser("foo1\r\n  bar\r\nfoo2").Lambda().Children.ToList();
+            Assert.Equal(2, result.Count);
+            Assert.Equal("foo1", result.First().Name);
+            Assert.Null(result.First().Value);
+            Assert.Single(result.First().Children);
+            Assert.Equal("bar", result.First().Children.First().Name);
+            Assert.Null(result.First().Children.First().Value);
+            Assert.Equal("foo2", result.Skip(1).First().Name);
+            Assert.Null(result.Skip(1).First().Value);
+            Assert.Empty(result.Skip(1).First().Children);
+        }
+
+        [Fact]
+        public void ParseString_09()
+        {
+            var result = new Parser("foo1\r\n  bar1\r\n    bar2\r\n  bar3").Lambda().Children.ToList();
+            Assert.Equal(1, result.Count);
+            Assert.Equal("foo1", result.First().Name);
+            Assert.Null(result.First().Value);
+            Assert.Equal(2, result.First().Children.Count());
+            Assert.Equal("bar1", result.First().Children.First().Name);
+            Assert.Null(result.First().Children.First().Value);
+            Assert.Equal("bar2", result.First().Children.First().Children.First().Name);
+            Assert.Null(result.First().Children.First().Children.First().Value);
+            Assert.Equal("bar3", result.First().Children.Skip(1).First().Name);
         }
     }
 }
