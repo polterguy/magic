2f4d5b25eec16aea443404009082b95b47c1dcf6
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-10 20:07:04 +0300

    Eval basic implementation

--- lib/magic.lambda/Eval.cs
+++ lib/magic.lambda/Eval.cs
@@ -3,9 +3,29 @@
  * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
  */
 
+using System;
+using magic.node;
+using magic.signals.contracts;
+
 namespace magic.lambda
 {
-    public class Eval
+    [Slot(Name = "eval")]
+    public class Eval : ISlot
     {
+        IServiceProvider _services;
+
+        public Eval(IServiceProvider services)
+        {
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+        }
+
+        public void Signal(Node input)
+        {
+            var signaler = _services.GetService(typeof(ISignaler)) as ISignaler;
+            foreach (var idx in input.Children)
+            {
+                signaler.Signal(idx.Name, idx);
+            }
+        }
     }
 }
