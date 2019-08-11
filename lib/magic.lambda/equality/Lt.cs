7a9dca7667fa8d5eba8c1fe5850679705ef9ae3f
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-11 13:05:29 +0300

    Equality comparers

---
+++ lib/magic.lambda/equality/Lt.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda.equality
+{
+    [Slot(Name = "lt")]
+    public class Lt : ISlot
+    {
+        readonly ISignaler _signaler;
+
+        public Lt(IServiceProvider services)
+        {
+            _signaler = services.GetService(typeof(ISignaler)) as ISignaler;
+        }
+
+        public void Signal(Node input)
+        {
+            if (input.Children.Count() != 2)
+                throw new ApplicationException("Operator [lt] requires exactly two values to compare to each other");
+
+            _signaler.Signal("eval", input);
+
+            var lhs = input.Children.First().Value;
+            var rhs = input.Children.Skip(1).First().Value;
+
+            if (lhs == null && rhs == null)
+                input.Value = false;
+            else if (lhs != null && rhs == null)
+                input.Value = false;
+            else if (lhs == null && rhs != null)
+                input.Value = true;
+            else
+                input.Value = ((IComparable)lhs).CompareTo(rhs) == -1;
+        }
+    }
+}
---
+++ lib/magic.lambda/equality/Not.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda.equality
+{
+    [Slot(Name = "not")]
+    public class Not : ISlot
+    {
+        readonly ISignaler _signaler;
+
+        public Not(IServiceProvider services)
+        {
+            _signaler = services.GetService(typeof(ISignaler)) as ISignaler;
+        }
+
+        public void Signal(Node input)
+        {
+            if (input.Children.Count() != 1)
+                throw new ApplicationException("Operator [not] requires exactly one child");
+
+            _signaler.Signal("eval", input);
+
+            input.Value = !input.Children.First().Get<bool>();
+        }
+    }
+}
