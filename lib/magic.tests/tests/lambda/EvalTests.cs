cbc045824094e6f74f7e24b677d09a44ac4a081b
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-11 19:54:33 +0300

    Double handled slot/signal test

--- lib/magic.tests/tests/lambda/EvalTests.cs
+++ lib/magic.tests/tests/lambda/EvalTests.cs
@@ -22,11 +22,36 @@ namespace magic.tests.tests.lambda
             }
         }
 
+        [Slot(Name = "foo_double")]
+        public class FooDoubleSlot1 : ISlot
+        {
+            public void Signal(Node input)
+            {
+                input.Value = input.Get<int>() + 1;
+            }
+        }
+
+        [Slot(Name = "foo_double")]
+        public class FooDoubleSlot2 : ISlot
+        {
+            public void Signal(Node input)
+            {
+                input.Value = input.Get<int>() + 1;
+            }
+        }
+
         [Fact]
         public void InvokeCustomSignal()
         {
             var lambda = Common.Evaluate(@"foo");
             Assert.Equal("OK", lambda.Children.First().Value);
+        }
+
+        [Fact]
+        public void InvokeCustomSignalDoubleHandled()
+        {
+            var lambda = Common.Evaluate(@"foo_double:int:0");
+            Assert.Equal(2, lambda.Children.First().Value);
         }
 
         [Fact]
