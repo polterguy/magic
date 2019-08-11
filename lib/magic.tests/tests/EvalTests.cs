1bba7fb1727ae422bad6a3a52578e0f9dc11d6f0
Thomas Hansen <thomas@gaiasoul.com>
2019-08-11 13:05:29 +0300

    Equality comparers

--- lib/magic.tests/tests/EvalTests.cs
+++ lib/magic.tests/tests/EvalTests.cs
@@ -139,6 +139,97 @@ namespace magic.tests.tests
             Assert.Equal(2, Foo2Slot.ExecutionCount);
         }
 
+        [Fact]
+        public void Eq_01()
+        {
+            var lambda = Evaluate(".foo1:OK\neq\n   value:x:../*/.foo1\n   .:OK");
+            Assert.Equal(true, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Eq_02()
+        {
+            var lambda = Evaluate(".foo1:not OK\neq\n   value:x:../*/.foo1\n   .:OK");
+            Assert.Equal(false, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Not_01()
+        {
+            var lambda = Evaluate(".foo1:OK\nnot\n   eq\n      value:x:../*/.foo1\n      .:OK");
+            Assert.Equal(false, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Mt_01()
+        {
+            var lambda = Evaluate(".foo1:A\nmt\n   value:x:../*/.foo1\n   .:B");
+            Assert.Equal(false, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Mt_02()
+        {
+            var lambda = Evaluate(".foo1:B\nmt\n   value:x:../*/.foo1\n   .:A");
+            Assert.Equal(true, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Lt_01()
+        {
+            var lambda = Evaluate(".foo1:A\nlt\n   value:x:../*/.foo1\n   .:B");
+            Assert.Equal(true, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Lt_02()
+        {
+            var lambda = Evaluate(".foo1:B\nlt\n   value:x:../*/.foo1\n   .:A");
+            Assert.Equal(false, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Lte_01()
+        {
+            var lambda = Evaluate(".foo1:A\nlte\n   value:x:../*/.foo1\n   .:A");
+            Assert.Equal(true, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Lte_02()
+        {
+            var lambda = Evaluate(".foo1:A\nlte\n   value:x:../*/.foo1\n   .:B");
+            Assert.Equal(true, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Lte_03()
+        {
+            var lambda = Evaluate(".foo1:B\nlte\n   value:x:../*/.foo1\n   .:A");
+            Assert.Equal(false, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Mte_01()
+        {
+            var lambda = Evaluate(".foo1:A\nmte\n   value:x:../*/.foo1\n   .:A");
+            Assert.Equal(true, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Mte_02()
+        {
+            var lambda = Evaluate(".foo1:A\nmte\n   value:x:../*/.foo1\n   .:B");
+            Assert.Equal(false, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void Mte_03()
+        {
+            var lambda = Evaluate(".foo1:B\nmte\n   value:x:../*/.foo1\n   .:A");
+            Assert.Equal(true, lambda.Children.Skip(1).First().Value);
+        }
+
         #region [ -- Private helper methods -- ]
 
         Node Evaluate(string hl)
