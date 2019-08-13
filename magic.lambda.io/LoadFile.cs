117165e9e19e79c9686a57d9962e90dafcd640cb
Thomas Hansen <thomas@gaiasoul.com>
2019-08-13 18:10:32 +0300

    [load-file]

---
+++ magic.lambda.io/LoadFile.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda
+{
+    [Slot(Name = "load-file")]
+    public class LoadFile : ISlot, IMeta
+    {
+        public void Signal(Node input)
+        {
+            string filename;
+            if (input.Value is string strValue)
+            {
+                filename = strValue;
+            }
+            else
+            {
+                var nodes = input.Get<Expression>().Evaluate(new Node[] { input });
+                if (nodes.Count() != 1)
+                    throw new ApplicationException("Expression for [load-file] yielded less or more than one result");
+                filename = nodes.First().Get<string>();
+            }
+            input.Value = File.ReadAllText(filename);
+        }
+
+        public IEnumerable<Node> GetArguments()
+        {
+            yield return new Node(":", "*");
+        }
+    }
+}
