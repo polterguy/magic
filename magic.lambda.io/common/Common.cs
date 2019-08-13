b7be5b51e5c4cedead98810bf1b530a8b53b68c2
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-13 18:18:10 +0300

    save file

---
+++ magic.lambda.io/common/Common.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using magic.node;
+
+namespace magic.lambda.common
+{
+    public static class Common
+    {
+        public static string GetFilename(Node input)
+        {
+            string filename;
+            if (input.Value is string strValue)
+            {
+                filename = strValue;
+            }
+            else
+            {
+                var nodes = input.Get<Expression>().Evaluate(new Node[] { input });
+                if (nodes.Count() != 1)
+                    throw new ApplicationException("Expression for [load-file] yielded less or more than one result");
+                filename = nodes.First().Get<string>();
+            }
+            return filename;
+        }
+    }
+}
