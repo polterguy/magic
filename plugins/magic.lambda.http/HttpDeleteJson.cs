3a3579a85c5074a07a20ba1752b472e4778985a7
Thomas Hansen <thomas@gaiasoul.com>
2019-08-16 07:12:13 +0300

    Put + Delete

---
+++ plugins/magic.lambda.http/HttpDeleteJson.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using magic.node;
+using magic.http.contracts;
+using magic.signals.contracts;
+
+namespace magic.lambda.http
+{
+    [Slot(Name = "http.delete.json")]
+    public class HttpDeleteJson : ISlot, IMeta
+    {
+        readonly ISignaler _signaler;
+        readonly IHttpClient _httpClient;
+
+        public HttpDeleteJson(ISignaler signaler, IHttpClient httpClient)
+        {
+            _signaler = signaler ?? throw new ArgumentNullException(nameof(signaler));
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public void Signal(Node input)
+        {
+            if (input.Children.Count() > 1 || input.Children.Any((x) => x.Name != "token"))
+                throw new ApplicationException("[http.delete.json] can only handle one [token] child node");
+
+            var url = input.Get<string>();
+            var token = input.Children.FirstOrDefault((x) => x.Name == "token")?.Get<string>();
+
+            // Notice, to sanity check the result we still want to roundtrip through a JToken result.
+            input.Value = _httpClient.DeleteAsync<JToken>(url, token).Result.ToString();
+        }
+
+        public IEnumerable<Node> GetArguments()
+        {
+            yield return new Node(":", "*");
+            yield return new Node("token", 1);
+        }
+    }
+}
---
+++ plugins/magic.lambda.http/HttpPutJson.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using magic.node;
+using magic.http.contracts;
+using magic.signals.contracts;
+
+namespace magic.lambda.http
+{
+    [Slot(Name = "http.put.json")]
+    public class HttpPutJson : ISlot, IMeta
+    {
+        readonly ISignaler _signaler;
+        readonly IHttpClient _httpClient;
+
+        public HttpPutJson(ISignaler signaler, IHttpClient httpClient)
+        {
+            _signaler = signaler ?? throw new ArgumentNullException(nameof(signaler));
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public void Signal(Node input)
+        {
+            if (input.Children.Count() > 2 || input.Children.Any((x) => x.Name != "token" && x.Name != "payload"))
+                throw new ApplicationException("[http.put.json] can only handle one [token] and one [payload] child node");
+
+            var url = input.Get<string>();
+            var token = input.Children.FirstOrDefault((x) => x.Name == "token")?.Get<string>();
+            var payload = input.Children.FirstOrDefault((x) => x.Name == "payload")?.Get<string>();
+
+            // Notice, to sanity check the result we still want to roundtrip through a JToken result.
+            input.Value = _httpClient.PutAsync<string, JToken>(url, payload, token).Result.ToString();
+        }
+
+        public IEnumerable<Node> GetArguments()
+        {
+            yield return new Node(":", "*");
+            yield return new Node("token", 1);
+            yield return new Node("payload", 1);
+        }
+    }
+}
