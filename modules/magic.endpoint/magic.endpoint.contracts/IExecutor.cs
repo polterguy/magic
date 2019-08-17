f739bb54b212b8a992ff3ee1a67171bbf3d70c11
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-17 14:54:45 +0300

    Continuing on generic HTTP REST controller

--- modules/magic.endpoint/magic.endpoint.contracts/IExecutor.cs
+++ modules/magic.endpoint/magic.endpoint.contracts/IExecutor.cs
@@ -3,10 +3,19 @@
  * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
  */
 
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
 namespace magic.endpoint.contracts
 {
 	public interface IExecutor
 	{
-		object Execute();
-	}
+        object ExecuteGet(string url, Dictionary<string, string> args);
+
+        object ExecuteDelete(string url, Dictionary<string, string> args);
+
+        object ExecutePost(string url, JContainer payload);
+
+        object ExecutePut(string url, JContainer payload);
+    }
 }
