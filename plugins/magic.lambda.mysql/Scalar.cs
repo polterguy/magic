4d4a2f78318b405d03e16c2a9700303d6f6fe369
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-17 10:39:23 +0300

    MySQL slots

---
+++ plugins/magic.lambda.mysql/Scalar.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Collections.Generic;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda.mysql
+{
+    [Slot(Name = "mysql.scalar")]
+    public class Scalar : ISlot, IMeta
+    {
+        readonly ISignaler _signaler;
+        readonly ConnectionStack _connections;
+
+        public Scalar(ISignaler signaler, ConnectionStack connections)
+        {
+            _signaler = signaler ?? throw new ArgumentNullException(nameof(signaler));
+            _connections = connections ?? throw new ArgumentNullException(nameof(connections));
+        }
+
+        public void Signal(Node input)
+        {
+            Executor.Execute(input, _connections, (cmd) =>
+            {
+                input.Value = cmd.ExecuteScalar();
+            });
+        }
+
+        public IEnumerable<Node> GetArguments()
+        {
+            yield return new Node(":", "*");
+        }
+    }
+}
---
+++ plugins/magic.lambda.mysql/Select.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Collections.Generic;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda.mysql
+{
+    [Slot(Name = "mysql.select")]
+    public class Select : ISlot, IMeta
+    {
+        readonly ISignaler _signaler;
+        readonly ConnectionStack _connections;
+
+        public Select(ISignaler signaler, ConnectionStack connections)
+        {
+            _signaler = signaler ?? throw new ArgumentNullException(nameof(signaler));
+            _connections = connections ?? throw new ArgumentNullException(nameof(connections));
+        }
+
+        public void Signal(Node input)
+        {
+            Executor.Execute(input, _connections, (cmd) =>
+            {
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var rowNode = new Node();
+                        for (var idxCol = 0; idxCol < reader.FieldCount; idxCol++)
+                        {
+                            var colNode = new Node(reader.GetName(idxCol), reader[idxCol]);
+                            rowNode.Add(colNode);
+                        }
+                        input.Add(rowNode);
+                    }
+                }
+            });
+        }
+
+        public IEnumerable<Node> GetArguments()
+        {
+            yield return new Node(":", "*");
+        }
+    }
+}
