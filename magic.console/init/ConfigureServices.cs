63353272021bdd3565221a5b5661b3176d52e638
Thomas Hansen <thomas@gaiasoul.com>
2019-08-18 14:50:16 +0300

    ScopedResolver on magic.console

--- magic.console/init/ConfigureServices.cs
+++ magic.console/init/ConfigureServices.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using magic.common.contracts;
 
-namespace magic.backend.init
+namespace magic.console.init
 {
     public class ConfigureServices : IConfigureServices
     {
