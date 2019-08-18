91c54cd1450b29e0ec14c72bb14a87fff36cff25
Thomas Hansen <thomas@gaiasoul.com>
2019-08-18 17:33:03 +0300

    Removed the ability to have multiple slots for same signal
    And throwing an exception in fact if there is. This simplifies code, and also makes it much faster when invoking signals.

--- magic.tests/lambda/EvalTests.cs
+++ magic.tests/lambda/EvalTests.cs
@@ -6,52 +6,16 @@
 using System;
 using System.Linq;
 using Xunit;
-using magic.node;
-using magic.signals.contracts;
 
 namespace magic.tests.lambda
 {
     public class EvalTests
     {
-        [Slot(Name = "foo")]
-        public class FooSlot : ISlot
-        {
-            public void Signal(Node input)
-            {
-                input.Value = "OK";
-            }
-        }
-
-        [Slot(Name = "foo_double")]
-        public class FooDoubleSlot1 : ISlot
-        {
-            public void Signal(Node input)
-            {
-                input.Value = input.Get<int>() + 1;
-            }
-        }
-
-        [Slot(Name = "foo_double")]
-        public class FooDoubleSlot2 : ISlot
-        {
-            public void Signal(Node input)
-            {
-                input.Value = input.Get<int>() + 1;
-            }
-        }
-
         [Fact]
         public void InvokeCustomSignal()
         {
             var lambda = Common.Evaluate(@"foo");
             Assert.Equal("OK", lambda.Children.First().Value);
-        }
-
-        [Fact]
-        public void InvokeCustomSignalDoubleHandled()
-        {
-            var lambda = Common.Evaluate(@"foo_double:int:0");
-            Assert.Equal(2, lambda.Children.First().Value);
         }
 
         [Fact]
