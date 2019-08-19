4c4e610d637f661d947d5d3252b5d7cfd0e07b65
Thomas Hansen <thomas@gaiasoul.com>
2019-08-19 11:30:56 +0300

    Better SQL generating

---
+++ plugins/magic.lambda.mysql/Select.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using magic.node;
+using magic.signals.contracts;
+using ut = magic.lambda.utilities;
+using magic.lambda.mysql.utilities;
+
+namespace magic.lambda.mysql
+{
+    [Slot(Name = "mysql.select")]
+    public class Select : ISlot, IMeta
+    {
+        readonly ut.Stack<MySqlConnection> _connections;
+
+        public Select(ut.Stack<MySqlConnection> connections)
+        {
+            _connections = connections ?? throw new ArgumentNullException(nameof(connections));
+        }
+
+        public void Signal(Node input)
+        {
+            // Creating parametrized SQL node.
+            var execute = Executor.CreateSelect(input);
+
+            // Checking if caller is only interested in SQL text.
+            var onlySql = input.Children.FirstOrDefault((x) => x.Name == "only-sql")?.Get<bool>() == true;
+
+            // Massaging node to get parameters correctly.
+            input.Value = execute.Value;
+            input.Clear();
+            input.AddRange(execute.Children.ToList());
+            if (onlySql)
+                return;
+
+            // Executing SQL.
+            Executor.Execute(execute, _connections, (cmd) =>
+            {
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var rowNode = new Node();
+                        for (var idxCol = 0; idxCol < reader.FieldCount; idxCol++)
+                        {
+                            var colNode = new Node(reader.GetName(idxCol), reader[idxCol]);
+                            rowNode.Add(colNode);
+                        }
+                        input.Add(rowNode);
+                    }
+                }
+            });
+        }
+
+        public IEnumerable<Node> GetArguments()
+        {
+            yield return new Node(":", "*");
+        }
+    }
+}
--- plugins/magic.lambda.mysql/utilities/Executor.cs
+++ plugins/magic.lambda.mysql/utilities/Executor.cs
@@ -4,6 +4,7 @@
  */
 
 using System;
+using System.Linq;
 using MySql.Data.MySqlClient;
 using magic.node;
 using magic.lambda.utilities;
@@ -28,5 +29,119 @@ namespace magic.lambda.mysql.utilities
                 functor(cmd);
             }
         }
+
+        public static Node CreateSelect(Node root)
+        {
+            // Dynamically building SQL according to input nodes.
+            var result = new Node("sql");
+            var sql = "select ";
+            var columns = root.Children.FirstOrDefault((x) => x.Name == "columns");
+            if (columns != null)
+            {
+                var first = true;
+                foreach (var idxCol in columns.Children)
+                {
+                    if (first)
+                        first = false;
+                    else
+                        sql += ",";
+                    sql += "`" + idxCol.Name.Replace("`", "``") + "`";
+                }
+            }
+            else
+            {
+                sql += "*";
+            }
+
+            sql += " from " + "`" + root.Children.First((x) => x.Name == "table").Get<string>().Replace("`", "``") + "`";
+
+            var where = root.Children.FirstOrDefault((x) => x.Name == "where");
+            if (where != null)
+            {
+                if (where.Children.Count() != 1)
+                    throw new ArgumentException("Too many children nodes to SQL [where] parameters");
+
+                sql += " where " + CreateWhereSql(where, result);
+            }
+
+            var limit = root.Children.FirstOrDefault((x) => x.Name == "limit");
+            if (limit != null)
+                sql += " limit " + limit.Get<long>();
+
+            var offset = root.Children.FirstOrDefault((x) => x.Name == "offset");
+            if (offset != null)
+                sql += " offset " + offset.Get<long>();
+
+            result.Value = sql;
+            return result;
+        }
+
+        #region [ -- Private helper methods -- ]
+
+        static string CreateWhereSql(Node where, Node root)
+        {
+            var result = "";
+            int levelNo = 0;
+            foreach (var idx in where.Children)
+            {
+                switch(idx.Name)
+                {
+                    case "and":
+                        if (result != "")
+                            result += " and ";
+                        result += BuildWhereLevel(idx, "and", root, ref levelNo);
+                        break;
+
+                    case "or":
+                        if (result != "")
+                            result += " or ";
+                        result += BuildWhereLevel(idx, "or", root, ref levelNo);
+                        break;
+
+                    default:
+                        throw new ArgumentException($"I don't understand '{idx.Name}' as a where clause while trying to build SQL");
+                }
+            }
+
+            // Stripping outer paranthesis.
+            if (result.Length > 0)
+                result = result.Substring(1, result.Length - 2);
+
+            return result;
+        }
+
+        static string BuildWhereLevel(Node level, string oper, Node root, ref int levelNo)
+        {
+            var result = "(";
+            bool first = true;
+            foreach (var idxCol in level.Children)
+            {
+                if (first)
+                    first = false;
+                else
+                    result += " " + oper + " ";
+
+                switch (idxCol.Name)
+                {
+                    case "and":
+                        result += BuildWhereLevel(idxCol, "and", root, ref levelNo);
+                        break;
+                    case "or":
+                        result += BuildWhereLevel(idxCol, "or", root, ref levelNo);
+                        break;
+                    default:
+                        var argName = "@" + levelNo;
+                        result += "`" + idxCol.Name.Replace("`", "``") + "` = " + argName;
+                        root.Add(new Node(argName, idxCol.Value));
+                        ++levelNo;
+                        break;
+                }
+            }
+            result = result.TrimEnd();
+            result += ")";
+            return result;
+        }
+
+        #endregion
     }
 }
