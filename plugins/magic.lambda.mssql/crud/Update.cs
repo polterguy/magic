0b16c350124b6aab3dd6480c95ef7698f6635425
Thomas Hansen <thomas@gaiasoul.com>
2019-08-21 07:27:43 +0300

    ms sql CRUD operations

---
+++ plugins/magic.lambda.mssql/crud/Update.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using magic.node;
+using magic.signals.contracts;
+using ut = magic.lambda.utilities;
+using magic.lambda.mssql.utilities;
+using magic.lambda.mssql.crud.utilities;
+
+namespace magic.lambda.mysql.crud
+{
+    [Slot(Name = "mssql.update")]
+    public class Update : ISlot, IMeta
+    {
+        readonly ut.Stack<SqlConnection> _connections;
+        readonly ISignaler _signaler;
+
+        public Update(ut.Stack<SqlConnection> connections, ISignaler signaler)
+        {
+            _connections = connections ?? throw new ArgumentNullException(nameof(connections));
+            _signaler = signaler ?? throw new ArgumentNullException(nameof(signaler));
+        }
+
+        public void Signal(Node input)
+        {
+            var builder = new SqlUpdateBuilder(input, _signaler);
+            var sqlNode = builder.Build();
+
+            // Checking if this is a "build only" invocation.
+            if (builder.IsGenerateOnly)
+            {
+                input.Value = sqlNode.Value;
+                input.Clear();
+                input.AddRange(sqlNode.Children.ToList());
+                return;
+            }
+
+            // Executing SQL, now parametrized.
+            Executor.Execute(sqlNode, _connections, _signaler, (cmd) =>
+            {
+                input.Value = cmd.ExecuteNonQuery();
+                input.Clear();
+            });
+        }
+
+        public IEnumerable<Node> GetArguments()
+        {
+            yield return new Node(":", "*");
+            yield return new Node("connection", "*");
+            yield return new Node("table", "*");
+            yield return new Node("where");
+            yield return new Node("values");
+        }
+    }
+}
---
+++ plugins/magic.lambda.mssql/crud/utilities/SqlDeleteBuilder.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System.Text;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda.mssql.crud.utilities
+{
+    public class SqlDeleteBuilder : SqlBuilder
+    {
+        public SqlDeleteBuilder(Node node, ISignaler signaler)
+            : base(node, signaler)
+        { }
+
+        public override Node Build()
+        {
+            // Return value.
+            var result = new Node("sql");
+            var builder = new StringBuilder();
+
+            // Building SQL text and parameter collection.
+            builder.Append("delete from ");
+
+            // Getting table name from base class.
+            GetTableName(builder);
+
+            // Getting [where] clause.
+            BuildWhere(result, builder);
+
+            // Returning result to caller.
+            result.Value = builder.ToString();
+            return result;
+        }
+    }
+}
