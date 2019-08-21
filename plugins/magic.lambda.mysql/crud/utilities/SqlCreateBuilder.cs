e15a987638ad078cba4829611382bfccd9bdf261
Thomas Hansen <thomas@gaiasoul.com>
2019-08-21 18:22:54 +0300

    fgh

--- plugins/magic.lambda.mysql/crud/utilities/SqlCreateBuilder.cs
+++ plugins/magic.lambda.mysql/crud/utilities/SqlCreateBuilder.cs
@@ -3,7 +3,7 @@
  * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
  */
 
-using System.Linq;
+using System.Text;
 using magic.node;
 using com = magic.data.common;
 using magic.signals.contracts;
@@ -13,7 +13,12 @@ namespace magic.lambda.mysql.crud.utilities
     public class SqlCreateBuilder : com.SqlCreateBuilder
     {
         public SqlCreateBuilder(Node node, ISignaler signaler)
-            : base(node, signaler, "`", (builder) => builder.Append("; select last_insert_id();"), (_) => { })
+            : base(node, signaler, "`")
         { }
+
+        protected override void GetTail(StringBuilder builder)
+        {
+            builder.Append("; select last_insert_id();");
+        }
     }
 }
