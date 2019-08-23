3e71342702cc28eb8740be318c733129dca873ee
Thomas Hansen <thomas@gaiasoul.com>
2019-08-23 15:21:05 +0300

    sdf

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { EndpointsComponent } from './endpoints/endpoints.component';
 import { HomeComponent } from './home/home.component';
 
@@ -26,21 +29,25 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatButtonModule,
     MatToolbarModule,
+    MatCardModule,
     MatTableModule,
     MatInputModule,
     MatFormFieldModule,
-    MatCardModule,
-    HttpClientModule,
+    MatTooltipModule,
+    MatSnackBarModule,
   ],
   exports: [
     MatButtonModule,
     MatToolbarModule,
+    MatCardModule,
     MatTableModule,
     MatInputModule,
     MatFormFieldModule,
-    MatCardModule,
+    MatTooltipModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
