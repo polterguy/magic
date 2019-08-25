dcd775e6c670202f55bbebf2d2fe5df401dd4053
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-25 21:58:17 +0300

    Authentication largely done

---
+++ frontend/src/app/services/authenticate-service.ts
@@ -0,0 +1,21 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  authenticate(username: string, password: string) {
+    return this.httpClient.get<any>(
+      environment.apiURL + 
+      'hl/system/authenticate?username=' + 
+      encodeURI(username) + 
+      '&password=' + 
+      encodeURI(password));
+  }
+}
