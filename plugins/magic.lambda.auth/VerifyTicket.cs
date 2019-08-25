355c9fd1b4a695fd60e73b171512f541092ff307
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-25 22:42:13 +0300

    Almost done with authentication

---
+++ plugins/magic.lambda.auth/VerifyTicket.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Collections.Generic;
+using magic.node;
+using magic.lambda.auth.init;
+using magic.signals.contracts;
+using magic.hyperlambda.utils;
+
+namespace magic.lambda
+{
+	[Slot(Name = "auth.verify-ticket")]
+	public class VerifyTicket : ISlot, IMeta
+	{
+        readonly IServiceProvider _services;
+        readonly HttpService _httpService;
+        readonly ISignaler _signaler;
+
+        public VerifyTicket(
+            IServiceProvider services,
+            HttpService httpService,
+            ISignaler signaler)
+		{
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+            _httpService = httpService ?? throw new ArgumentNullException(nameof(httpService));
+            _signaler = signaler ?? throw new ArgumentNullException(nameof(signaler));
+        }
+
+        public void Signal(Node input)
+		{
+            _httpService.VerifyTicket(_services, input.GetEx<string>(_signaler));
+		}
+
+        public IEnumerable<Node> GetArguments()
+		{
+            yield return new Node(":", "*");
+        }
+    }
+}
---
+++ plugins/magic.lambda.auth/init/HttpService.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace magic.lambda.auth.init
+{
+    public class HttpService
+    {
+        public void VerifyTicket(IServiceProvider services, string role)
+        {
+            var contextAccessor = services.GetService(typeof(IHttpContextAccessor)) as IHttpContextAccessor;
+            var context = contextAccessor.HttpContext;
+            if (context == null)
+                throw new ApplicationException("No HTTP context exists");
+
+            var user = context.User;
+            if (user == null ||
+                !user.Identity.IsAuthenticated ||
+                !user.IsInRole(role))
+                throw new ApplicationException("Access denied");
+        }
+    }
+}
