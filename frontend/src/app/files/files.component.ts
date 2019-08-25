a758bcd9c53473306cf4f4dd57e036d7e20eb6b7
Thomas Hansen <thomas@gaiasoul.com>
2019-08-25 10:34:14 +0300

    Multiple modes for CodeMirror

--- frontend/src/app/files/files.component.ts
+++ frontend/src/app/files/files.component.ts
@@ -44,10 +44,13 @@ export class FilesComponent implements OnInit {
     if (path.endsWith('/')) {
       this.path = path;
       this.getPath();
-    } else if (path.endsWith('.hl')) {
-      this.openFile(path);
     } else {
-      this.showError('No editor registered for file');
+      const mode = this.getMode(path);
+      if (mode == null) {
+        this.showError('No editor registered for file');
+      } else{
+        this.openFile(path);
+      }
     }
   }
 
@@ -64,6 +67,40 @@ export class FilesComponent implements OnInit {
       this.fileContent = res;
       this.filePath = path;
     })
+  }
+
+  getOptions() {
+    return {
+      lineNumbers: true,
+      theme: 'material',
+      mode: this.getMode(this.filePath),
+      tabSize: 3,
+      indentUnit: 3,
+      indentAuto:true,
+      extraKeys:{
+        'Shift-Tab':'indentLess',
+        'Tab':'indentMore'
+      }
+    };
+  }
+
+  getMode(path: string) {
+    const fileEnding = path.substr(path.lastIndexOf('.') + 1);
+    switch(fileEnding) {
+      case 'hl':
+        return 'hyperlambda';
+      case 'md':
+        return 'markdown';
+      case 'js':
+        return 'jsx';
+      case 'css':
+        return 'css';
+      case 'htm':
+      case 'html':
+        return 'htmlmixed';
+      default:
+        return null;
+    }
   }
 
   evaluate() {
