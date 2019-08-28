b3519ec1b3678cbf0e7019d3635a82bff9b7900a
thomas@ainiro.io <thomas@gaiasoul.com>
2019-08-28 20:36:15 +0300

    Further cleaning up

--- plugins/magic.lambda/change/Convert.cs
+++ plugins/magic.lambda/change/Convert.cs
@@ -43,42 +43,55 @@ namespace magic.lambda.change
                 case "int":
                     input.Value = System.Convert.ToInt32(value, CultureInfo.InvariantCulture);
                     break;
+
                 case "long":
                     input.Value = System.Convert.ToInt64(value, CultureInfo.InvariantCulture);
                     break;
+
                 case "decimal":
                     input.Value = System.Convert.ToDecimal(value, CultureInfo.InvariantCulture);
                     break;
+
                 case "double":
                     input.Value = System.Convert.ToDouble(value, CultureInfo.InvariantCulture);
                     break;
+
                 case "single":
                     input.Value = System.Convert.ToSingle(value, CultureInfo.InvariantCulture);
                     break;
+
                 case "bool":
                     input.Value = value.Equals("true");
                     break;
+
                 case "date":
                     input.Value = DateTime.ParseExact(value.ToString(), "yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture);
                     break;
+
                 case "guid":
                     input.Value = new Guid(value.ToString());
                     break;
+
                 case "char":
                     input.Value = System.Convert.ToChar(value, CultureInfo.InvariantCulture);
                     break;
+
                 case "byte":
                     input.Value = System.Convert.ToByte(value, CultureInfo.InvariantCulture);
                     break;
+
                 case "x":
                     input.Value = new Expression(value.ToString());
                     break;
+
                 case "string":
                     input.Value = value.ToString();
                     break;
+
                 case "node":
                     input.Value = new Parser(value.ToString()).Lambda();
                     break;
+
                 default:
                     throw new ApplicationException($"Unknown type '{type}' when invoking [convert]");
             }
