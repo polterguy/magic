f327a9444031afb6810d804831c2622d9eef1561
Thomas Hansen <thomas@gaiasoul.com>
2019-09-01 08:58:49 +0300

    ToLowers on crudifyer

---
+++ plugins/magic.lambda/strings/ToLower.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Collections.Generic;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda.strings
+{
+    [Slot(Name = "to-lower")]
+    public class ToLower : ISlot, IMeta
+    {
+        readonly ISignaler _signaler;
+
+        public ToLower(ISignaler signaler)
+        {
+            _signaler = signaler ?? throw new ArgumentNullException(nameof(signaler));
+        }
+
+        public void Signal(Node input)
+        {
+            input.Value = input.GetEx<string>(_signaler).ToLowerInvariant();
+        }
+
+        public IEnumerable<Node> GetArguments()
+        {
+            yield return new Node(":", "*");
+        }
+    }
+}
