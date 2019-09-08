a2d6cd641b6b2959ab0a43cf347a7f02b6b318f9
thomas@ainiro.io <thomas@gaiasoul.com>
2019-09-08 17:29:23 +0300

    Comments

--- plugins/magic.lambda.auth/init/HttpService.cs
+++ plugins/magic.lambda.auth/init/HttpService.cs
@@ -13,24 +13,30 @@ namespace magic.lambda.auth.init
     {
         public void VerifyTicket(IServiceProvider services, string role)
         {
+            // Retrieving the HttpContext object.
             var contextAccessor = services.GetService(typeof(IHttpContextAccessor)) as IHttpContextAccessor;
             var context = contextAccessor.HttpContext;
             if (context == null)
                 throw new ApplicationException("No HTTP context exists");
 
+            // Verifying we're dealing with an authenticated user.
             var user = context.User;
             if (user == null ||
                 !user.Identity.IsAuthenticated)
                 throw new ApplicationException("Access denied");
+
+            // Verifying the users belongs to (at least) one of the comma separated roles supplied.
             var inRole = false;
             foreach (var idxRole in role.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
             {
-                if (user.IsInRole(idxRole))
+                if (user.IsInRole(idxRole.Trim()))
                 {
                     inRole = true;
                     break;
                 }
             }
+
+            // Throwing an exception unless the user belongs to the specified role.
             if (!inRole)
                 throw new ApplicationException("Access denied");
         }
