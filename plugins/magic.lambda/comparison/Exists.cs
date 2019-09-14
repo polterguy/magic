249ef0bb43ba890d9cf65994bb1a183f12d6af69
thomas@ainiro.io <thomas@gaiasoul.com>
2019-09-14 12:27:07 +0300

    [exists] slot

---
+++ plugins/magic.lambda/comparison/Exists.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda.comparison
+{
+    [Slot(Name = "exists")]
+    public class Exists : ISlot
+    {
+        public void Signal(Node input)
+        {
+            input.Value = input.Evaluate().Any();
+        }
+    }
+}
