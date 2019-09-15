3f224b4840c4c1bdafc6e568a460930a1ac12bcd
thomas@ainiro.io <thomas@gaiasoul.com>
2019-09-15 09:23:13 +0300

    Implemented Hyperlambda support for "refresh ticket" (JWT token)

--- plugins/magic.lambda.auth/CreateTicket.cs
+++ plugins/magic.lambda.auth/CreateTicket.cs
@@ -5,20 +5,16 @@
 
 using System;
 using System.Linq;
-using System.Text;
-using System.Security.Claims;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
 using magic.node;
+using magic.lambda.auth.helpers;
 using magic.node.extensions;
 using magic.signals.contracts;
 
-namespace magic.lambda
+namespace magic.lambda.auth
 {
 	[Slot(Name = "auth.create-ticket")]
-	public class CreateTicket : ISlot, IMeta
+	public class CreateTicket : ISlot
 	{
 		readonly ISignaler _signaler;
 		readonly IConfiguration _configuration;
@@ -32,58 +28,19 @@ namespace magic.lambda
 		public void Signal(Node input)
 		{
             if (input.Children.Any(x => x.Name != "username" && x.Name != "roles"))
-                throw new ApplicationException("[authenticate] can only handle [username] and [roles] children nodes");
+                throw new ApplicationException("[auth.create-ticket] can only handle [username] and [roles] children nodes");
 
             var usernameNode = input.Children.Where(x => x.Name == "username");
             var rolesNode = input.Children.Where(x => x.Name == "roles");
 
             if (usernameNode.Count() != 1 || rolesNode.Count() != 1)
-                throw new ApplicationException("[authenticate] must be given exactly one [username] and one [roles] children nodes");
+                throw new ApplicationException("[auth.create-ticket] must be given exactly one [username] and one [roles] children nodes");
 
             var username = usernameNode.First().GetEx<string>(_signaler);
             var roles = rolesNode.First().Children.Select(x => x.GetEx<string>(_signaler));
 
             input.Clear();
-            input.Value = CreateJWTToken(username, roles);
+            input.Value = TickerFactory.CreateTicket(_configuration, username, roles);
 		}
-
-        public IEnumerable<Node> GetArguments()
-		{
-            yield return new Node("username", 1);
-            yield return new Node("roles", 1);
-        }
-
-        #region [ -- Private helper methods -- ]
-
-        string CreateJWTToken(string username, IEnumerable<string> roles)
-        {
-            // Getting data to put into token.
-            var secret = _configuration["auth:secret"];
-            var validMinutes = int.Parse(_configuration["auth:valid-minutes"]);
-            var key = Encoding.ASCII.GetBytes(secret);
-
-            // Creating our token.
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, username),
-                }),
-                Expires = DateTime.UtcNow.AddMinutes(validMinutes),
-                SigningCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(key),
-                    SecurityAlgorithms.HmacSha512Signature),
-            };
-
-            // Adding all roles.
-            tokenDescriptor.Subject.AddClaims(roles.Select(x => new Claim(ClaimTypes.Role, x)));
-
-            // Creating token and returning to caller.
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
-        }
-
-        #endregion
     }
 }
--- plugins/magic.lambda.auth/init/HttpService.cs
+++ plugins/magic.lambda.auth/init/HttpService.cs
@@ -5,7 +5,9 @@
 
 using System;
 using System.Linq;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
+using magic.lambda.auth.helpers;
 
 namespace magic.lambda.auth.init
 {
@@ -21,24 +23,51 @@ namespace magic.lambda.auth.init
 
             // Verifying we're dealing with an authenticated user.
             var user = context.User;
-            if (user == null ||
-                !user.Identity.IsAuthenticated)
+            if (user == null || !user.Identity.IsAuthenticated)
                 throw new ApplicationException("Access denied");
 
-            // Verifying the users belongs to (at least) one of the comma separated roles supplied.
-            var inRole = false;
-            foreach (var idxRole in role.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            // Checking if caller is also trying to check if user belongs to some role(s).
+            if (!string.IsNullOrEmpty(role))
             {
-                if (user.IsInRole(idxRole.Trim()))
+                // Verifying the users belongs to (at least) one of the comma separated roles supplied.
+                var inRole = false;
+                foreach (var idxRole in role.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    inRole = true;
-                    break;
+                    if (user.IsInRole(idxRole.Trim()))
+                    {
+                        inRole = true;
+                        break;
+                    }
                 }
-            }
 
-            // Throwing an exception unless the user belongs to the specified role.
-            if (!inRole)
+                // Throwing an exception unless the user belongs to the specified role.
+                if (!inRole)
+                    throw new ApplicationException("Access denied");
+            }
+        }
+
+        public Ticket GetTicket(IServiceProvider services)
+        {
+            // Retrieving the HttpContext object.
+            var contextAccessor = services.GetService(typeof(IHttpContextAccessor)) as IHttpContextAccessor;
+            var context = contextAccessor.HttpContext;
+            if (context == null)
+                throw new ApplicationException("No HTTP context exists");
+
+            // Verifying we're dealing with an authenticated user.
+            var user = context.User;
+            if (user == null || !user.Identity.IsAuthenticated)
                 throw new ApplicationException("Access denied");
+
+            // Finding roles for user.
+            var identity = user.Identity as ClaimsIdentity;
+            var username = identity.Claims
+                .Where(c => c.Type == ClaimTypes.Name)
+                .Select(c => c.Value).First();
+            var roles = identity.Claims
+                .Where(c => c.Type == ClaimTypes.Role)
+                .Select(c => c.Value);
+            return new Ticket(username, roles);
         }
     }
 }
