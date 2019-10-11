4c91f33f7d7e6e39d55cc4d2eedf186d027f45b1
Thomas Hansen <thomas@gaiasoul.com>
2019-10-11 13:56:42 +0300

    Updating license

--- magic.backend/Startup.cs
+++ magic.backend/Startup.cs
@@ -3,7 +3,6 @@
  * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
  */
 
-using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
@@ -31,48 +30,26 @@ namespace magic.backend
         {
             // Adding some basic configurations.
             services.AddSingleton(Configuration);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             /*
              * Initializing Magic.
              */
-            Initializer.InitializeLog4net(
-                string.Concat(
-                    AppDomain.CurrentDomain.BaseDirectory, "log4net.config"));
-            Initializer.InitializeServices(Configuration, services);
+            services.AddMagic(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             /*
-             * Making sure we're storing errors into our log file, and that
-             * we're abot to return the exception message to client.
+             * Initializing Magic.
              */
-            app.UseExceptionHandler(errorApp => errorApp.Run(async context =>
-            {
-                context.Response.StatusCode = 500;
-                context.Response.ContentType = "application/json";
-                var ex = context.Features.Get<IExceptionHandlerPathFeature>();
-                var logger = LogManager.GetLogger(ex?.Error.GetType() ?? typeof(Startup));
-                var msg = ex?.Error.Message ?? "Unknown error";
-                logger.Error("At path: " + ex?.Path);
-                logger.Error(msg, ex?.Error);
-                var response = new JObject
-                {
-                    ["message"] = msg,
-                };
-                await context.Response.WriteAsync(response.ToString(Formatting.Indented));
-            }));
+			app.UseMagic(Configuration);
 
             app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
             app.UseAuthentication();
             app.UseMvc();
-
-            /*
-             * Initializing Magic.
-             */
-            Initializer.InitalizeApp(app);
         }
     }
 }
