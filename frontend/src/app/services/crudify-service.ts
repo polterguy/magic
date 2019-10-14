8b719b10371fdab403620748aa83025fd3688a7d
Thomas Hansen <thomas@gaiasoul.com>
2019-10-14 16:43:50 +0300

    Bettering

--- frontend/src/app/services/crudify-service.ts
+++ frontend/src/app/services/crudify-service.ts
@@ -13,30 +13,30 @@ export class CrudifyService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public getDatabases() {
-    return this.httpClient.get<any[]>(environment.apiURL + 'magic/modules/mysql/databases');
+  public getDatabases(databaseType: string) {
+    return this.httpClient.get<any[]>(environment.apiURL + `magic/modules/${databaseType}/databases`);
   }
 
-  public getTables(database: string) {
+  public getTables(databaseType: string, database: string) {
     return this.httpClient.get<any[]>(
       environment.apiURL +
-      'magic/modules/mysql/tables?database=' + encodeURI(database));
+      `magic/modules/${databaseType}/tables?database=` + encodeURI(database));
   }
 
-  public getColumns(database: string, table: string) {
+  public getColumns(databaseType: string, database: string, table: string) {
     return this.httpClient.get<any[]>(
       environment.apiURL +
-      'magic/modules/mysql/columns?database=' + encodeURI(database) +
+      `magic/modules/${databaseType}/columns?database=` + encodeURI(database) +
       '&table=' + encodeURI(table));
   }
 
-  public generateCrudEndpoints(model: CrudifyModel) {
+  public generateCrudEndpoints(databaseType: string, model: CrudifyModel) {
     return this.httpClient.post<CrudifyResult>(
-      environment.apiURL + 'magic/modules/mysql/crudify', model);
+      environment.apiURL + `magic/modules/${databaseType}/crudify`, model);
   }
 
-  public createCustomSqlEndpoint(model: CustomSQLModel) {
+  public createCustomSqlEndpoint(databaseType: string, model: CustomSQLModel) {
     return this.httpClient.post<CrudifyResult>(
-      environment.apiURL + 'magic/modules/mysql/custom-sql', model);
+      environment.apiURL + `magic/modules/${databaseType}/custom-sql`, model);
   }
 }
