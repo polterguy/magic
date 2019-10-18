78b5dfd115ff8e19543e5ed012337fa9a3a89c4d
Thomas Hansen <thomas@gaiasoul.com>
2019-10-18 13:52:43 +0300

    [version] slot, and updated ping, returning version

---
+++ magic.backend/slots/Version.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019, thomas@gaiasoul.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda.slots
+{
+    /// <summary>
+    /// [version] slot returning the version of Magic that is currently being used.
+    /// </summary>
+    [Slot(Name = "version")]
+    public class Version : ISlot
+    {
+        /// <summary>
+        /// Implementation of signal
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal</param>
+        /// <param name="input">Parameters passed from signaler</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            input.Value = "5.2.0";
+        }
+    }
+}
