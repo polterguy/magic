32d9cfd8ebc48d8709f5b15df95868daa5a911e4
thomas@ainiro.io <thomas@gaiasoul.com>
2019-10-20 16:15:34 +0300

    Displaying version in front end, and warning if default auth:secret is used
    Plus someother minor fixes.

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { PingService } from 'src/app/services/ping-service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private version: string;
+  private error: string = null;
 
-  constructor() { }
+  constructor(private pingService: PingService) { }
 
   ngOnInit() {
+    this.pingService.ping().subscribe(res => {
+      this.version = res.version;
+      if (res.warnings !== undefined) {
+        for (let idx in res.warnings) {
+          console.warn(res.warnings[idx]);
+        }
+      }
+    });
   }
 }
---
+++ frontend/src/app/services/ping-service.ts
@@ -0,0 +1,16 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PingService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public ping() {
+    return this.httpClient.get<any>(environment.apiURL + 'magic/modules/system/ping');
+  }
+}
