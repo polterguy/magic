b385138540c03823a79c21716d307a86aafb41bc
thomas@ainiro.io <thomas@gaiasoul.com>
2019-11-22 18:47:13 +0200

    Bettering

--- frontend/src/app/components/scheduler/scheduler.component.ts
+++ frontend/src/app/components/scheduler/scheduler.component.ts
@@ -34,6 +34,10 @@ export class SchedulerComponent implements OnInit {
     });
   }
 
+  createNewTask() {
+    console.log('TODO: Implement ...');
+  }
+
   getCodeMirrorOptions() {
     return {
       lineNumbers: true,
