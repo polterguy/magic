3be0e2c13ba871d806a438499bb3d56c2c4c7aea
Thomas Hansen <thomas@gaiasoul.com>
2019-12-02 14:53:09 +0200

    Merging retrieval of columns #19

--- magic.backend/slots/Version.cs
+++ magic.backend/slots/Version.cs
@@ -21,7 +21,7 @@ namespace magic.backend.slots
         /// <param name="input">Parameters passed from signaler</param>
         public void Signal(ISignaler signaler, Node input)
         {
-            input.Value = "5.5.5";
+            input.Value = "5.5.6";
         }
     }
 }
