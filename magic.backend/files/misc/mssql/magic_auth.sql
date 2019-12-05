498bfbbb4c5432f86c8c470ec654237d9ac9e0a5
thomas@ainiro.io <thomas@gaiasoul.com>
2019-12-05 15:59:58 +0200

    Bettering

--- magic.backend/files/misc/mssql/magic_auth.sql
+++ magic.backend/files/misc/mssql/magic_auth.sql
@@ -1,29 +1,27 @@
 
-USE [master];
-
-CREATE DATABASE [magic_auth];
+CREATE DATABASE [magic_auth]
+GO
 
 USE [magic_auth];
-
 CREATE TABLE [dbo].[users](
 	[username] [nvarchar](128) NOT NULL,
 	[password] [nvarchar](128) NOT NULL,
  CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED 
 (
 	[username] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY];
+)
+GO
 
-
+USE [magic_auth];
 CREATE TABLE [dbo].[roles](
 	[name] [nvarchar](128) NOT NULL,
  CONSTRAINT [PK_roles] PRIMARY KEY CLUSTERED 
 (
 	[name] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY];
+)
+GO
 
-
+USE [magic_auth];
 CREATE TABLE [dbo].[users_roles](
 	[user] [nvarchar](128) NOT NULL,
 	[role] [nvarchar](128) NOT NULL,
@@ -31,10 +29,16 @@ CREATE TABLE [dbo].[users_roles](
 (
 	[user] ASC,
 	[role] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY];
+)
+GO
 
-
+USE [magic_auth];
 insert into roles (name) values ('root');
+GO
+
+USE [magic_auth];
 insert into roles (name) values ('user');
+GO
+
+USE [magic_auth];
 insert into roles (name) values ('guest');
