1bc6d2da74b08cbbc4826a072c4ef93564dc4c48
Thomas Hansen <thomas@gaiasoul.com>
2019-12-05 17:56:40 +0200

    Bettering

---
+++ magic.backend/files/misc/mysql/templates/samples.sql
@@ -0,0 +1,7 @@
+
+
+-- These are sample SQL scripts that you can store, and rapidly reload from your SQL editor
+-- They can be found in the '/misc/mysql/templates/' folder in your system
+
+use some_database;
+select * from some_table;
