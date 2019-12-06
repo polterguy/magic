df6d64540e3c10b632ecb14f259a724542a51a2b
thomas@ainiro.io <thomas@gaiasoul.com>
2019-12-06 14:16:53 +0200

    Started woring on users module

--- frontend/src/app/components/users/users.component.ts
+++ frontend/src/app/components/users/users.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users-service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  private filter = '';
+  public displayedColumns: string[] = ['username'];
+  public users: any[];
+
+  constructor(private usersService: UsersService) { }
+
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.usersService.list(this.filter).subscribe(res => {
+      this.users = res;
+    });
+  }
+
+  dataSource() {
+    return this.users;
+  }
+
+  filterChanged() {
+    this.getUsers();
   }
 }
