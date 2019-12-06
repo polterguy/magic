ea9c70dc875f72f1749763957a1f6ae7987b719f
thomas@ainiro.io <thomas@gaiasoul.com>
2019-12-06 11:05:57 +0200

    Update magic_auth.sql

--- magic.backend/files/misc/mysql/magic_auth.sql
+++ magic.backend/files/misc/mysql/magic_auth.sql
@@ -1,45 +1,18 @@
 
 /*
- * This SQL script creates a database for you, with 3 tables,
+ * This MySQL script creates a database for you, with 3 tables,
  * serving as an authentication/authorization database for you.
- *
- * You can of course implement your own logic, and use your existing
- * database/table structure - However, this is a "one size fits all"
- * solution to that problem if you need it.
- *
- * To insert a new user into your database, you can use the "create-user.hl"
- * Hyperlambda file, that will cryptographically hash your password,
- * using a per user based salt, with the blow fish algorithm (extremely secure).
  */
 
-
-
-
-CREATE DATABASE  IF NOT EXISTS `magic_auth` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+/*
+ * Creating database
+ */
+CREATE DATABASE IF NOT EXISTS `magic_auth`;
 USE `magic_auth`;
--- MySQL dump 10.13  Distrib 8.0.17, for macos10.14 (x86_64)
---
--- Host: localhost    Database: magic_auth
--- ------------------------------------------------------
--- Server version	8.0.17
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `users`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*
+ * Creating users table.
+ */
 CREATE TABLE `users` (
   `username` varchar(256) NOT NULL,
   `password` varchar(256) NOT NULL,
@@ -47,39 +20,27 @@ CREATE TABLE `users` (
   UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
-
---
--- Table structure for table `roles`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*
+ * Creating roles table.
+ */
 CREATE TABLE `roles` (
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`name`),
   UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*
+ * Creating association between roles and users through users_roles table.
+ */
+CREATE TABLE `users_roles` (
+  `role` varchar(45) NOT NULL,
+  `user` varchar(256) NOT NULL,
+  PRIMARY KEY (`role`, `user`),
+  KEY `roles_fky_idx` (`role`),
+  KEY `users_fky_idx` (`user`),
+  CONSTRAINT `roles_fky` FOREIGN KEY (`role`) REFERENCES `roles` (`name`) ON DELETE CASCADE,
+  CONSTRAINT `users_fky` FOREIGN KEY (`user`) REFERENCES `users` (`username`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*
  * Inserting some few roles into our roles table.
@@ -87,32 +48,3 @@ CREATE TABLE `roles` (
 insert into roles (name) values ('root');
 insert into roles (name) values ('user');
 insert into roles (name) values ('guest');
-
-
---
--- Table structure for table `users_roles`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users_roles` (
-  `role` varchar(45) NOT NULL,
-  `user` varchar(256) NOT NULL,
-  PRIMARY KEY (`role`, `user`),
-  KEY `roles_fky_idx` (`role`),
-  KEY `users_fky_idx` (`user`),
-  CONSTRAINT `roles_fky` FOREIGN KEY (`role`) REFERENCES `roles` (`name`),
-  CONSTRAINT `users_fky` FOREIGN KEY (`user`) REFERENCES `users` (`username`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-09-07  9:24:38
