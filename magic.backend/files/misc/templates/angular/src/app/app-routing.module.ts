1c7ee1782fcb17e57550c452e3defeb3a420dc2c
thomas@ainiro.io <thomas@gaiasoul.com>
2019-12-26 16:09:50 +0200

    Routes

--- magic.backend/files/misc/templates/angular/src/app/app-routing.module.ts
+++ magic.backend/files/misc/templates/angular/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+[[imports]]
 
-const routes: Routes = [];
+const routes: Routes = [
+[[routes]]];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
