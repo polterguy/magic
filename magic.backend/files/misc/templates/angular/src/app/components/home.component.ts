d66a7f070405feb35e9f7bbafe6377d6b1ce4234
Thomas Hansen <thomas@gaiasoul.com>
2019-12-26 18:10:21 +0200

    Bettering

---
+++ magic.backend/files/misc/templates/angular/src/app/components/home.component.ts
@@ -0,0 +1,10 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+}
