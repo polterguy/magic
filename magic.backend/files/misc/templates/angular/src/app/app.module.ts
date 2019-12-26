47fd03194016b3b0ea3123952f43a37923e1ec46
thomas@ainiro.io <thomas@gaiasoul.com>
2019-12-26 12:56:48 +0200

    Bettering

--- magic.backend/files/misc/templates/angular/src/app/app.module.ts
+++ magic.backend/files/misc/templates/angular/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+[[imports]]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+[[declarations]]
   ],
   imports: [
     BrowserModule,
