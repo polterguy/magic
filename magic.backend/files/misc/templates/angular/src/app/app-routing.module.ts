fe7a2595055ba2f92797c3d770be1942dcb44a8d
Thomas Hansen <thomas@gaiasoul.com>
2019-12-29 13:08:36 +0200

    Bettering

--- magic.backend/files/misc/templates/angular/src/app/app-routing.module.ts
+++ magic.backend/files/misc/templates/angular/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { AuthComponent } from './components/auth/auth.component';
 [[imports]]
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'auth', component: AuthComponent },
 [[routes]]];
 
 @NgModule({
---
+++ magic.backend/files/misc/templates/angular/src/app/auth-service.ts
@@ -0,0 +1,112 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+export class AuthFilter {
+  filter?: string;
+  offset: number;
+  limit: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(filter: AuthFilter = null) {
+    let query = '';
+    if (filter !== null) {
+      query += '?limit=' + filter.limit;
+      query += "&offset=" + filter.offset;
+      if (filter.filter !== null && filter.filter !== undefined && filter.filter != '') {
+        query += '&username.like=' + encodeURIComponent(filter.filter + '%');
+      }
+    }
+    return this.httpClient.get<any>(
+      environment.apiUrl +
+      'magic/modules/magic_auth/users' + query);
+  }
+
+  getUsersCount(filter: string = null) {
+    let query = '';
+    if (filter !== null) {
+      query += '?username.like=' + encodeURIComponent(filter + '%');
+    }
+    return this.httpClient.get<any>(
+      environment.apiUrl +
+      'magic/modules/magic_auth/users-count' + query);
+  }
+
+  getRoles(filter: AuthFilter = null) {
+    let query = '';
+    if (filter !== null) {
+      query += '?limit=' + filter.limit;
+      query += "&offset=" + filter.offset;
+      if (filter.filter !== null && filter.filter !== undefined && filter.filter != '') {
+        query += '&name.like=' + encodeURIComponent(filter.filter + '%');
+      }
+    }
+    return this.httpClient.get<any>(
+      environment.apiUrl +
+      'magic/modules/magic_auth/roles' + query);
+  }
+
+  getRolesCount(filter: string = null) {
+    let query = '';
+    if (filter !== null) {
+      query += '?name.like=' + encodeURIComponent(filter + '%');
+    }
+    return this.httpClient.get<any>(
+      environment.apiUrl +
+      'magic/modules/magic_auth/roles-count' + query);
+  }
+
+  createUser(username: string, password: string) {
+    return this.httpClient.post<any>(environment.apiUrl + 'magic/modules/magic_auth/users', {
+      username,
+      password,
+    });
+  }
+
+  createRole(name: string, description: string) {
+    return this.httpClient.post<any>(environment.apiUrl + 'magic/modules/magic_auth/roles', {
+      name,
+      description,
+    });
+  }
+
+  deleteUser(username: string) {
+    return this.httpClient.delete<any>(
+      environment.apiUrl + 
+      'magic/modules/magic_auth/users?username=' + encodeURIComponent(username));
+  }
+
+  deleteRole(name: string) {
+    return this.httpClient.delete<any>(
+      environment.apiUrl + 
+      'magic/modules/magic_auth/roles?name=' + encodeURIComponent(name));
+  }
+
+  getUserRoles(username: string) {
+    return this.httpClient.get<any>(
+      environment.apiUrl +
+      'magic/modules/magic_auth/users_roles?user.eq=' + encodeURIComponent(username));
+  }
+
+  addRoleToUser(user: string, role: string) {
+    return this.httpClient.post<any>(environment.apiUrl + 'magic/modules/magic_auth/users_roles', {
+      user,
+      role,
+    });
+  }
+
+  deleteRoleFromUser(user: string, role: string) {
+    return this.httpClient.delete<any>(
+      environment.apiUrl + 
+      'magic/modules/magic_auth/users_roles?role=' + encodeURIComponent(role) +
+      '&user=' + encodeURIComponent(user));
+  }
+}
