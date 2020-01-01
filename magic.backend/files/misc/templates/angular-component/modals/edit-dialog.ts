0248472b2907001a4d1bffa6e2509bf27f3697ef
Thomas Hansen <thomas@gaiasoul.com>
2020-01-01 21:11:55 +0200

    Bettering

--- magic.backend/files/misc/templates/angular-component/modals/edit-dialog.ts
+++ magic.backend/files/misc/templates/angular-component/modals/edit-dialog.ts
@@ -3,6 +3,12 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatSnackBar } from '@angular/material';
 import { HttpService } from 'src/app/services/http-service';
 
+export interface DialogData {
+  isEdit: boolean;
+  entity: any;
+  editableFields: string[];
+}
+
 @Component({
   templateUrl: '[[filename]]-edit-modal.html',
   styleUrls: ['[[filename]]-edit-modal.scss']
@@ -11,13 +17,17 @@ export class [[edit-component-name]] {
 
   constructor(
     public dialogRef: MatDialogRef<[[edit-component-name]]>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private snackBar: MatSnackBar,
     private service: HttpService) { }
 
+  isEditable(name: string) {
+    return this.data.editableFields.indexOf(name) !== -1;
+  }
+
   ok() {
-    this.service.[[update-method]](this.data).subscribe(res => {
-      this.dialogRef.close(this.data);
+    this.service.[[update-method]](this.data.entity).subscribe(res => {
+      this.dialogRef.close(this.data.entity);
     }, error => {
       this.snackBar.open(error.error.message, 'Close', {
         duration: 5000,
