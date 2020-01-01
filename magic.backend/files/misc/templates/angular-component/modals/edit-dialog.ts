e82bdd99d1d4aa8b71fd36e76beebbe43504d620
Thomas Hansen <thomas@gaiasoul.com>
2020-01-01 22:06:21 +0200

    Bettering

--- magic.backend/files/misc/templates/angular-component/modals/edit-dialog.ts
+++ magic.backend/files/misc/templates/angular-component/modals/edit-dialog.ts
@@ -6,7 +6,6 @@ import { HttpService } from 'src/app/services/http-service';
 export interface DialogData {
   isEdit: boolean;
   entity: any;
-  editableFields: string[];
 }
 
 @Component({
@@ -21,19 +20,26 @@ export class [[edit-component-name]] {
     private snackBar: MatSnackBar,
     private service: HttpService) { }
 
-  isEditable(name: string) {
-    return this.data.editableFields.indexOf(name) !== -1;
-  }
-
   ok() {
-    this.service.[[update-method]](this.data.entity).subscribe(res => {
-      this.dialogRef.close(this.data.entity);
-    }, error => {
-      this.snackBar.open(error.error.message, 'Close', {
-        duration: 5000,
-        panelClass: ['error-snackbar'],
+    if (this.data.isEdit) {
+      this.service.[[update-method]](this.data.entity).subscribe(res => {
+        this.dialogRef.close(this.data.entity);
+      }, error => {
+        this.snackBar.open(error.error.message, 'Close', {
+          duration: 5000,
+          panelClass: ['error-snackbar'],
+        });
       });
-    });
+    } else {
+      this.service.[[create-method]](this.data.entity).subscribe(res => {
+        this.dialogRef.close(this.data.entity);
+      }, error => {
+        this.snackBar.open(error.error.message, 'Close', {
+          duration: 5000,
+          panelClass: ['error-snackbar'],
+        });
+      });
+    }
   }
   
   cancel() {
