def89b4f3ce8e88b12160dcb47a57a50bdc7d217
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:25:02 +0200

    Bettering

--- build4.cmd
+++ build4.cmd
@@ -1,12 +1,4 @@
 
-cd %~dp0/..
-for /d /r . %d in (bin) do @if exist "%d" rd /s/q "%d"
-for /d /r . %d in (obj) do @if exist "%d" rd /s/q "%d"
-
-../magic.data.common/build.cmd
-../magic.endpoint/build.cmd
-../magic.http/build.cmd
-../magic.io/build.cmd
 ../magic.lambda/build.cmd
 ../magic.lambda.auth/build.cmd
 ../magic.lambda.config/build.cmd
@@ -23,6 +15,3 @@ for /d /r . %d in (obj) do @if exist "%d" rd /s/q "%d"
 ../magic.lambda.slots/build.cmd
 ../magic.lambda.strings/build.cmd
 ../magic.lambda.validators/build.cmd
-../magic.library/build.cmd
-../magic.node/build.cmd
-../magic.signals/build.cmd
---
+++ build5.cmd
@@ -0,0 +1,2 @@
+
+../magic.library/build.cmd
