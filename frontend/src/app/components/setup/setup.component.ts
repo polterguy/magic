f62d5e54c6e5aa11ca3d56b1780e03d677015e9d
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 10:59:32 +0200

    Bettering

--- frontend/src/app/components/setup/setup.component.ts
+++ frontend/src/app/components/setup/setup.component.ts
@@ -2,6 +2,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 import { SetupService } from 'src/app/services/setup-service';
+import { AuthenticateService } from 'src/app/services/authenticate-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-evaluator',
@@ -15,10 +17,15 @@ export class SetupComponent implements OnInit {
   databaseType: string = null;
   mssqlConnectionString: string = null;
   mysqlConnectionString: string = null;
+  password: string = null;
+  repeatPassword: string = null;
+  hasShownSucces = false;
 
   constructor(
     private setupService: SetupService,
-    private snackBar: MatSnackBar) { }
+    private authService: AuthenticateService,
+    private snackBar: MatSnackBar,
+    private router: Router) { }
 
   ngOnInit() {
     this.setupService.getAppSettingsJson().subscribe(res => {
@@ -31,12 +38,35 @@ export class SetupComponent implements OnInit {
     });
   }
 
+  checkConfig() {
+    if (this.jwtSecret !== null &&
+      this.jwtSecret.length >= 50 &&
+      this.databaseType !== null &&
+      this.password === this.repeatPassword &&
+      this.password !== null) {
+        if (!this.hasShownSucces) {
+          this.hasShownSucces = true;
+          this.showInfo('You can now save your configuration');
+        }
+        return true;
+    }
+    this.hasShownSucces = false;
+    return false;
+  }
+
   save() {
     this.config.magic.auth.secret = this.jwtSecret;
     this.config.magic.databases.mssql.generic = this.mssqlConnectionString;
     this.config.magic.databases.mysql.generic = this.mysqlConnectionString;
     this.setupService.saveAppSettingsJson(this.config).subscribe(res => {
-      if (res.result.success === 'success') {}
+      if (res.result === 'success') {
+        this.authService.authenticate('root', this.password).subscribe(res2 => {
+          this.showInfo('You have successfully secured your system');
+          this.router.navigate(['']);
+        }, err => {
+          this.showError('Error, are you sure your connection string is correct?');
+        });
+      }
     });
   }
 
@@ -49,7 +79,7 @@ export class SetupComponent implements OnInit {
 
   showInfo(error: string) {
     this.snackBar.open(error, 'Close', {
-      duration: 10000,
+      duration: 2000,
       panelClass: ['info-snackbar'],
     });
   }
