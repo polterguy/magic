610edea0fb94d0c67d5247c86e53eabefcc3a26e
thomas@ainiro.io <thomas@gaiasoul.com>
2020-01-16 16:43:03 +0200

    Update build1.cmd

--- build1.cmd
+++ build1.cmd
@@ -1,5 +1,16 @@
 
-../magic.node/build.cmd
-../magic.http/build.cmd
-../magic.endpoint/build.cmd
-../magic.io/build.cmd
+echo Moving one folder up
+
+cd ..
+
+echo Deleting all bin and obj folders
+
+FOR /d /r . %%d IN ("bin") DO @IF EXIST "%%d" rd /s /q "%%d"
+FOR /d /r . %%d IN ("obj") DO @IF EXIST "%%d" rd /s /q "%%d"
+
+echo Building node, http, endpoint, and io
+
+magic.node/build.cmd
+magic.http/build.cmd
+magic.endpoint/build.cmd
+magic.io/build.cmd
