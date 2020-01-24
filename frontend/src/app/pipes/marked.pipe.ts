19036314635223793dabe100dfbb00edf73d0db7
thomas@ainiro.io <thomas@gaiasoul.com>
2020-01-24 18:03:53 +0200

    Displaying markdown for templates

---
+++ frontend/src/app/pipes/marked.pipe.ts
@@ -0,0 +1,15 @@
+
+import { Pipe, PipeTransform } from '@angular/core';
+import * as marked from "marked";
+
+@Pipe({
+  name: 'marked'
+})
+export class MarkedPipe implements PipeTransform {
+  transform(value: any): any {
+    if (value && value.length > 0) {
+      return marked(value);
+    }
+    return value;
+  }
+}
