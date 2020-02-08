7336175d19fa3d01d168121a7157536a6f11c7fe
thomas@ainiro.io <thomas@gaiasoul.com>
2020-02-08 12:01:20 +0200

    Update component.ts

--- magic.backend/files/misc/templates/angular-default/templates/sql-component/component.ts
+++ magic.backend/files/misc/templates/angular-default/templates/sql-component/component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { JwtHelperService } from '@auth0/angular-jwt';
 import { ChartOptions } from 'chart.js';
 import { Label } from 'ng2-charts';
 
@@ -20,27 +19,45 @@ export class [[component-name]] implements OnInit {
   private chartOptions: ChartOptions = {
     responsive: true,
     legend: {
-      position: 'top',
+      position: 'left',
     },
   };
   private chartLabels: Label[] = [];
   private chartData: number[] = [];
   private chartColors = [
     {
-      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
+      backgroundColor: [
+        'rgba(240,240,240,0.8)',
+        'rgba(225,225,225,0.8)',
+        'rgba(210,210,210,0.8)',
+        'rgba(195,195,195,0.8)',
+        'rgba(180,180,180,0.8)',
+        'rgba(165,165,165,0.8)',
+        'rgba(150,150,150,0.8)',
+        'rgba(135,135,135,0.8)',
+        'rgba(120,120,120,0.8)',
+        'rgba(105,105,105,0.8)',
+        'rgba(90,90,90,0.8)',
+        'rgba(75,75,75,0.8)',
+        'rgba(60,60,60,0.8)',
+        'rgba(45,45,45,0.8)',
+        'rgba(30,30,30,0.8)',
+        'rgba(15,15,15,0.8)',
+      ],
     },
   ];
 
   constructor(
     private httpService: HttpService,
-    private jwtHelper: JwtHelperService,
     private snackBar: MatSnackBar) { }
 
   // OnInit implementation. Retrieves statistics from backend, and initialized data for our graph/chart.
   ngOnInit() {
-    this.httpService.[[service-get-method]]({}).subscribe(res => {
+    this.httpService.statistics_1_Get({}).subscribe(res => {
       this.chartData = res.map(x => x.value);
       this.chartLabels = res.map(x => x.label);
+    }, error => {
+      this.error(error.error.message);
     });
   }
 
