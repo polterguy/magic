8f335404904acc66dc1b3bd7f631a2b800edaa20
Thomas Hansen <thomas@gaiasoul.com>
2020-02-10 11:54:29 +0200

    Adding Northwind statistics example

--- magic.backend/files/misc/mssql/templates/samples.sql
+++ magic.backend/files/misc/mssql/templates/samples.sql
@@ -3,5 +3,14 @@
 -- These are sample SQL scripts that you can store, and rapidly reload from your SQL editor
 -- They can be found in the '/misc/mssql/templates/' folder in your system
 
+-- Selects all records from some table
+
 use some_database;
 select * from some_table;
+
+-- Selects top 15 average UnitPrice from Northwind
+
+select top 15 ProductName as label, avg(UnitPrice) as value
+	from Products
+    group by ProductName
+    order by value desc
--- magic.backend/files/misc/mysql/templates/samples.sql
+++ magic.backend/files/misc/mysql/templates/samples.sql
@@ -3,5 +3,8 @@
 -- These are sample SQL scripts that you can store, and rapidly reload from your SQL editor
 -- They can be found in the '/misc/mysql/templates/' folder in your system
 
+-- Selects all records from some table.
+
 use some_database;
 select * from some_table;
+
