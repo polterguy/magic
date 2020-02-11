5681f6f6304f9821298b7be2adc123e42d5766b4
Thomas Hansen <thomas@gaiasoul.com>
2020-02-11 10:49:00 +0200

    Bettering

--- magic.backend/files/misc/mssql/templates/samples.sql
+++ magic.backend/files/misc/mssql/templates/samples.sql
@@ -3,14 +3,10 @@
 -- These are sample SQL scripts that you can store, and rapidly reload from your SQL editor
 -- They can be found in the '/misc/mssql/templates/' folder in your system
 
--- Selects all records from some table
-
-use some_database;
-select * from some_table;
-
 -- Selects top 15 average UnitPrice from Northwind
-use Northwind;
+-- Notice, make sure you somehow select the "Northwind" database, using for instance.
+-- use Northwind; -- or something similar ...
 select top 15 ProductName as label, avg(UnitPrice) as value
 	from Products
     group by ProductName
-    order by value desc;
+    order by value desc
--- magic.backend/files/misc/mysql/templates/samples.sql
+++ magic.backend/files/misc/mysql/templates/samples.sql
@@ -3,16 +3,11 @@
 -- These are sample SQL scripts that you can store, and rapidly reload from your SQL editor
 -- They can be found in the '/misc/mysql/templates/' folder in your system
 
--- Selects all records from some table.
-
-use some_database;
-select * from some_table;
-
 -- Selects top 10 paying customers from Sakila database.
-use Sakila;
+-- Notice, make sure you somehow select the "Sakila" database, using for instance.
+-- use Sakila; -- or something similar ...
 select concat(c.first_name, ' ', c.last_name) as label, sum(amount) as value
     from payment p
 	inner join customer c on p.customer_id = c.customer_id
 	group by c.customer_id order by value desc
-    limit 10;
-
+    limit 10
