9bc809107af0b350783c56992aa7145b65957985
thomas@ainiro.io <thomas@gaiasoul.com>
2020-05-26 10:30:44 +0300

    Evaluator FullScreen support

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,6 +8,7 @@ import { environment } from './environments/environment';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/sql/sql';
+import 'codemirror/addon/display/fullscreen';
 
 if (environment.production) {
   enableProdMode();
