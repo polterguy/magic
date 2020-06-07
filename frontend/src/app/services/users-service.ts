d6a5483fea8d2136a38a14cda529ab54370d59d4
thomas@ainiro.io <thomas@gaiasoul.com>
2020-06-07 12:35:23 +0300

    Making sure auth database is configurable

--- frontend/src/app/services/users-service.ts
+++ frontend/src/app/services/users-service.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { TicketService } from './ticket-service';
+import { Observable } from 'rxjs';
 
+// TODO: Cleanup, too much repetition!
 @Injectable({
   providedIn: 'root'
 })
@@ -20,9 +22,18 @@ export class UsersService {
         '' :
         '?username.like=' + encodeURIComponent('%' + filter + '%');
 
-    return this.httpClient.get<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/users' + like);
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.get<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/users` + like).subscribe(res2 => {
+              observer.next(res2);
+              observer.complete();
+            });
+        });
+      });
   }
 
   listRoles(filter: string = null) {
@@ -32,66 +43,151 @@ export class UsersService {
         '' :
         '?name.like=' + encodeURIComponent('%' + filter + '%');
 
-    return this.httpClient.get<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/roles' + like);
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.get<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/roles` + like).subscribe(res2 => {
+              observer.next(res2);
+              observer.complete();
+            });
+        });
+      });
   }
 
   createUser(username: string, password: string) {
-    return this.httpClient.post<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/users', {
-      username,
-      password,
-    });
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.post<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/users`, {
+            username,
+            password,
+          }).subscribe(res2 => {
+            observer.next(res2);
+            observer.complete();
+          });
+        });
+      });
   }
 
   createRole(name: string, description: string) {
-    return this.httpClient.post<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/roles', {
-      name,
-      description
-    });
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.post<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/roles`, {
+            name,
+            description
+          }).subscribe(res2 => {
+            observer.next(res2);
+            observer.complete();
+          });
+        });
+      });
   }
 
   deleteUser(username: string) {
-    return this.httpClient.delete<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/users?username=' + encodeURIComponent(username));
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.delete<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/users?username=` +
+            encodeURIComponent(username)).subscribe(res2 => {
+              observer.next(res2);
+              observer.complete();
+            });
+        });
+      });
   }
 
   deleteRole(name: string) {
-    return this.httpClient.delete<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/roles?name=' + encodeURIComponent(name));
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.delete<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/roles?name=` +
+            encodeURIComponent(name)).subscribe(res2 => {
+              observer.next(res2);
+              observer.complete();
+            });
+        });
+      });
   }
 
   getRoles(username: string) {
-    return this.httpClient.get<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/users_roles?user.eq=' + encodeURIComponent(username));
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.get<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/users_roles?user.eq=` +
+            encodeURIComponent(username)).subscribe(res2 => {
+              observer.next(res2);
+              observer.complete();
+            });
+        });
+      });
   }
 
   addRoleToUser(user: string, role: string) {
-    return this.httpClient.post<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/users_roles', {
-        user,
-        role,
-    });
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.post<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/users_roles`, {
+              user,
+              role,
+          }).subscribe(res2 => {
+            observer.next(res2);
+            observer.complete();
+          });
+        });
+      });
   }
 
   deleteRoleFromUser(user: string, role: string) {
-    return this.httpClient.delete<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/users_roles?role=' + encodeURIComponent(role) +
-      '&user=' + encodeURIComponent(user));
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.delete<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/users_roles?role=` +
+            encodeURIComponent(role) +
+            '&user=' + encodeURIComponent(user)).subscribe(res2 => {
+              observer.next(res2);
+              observer.complete();
+            });
+        });
+      });
   }
 
   getAllRoles() {
-    return this.httpClient.get<any>(
-      this.ticketService.getBackendUrl() +
-      'magic/modules/magic_auth/roles');
+    return new Observable<any>(observer => {
+      this.httpClient.get<any>(
+        this.ticketService.getBackendUrl() +
+        'magic/modules/system/auth/database').subscribe(res => {
+          return this.httpClient.get<any>(
+            this.ticketService.getBackendUrl() +
+            `magic/modules/${res.database}/roles`).subscribe(res2 => {
+              observer.next(res2);
+              observer.complete();
+            });
+        });
+      });
   }
 }
