bb31edb80f054ea0671ca35f842419583285fb60
thomas@ainiro.io <thomas@gaiasoul.com>
2020-06-28 22:47:45 +0300

    Update environment.prod.ts

--- backend/files/misc/templates/angular-default/templates/main/src/environments/environment.prod.ts
+++ backend/files/misc/templates/angular-default/templates/main/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // The URL to your backend API.
+  apiUrl: '[[apiUrl]]',
+
+  // The domain for your backend API. Needed for "oauth0".
+  apiDomain: '[[environmentDomain]]',
 };
