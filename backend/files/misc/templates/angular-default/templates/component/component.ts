d343755bf6d91352ff0d4c700a13144bc0daf7da
thomas@ainiro.io <thomas@gaiasoul.com>
2020-06-29 20:44:18 +0300

    Bettering

--- backend/files/misc/templates/angular-default/templates/component/component.ts
+++ backend/files/misc/templates/angular-default/templates/component/component.ts
@@ -58,8 +58,27 @@ export class [[component-name]] extends GridComponent implements OnInit {
     return this.httpService.[[service-count-method]](filter);
   }
 
-  // OnInit implementation. Retrieves initial data (unfiltered) and instantiates our FormControls.
-  ngOnInit() {
+  /**
+   * Overridden abstract method from base class, that returns the Observable
+   * necessary to actually delete items in backend.
+   */
+  protected getDelete(ids: any) {
+    return this.httpService.[[service-delete-method]](ids);
+  }
+
+  /**
+   * Implementation of abstract base class method, to reset paginator
+   * of component.
+   */
+  protected resetPaginator() {
+    this.paginator.pageIndex = 0;
+  }
+
+  /**
+   * OnInit implementation. Retrieves initial data (unfiltered),
+   * and instantiates our FormControls.
+   */
+  public ngOnInit() {
 
     // Retrieves data from our backend, unfiltered, and binds our mat-table accordingly.
     this.getData();
@@ -67,129 +86,40 @@ export class [[component-name]] extends GridComponent implements OnInit {
     // Necessary to make sure we can have "live filtering" in our datagrid.
 [[form-control-instantiations]]  }
 
-  // Sorts by the specified column.
-  sort(column: string) {
-    if (this.filter.order === column) {
-
-      // Inverting sort direction.
-      this.filter.direction =
-        this.filter.direction === 'asc' ||
-        this.filter.direction === null ||
-        this.filter.direction === undefined ?
-          'desc' :
-          'asc';
-    } else {
-      this.filter.order = column;
-      this.filter.direction = 'asc';
-    }
-    this.paginator.pageIndex = 0;
-    this.filter.offset = 0;
-    this.getData(false);
-  }
-
-  /*
+  /**
    * Invoked when user wants to edit an entity
    * This will show a modal dialog, allowing the user to edit his record.
    */
-  editDetails(entity: any) {
+  public editDetails(entity: any) {
 
-    /*
-     * Cloning our entity, in case user doesn't click the "Save" button,
-     * to avoid changing main databound entity.
-     */
-    const data = {
-      isEdit: true,
-      entity: {},
-    };
-    for (const idx in entity) {
-      if (Object.prototype.hasOwnProperty.call(entity, idx)) {
-        data.entity[idx] = entity[idx];
-      }
-    }
-
-    // Creating our modal dialog, passing in the cloned entity, and "isEdit" as true.
     const dialogRef = this.dialog.open(Edit[[component-name]], {
-      data
+      data: this.getEditData(entity)
     });
-    dialogRef.afterClosed().subscribe(res => {
-      if (res !== null && res !== undefined) {
-
-        // User clicked "Save" button, making sure we update databound grid item according to the new value(s).
-        for (const idx in res) {
-          if (Object.prototype.hasOwnProperty.call(res, idx)) {
-            entity[idx] = res[idx];
-          }
-        }
-
-        // Showing a little information window, giving the user feedback about that editing was successful.
-        this.snackBar.open('[[component-header]] item successfully updated', 'Close', {
-          duration: 2000,
-        });
-      }
+    dialogRef.afterClosed().subscribe(editResult => {
+      this.setEditData(editResult, entity);
     });
   }
 
-  // Creates a new data record, by showing the modal edit/create dialog.
-  createNewRecord() {
+  /**
+   * Creates a new data record, by showing the modal edit/create dialog.
+   */
+  public createNewRecord() {
 
-    // Parametrizing our modal dialog correctly. Notice "idEdit" being false.
-    const data = {
-      isEdit: false,
-      entity: {},
-    };
-
-    // Opening our dialog.
     const dialogRef = this.dialog.open(Edit[[component-name]], {
-      data
-    });
-    dialogRef.afterClosed().subscribe(res => {
-      if (res !== null && res !== undefined) {
-
-        // Notice, at this point, the item is already saved. Hence, re-retrieving data from backend.
-        this.getData();
-        this.snackBar.open('[[component-header]] item successfully created', 'Close', {
-          duration: 2000,
-        });
-      }
+      data: {
+        isEdit: false,
+        entity: {},
+      }});
+    dialogRef.afterClosed().subscribe((createResult: any) => {
+      this.itemCreated(createResult);
     });
   }
 
-  // Invoked when an entity is deleted. Invokes HTTP service that deletes item from backend.
-  delete(entity: any, ids: any) {
-
-    // Making sure we actually have a primary key, and if not, preventing deletion.
-    let hasKeys = false;
-    for (const idx in ids) {
-      if (ids.hasOwnProperty(idx)) {
-        hasKeys = true;
-        break;
-      }
-    }
-    if (!hasKeys) {
-      this.showError('Your endpoint does not accept any primary keys, and hence deletion of individual entities becomes impossible');
-      return;
-    }
-
-    // Invoking HTTP service DELETE method.
-    this.httpService.[[service-delete-method]](ids).subscribe(res => {
-
-      // Sanity checking invocation.
-      if (res['deleted-records'] !== 1) {
-        this.showError(`For some reasons ${res['deleted-records']} records was deleted, and not 1 as expected!`);
-      }
-
-      // Making sure we remove "view details" for item, if item is currently being viewed.
-      const indexOf = this.viewDetails.indexOf(entity);
-      if (indexOf !== -1) {
-        this.viewDetails.splice(indexOf, 1);
-      }
-
-      // Re-retrieving data from backend, according to filter (we're down one record now according to our pager).
-      this.getData();
-    }, error => this.showError(error));
-  }
-
-  // Invoked when pager is paged.
+  /**
+   * Invoked when pager is paged.
+   * 
+   * @param e Paging event
+   */
   paged(e: PageEvent) {
     this.viewDetails = [];
     if (this.filter.limit !== e.pageSize) {
