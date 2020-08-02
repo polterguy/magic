9bfe9ad6e4a7c4b1cea1d632eb42381762213ab5
thomas@ainiro.io <thomas@gaiasoul.com>
2020-08-02 10:47:02 +0300

    Update magic_tasks.sql

--- backend/files/misc/mysql/magic_tasks.sql
+++ backend/files/misc/mysql/magic_tasks.sql
@@ -12,7 +12,7 @@ use magic_tasks;
  */
 create table tasks (
   id varchar(256) not null,
-  description varchar(1024) not null,
+  description varchar(1024) null,
   hyperlambda text not null,
   created datetime not null default current_timestamp,
   primary key (id)
@@ -26,7 +26,7 @@ create table task_due (
   id int(11) not null auto_increment,
   task varchar(256) not null,
   due datetime not null,
-  repetition varchar(128) null,
+  pattern varchar(128) null,
   constraint task_due_task_fky foreign key (task) references tasks (id) on delete cascade,
   primary key (id),
   unique key id_UNIQUE (id)
