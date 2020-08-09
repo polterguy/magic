6baa4e12c62de10632eb0551fd9fd664fe256fa2
Thomas Hansen <thomas@gaiasoul.com>
2020-08-09 11:57:28 +0300

    Bettering

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -93,6 +93,7 @@ export function tokenGetter() {
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
