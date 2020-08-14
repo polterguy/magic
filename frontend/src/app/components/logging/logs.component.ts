9c2df87c32f1798e58c1cfc2fa1ea47e247af3c4
Thomas Hansen <thomas@gaiasoul.com>
2020-08-14 19:32:26 +0300

    Bettering

--- frontend/src/app/components/logging/logs.component.ts
+++ frontend/src/app/components/logging/logs.component.ts
@@ -13,6 +13,7 @@ export class LogsComponent implements OnInit {
   public offset = 0;
   public limit = 10;
   public items: LogItem[] = null;
+  public count: number;
   public displayedColumns: string[] = ['when', 'type', 'content'];
 
   constructor(private logService: LogService) { }
@@ -20,6 +21,9 @@ export class LogsComponent implements OnInit {
   ngOnInit() {
     this.logService.listLogItems(this.offset, this.limit).subscribe(res => {
       this.items = res;
+    });
+    this.logService.countLogItems().subscribe(res => {
+      this.count = res.result;
     });
   }
 }
