220ed5fcbd31a966d3b1262c80fb938bd82364b7
Thomas Hansen <thomas@gaiasoul.com>
2020-08-14 18:39:30 +0300

    Bettering

--- frontend/src/app/pipes/date-to-pipe.ts
+++ frontend/src/app/pipes/date-to-pipe.ts
@@ -1,33 +1,34 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'fromNow'
+  name: 'since'
 })
-export class DateFromPipe implements PipeTransform {
+export class DateToPipe implements PipeTransform {
+
   transform(value: any, args: string[]): any {
-    const dateWhen = new Date(value).getTime();
+    const due = new Date(value).getTime();
     const now = new Date().getTime();
-    const deltaSeconds = Math.round((dateWhen - now) / 1000);
+    const deltaSeconds = Math.round((now - due) / 1000);
     if (deltaSeconds < 180) {
-      return `${deltaSeconds} seconds ago`;
+      return `${deltaSeconds} seconds since now`;
     }
     const deltaMinutes = Math.round(deltaSeconds / 60);
     if (deltaMinutes < 60) {
-      return `${deltaMinutes} minutes ago`;
+      return `${deltaMinutes} minutes since now`;
     }
     const deltaHours = Math.round(deltaMinutes / 60);
     if (deltaHours < 24) {
-      return `${deltaHours} hours ago`;
+      return `${deltaHours} hours since now`;
     }
     const deltaDays = Math.round(deltaHours / 24);
     if (deltaDays < 60) {
-      return `${deltaDays} days ago`;
+      return `${deltaDays} days since now`;
     }
     const deltaWeeks = Math.round(deltaDays / 7);
     if (deltaWeeks < 20) {
-      return `${deltaWeeks} weeks ago`;
+      return `${deltaWeeks} weeks since now`;
     }
     const deltaMonths = Math.round(deltaWeeks / 4.2);
-    return `${deltaMonths} months ago`;
+    return `${deltaMonths} months since now`;
   }
 }
