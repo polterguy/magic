d1a827e71e90093941d733fd3c7d98c8bb95d886
thomas@ainiro.io <thomas@gaiasoul.com>
2020-08-14 09:59:31 +0300

    Bettering

--- backend/files/misc/mysql/magic.sql
+++ backend/files/misc/mysql/magic.sql
@@ -1,8 +1,7 @@
 
 /*
- * This MySQL script creates a database for you, with 5 tables,
- * serving as an authentication/authorization database for you,
- * in addition to your task scheduler database tables.
+ * This MySQL script creates a database for you, serving
+ * as Magic's main database.
  */
 
 
@@ -14,36 +13,36 @@
 /*
  * Creating users table.
  */
-CREATE TABLE `users` (
-  `username` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  PRIMARY KEY (`username`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
+create table `users` (
+  `username` varchar(256) not null,
+  `password` varchar(256) not null,
+  primary key (`username`),
+  unique key `username_UNIQUE` (`username`)
 );
 
 
 /*
  * Creating roles table.
  */
-CREATE TABLE `roles` (
-  `name` varchar(45) NOT NULL,
-  `description` varchar(256) NULL,
-  PRIMARY KEY (`name`),
-  UNIQUE KEY `name_UNIQUE` (`name`)
+create table `roles` (
+  `name` varchar(45) not null,
+  `description` varchar(256) null,
+  primary key (`name`),
+  unique key `name_UNIQUE` (`name`)
 );
 
 
 /*
  * Creating association between roles and users through users_roles table.
  */
-CREATE TABLE `users_roles` (
-  `role` varchar(45) NOT NULL,
-  `user` varchar(256) NOT NULL,
-  PRIMARY KEY (`role`, `user`),
-  KEY `roles_fky_idx` (`role`),
-  KEY `users_fky_idx` (`user`),
-  CONSTRAINT `roles_fky` FOREIGN KEY (`role`) REFERENCES `roles` (`name`) ON DELETE CASCADE,
-  CONSTRAINT `users_fky` FOREIGN KEY (`user`) REFERENCES `users` (`username`) ON DELETE CASCADE
+create table `users_roles` (
+  `role` varchar(45) not null,
+  `user` varchar(256) not null,
+  primary key (`role`, `user`),
+  key `roles_fky_idx` (`role`),
+  key `users_fky_idx` (`user`),
+  constraint `roles_fky` foreign key (`role`) references `roles` (`name`) on delete cascade,
+  constraint `users_fky` foreign key (`user`) references `users` (`username`) on delete cascade
 );
 
 
@@ -53,6 +52,11 @@ CREATE TABLE `users_roles` (
 insert into roles (name, description) values ('root', 'This is a root account in your system, and it has complete access to do anything.');
 insert into roles (name, description) values ('user', 'This is a normal user in your system, and it does not have elevated rights in general.');
 insert into roles (name, description) values ('guest', 'This is just a guest visitor to your system, and does not have elevated rights in general.');
+
+
+/*
+ * Tasks and task scheduler tables
+ */
 
 
 /*
@@ -76,6 +80,25 @@ create table task_due (
   due datetime not null,
   repeats varchar(128) null,
   constraint task_due_task_fky foreign key (task) references tasks (id) on delete cascade,
+  primary key (id),
+  unique key id_UNIQUE (id)
+);
+
+
+/*
+ * Logging tables.
+ */
+
+
+/*
+ * Creating log table.
+ */
+create table log_entries (
+  id int(11) not null auto_increment,
+  created datetime not null default current_timestamp,
+  type varchar(10) not null,
+  content text not null,
+  exception text null,
   primary key (id),
   unique key id_UNIQUE (id)
 );
