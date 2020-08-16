71f85c306e04cc2bb9b111661e6eb9ea24799c9f
thomas@ainiro.io <thomas@gaiasoul.com>
2020-08-16 12:29:49 +0300

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -113,7 +113,7 @@ export function tokenGetter() {
     JwtModule.forRoot({
       config: {
         tokenGetter,
-        whitelistedDomains: environment.whitelistedDomains,
+        allowedDomains: environment.whitelistedDomains,
       }
     }),
     MatButtonModule,
