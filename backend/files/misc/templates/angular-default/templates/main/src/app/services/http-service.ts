40dac6cbc9182ad3cac2b9bcc9661d483c813826
thomas@ainiro.io <thomas@gaiasoul.com>
2020-09-01 16:35:33 +0300

    b

--- backend/files/misc/templates/angular-default/templates/main/src/app/services/http-service.ts
+++ backend/files/misc/templates/angular-default/templates/main/src/app/services/http-service.ts
@@ -35,6 +35,54 @@ export class HttpService {
       password,
     });
   }
+
+  // Logs an info item in the backend server.
+  logInfo(content: string) {
+    this.httpClient.post<any>(environment.apiUrl + 'magic/modules/system/logging/log', {
+      type: 'info',
+      content
+    }).subscribe(res => {
+      console.log('Info item successfully logged');
+    }, error => {
+      console.error(error.error.message);
+    });
+  }
+
+  // Logs an error item in the backend server.
+  logError(content: string) {
+    this.httpClient.post<any>(environment.apiUrl + 'magic/modules/system/logging/log', {
+      type: 'error',
+      content
+    }).subscribe(res => {
+      console.log('Error item successfully logged');
+    }, error => {
+      console.error(error.error.message);
+    });
+  }
+
+  // Logs an info item in the backend server.
+  logDebug(content: string) {
+    this.httpClient.post<any>(environment.apiUrl + 'magic/modules/system/logging/log', {
+      type: 'debug',
+      content
+    }).subscribe(res => {
+      console.log('Debug item successfully logged');
+    }, error => {
+      console.error(error.error.message);
+    });
+  }
+
+  // Logs an info item in the backend server.
+  logFatal(content: string) {
+    this.httpClient.post<any>(environment.apiUrl + 'magic/modules/system/logging/log', {
+      type: 'fatal',
+      content
+    }).subscribe(res => {
+      console.log('Fatal item successfully logged');
+    }, error => {
+      console.error(error.error.message);
+    });
+  }
   
   // Creates QUERY arguments from the specified "args" argument given.
   getQueryArgs(args: any) {
