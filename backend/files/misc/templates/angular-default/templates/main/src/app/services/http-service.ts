c1647305677a02ef9477edbcde38d4fbe8e0ba8e
Thomas Hansen <thomas@gaiasoul.com>
2020-10-02 14:50:48 +0300

    Way better typing in CRUD method groups, returning interface declaration

--- backend/files/misc/templates/angular-default/templates/main/src/app/services/http-service.ts
+++ backend/files/misc/templates/angular-default/templates/main/src/app/services/http-service.ts
@@ -1,6 +1,58 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+
+ /**
+  * Entity method group containing Create, Read and Delete, but not Update.
+  * 
+  * This interface is implemented on method groups not containing update,
+  * which might happen, if a table have no updateable columns, such as
+  * link tables, with only primary keys, etc.
+  */
+ export interface ICrdEntity {
+
+  /**
+   * Creates a new entity.
+   *
+   * @param args Initial values for your entity
+   */
+  create(args: any) : Observable<any>;
+
+  /**
+   * Reads entities from your backend.
+   *
+   * @param filter Filter condition for items to retrieve
+   */
+  read(filter: any) : Observable<any>;
+
+  /**
+   * Counts entities from your backend.
+   *
+   * @param filter Filter condition for items to count
+   */
+  count(filter: any) : Observable<any>;
+
+  /**
+   * Deletes one entity from your backend.
+   *
+   * @param filter Filter condition for item to delete, primary key(s)
+   */
+  delete(args: any) : Observable<any>;
+}
+
+/**
+ * Entity method group containing all 4 CRUD operations.
+ */
+export interface ICrudEntity extends ICrdEntity {
+
+  /**
+   * Updates one entity in your backend.
+   *
+   * @param args What item to update, and what values to update it with
+   */
+  update(args: any) : Observable<any>;
+}
 
 /**
  * Your main HTTP service for invoking CRUD methods in your backend.
