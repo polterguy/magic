b9f7363d41819f538631227a959365933369f2ec
thomas@ainiro.io <thomas@gaiasoul.com>
2020-10-08 07:41:27 +0300

    Updating version to 8.3.3

--- backend/slots/Version.cs
+++ backend/slots/Version.cs
@@ -21,7 +21,7 @@ namespace magic.backend.slots
         /// <param name="input">Parameters passed from signaler</param>
         public void Signal(ISignaler signaler, Node input)
         {
-            input.Value = "8.3.2";
+            input.Value = "8.3.3";
         }
     }
 }
