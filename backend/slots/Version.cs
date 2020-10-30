5dc546a8fc81afe38c00f40d0cab731145006bee
thomas@ainiro.io <thomas@gaiasoul.com>
2020-10-30 13:56:06 +0200

    Update Version.cs

--- backend/slots/Version.cs
+++ backend/slots/Version.cs
@@ -21,7 +21,7 @@ namespace magic.backend.slots
         /// <param name="input">Parameters passed from signaler</param>
         public void Signal(ISignaler signaler, Node input)
         {
-            input.Value = "8.6.3";
+            input.Value = "8.6.4";
         }
     }
 }
