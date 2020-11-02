6a9c0d4f629a0252d85008bb34674663244467ab
Thomas Hansen <thomas@gaiasoul.com>
2020-11-02 10:52:47 +0200

    Displaying license information on main landing page (home component)

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -11,20 +11,28 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class HomeComponent implements OnInit {
 
   public version: string;
+  public licenseInfo: any = null;
 
   constructor(
     private pingService: PingService,
     private snackBar: MatSnackBar) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.pingService.version().subscribe(res => {
       this.version = res.version;
     }, error => {
       this.snackBar.open(error.error.message || 'Something went wrong when trying to ping backend', 'Close');
     });
+    this.pingService.license().subscribe(res => {
+      this.licenseInfo = res;
+    });
   }
 
   getVersion() {
     return this.version;
+  }
+
+  getLocalDate(date: string) {
+    return new Date(date).toLocaleString();
   }
 }
