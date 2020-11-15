66089413a54136f96832b0de43fd39dbd165a659
thomas@ainiro.io <thomas@gaiasoul.com>
2020-11-15 08:15:59 +0200

    Legend dialog + F5 on SQL menu item

---
+++ frontend/src/app/components/sql/modals/legend-dialog.ts
@@ -0,0 +1,10 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'legend-dialog.html',
+  styleUrls: ['legend-dialog.scss'],
+})
+export class LegendDialogComponent {
+
+}
