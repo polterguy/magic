c3f096583771050e555940ae1d92d44aa108881a
thomas@ainiro.io <thomas@gaiasoul.com>
2020-11-29 16:02:07 +0200

    Started work on logs

--- frontend2/src/app/app.module.ts
+++ frontend2/src/app/app.module.ts
@@ -4,45 +4,57 @@
  */
 
 // Angular imports.
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-// Material imports
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+// Material imports.
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-// App specific imports
+// Application specific imports.
+import { MarkedPipe } from './pipes/marked.pipe';
+import { DateToPipe } from './pipes/date-to.pipe';
+import { DateFromPipe } from './pipes/date-from.pipe';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app/app.component';
-import { SqlComponent } from './components/sql/sql.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginDialogComponent } from './components/app/login-dialog/login-dialog.component';
+import { LoaderService } from './services/loader-service';
 import { AuthInterceptor } from './services/interceptors/auth.interceptor';
 import { LoaderInterceptor } from './services/interceptors/loader-interceptor';
-import { LoaderService } from './services/loader-service';
-import { CrudifierComponent } from './components/crudifier/crudifier.component';
-import { EndpointsComponent } from './components/endpoints/endpoints.component';
+
+// Components.
+import { LogComponent } from './components/log/log.component';
+import { AppComponent } from './components/app/app.component';
+import { SqlComponent } from './components/sql/sql.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { HomeComponent } from './components/home/home.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { FilesComponent } from './components/files/files.component';
-import { EvaluatorComponent } from './components/evaluator/evaluator.component';
-import { AuthComponent } from './components/auth/auth.component';
-import { LogComponent } from './components/log/log.component';
 import { CryptoComponent } from './components/crypto/crypto.component';
+import { CrudifierComponent } from './components/crudifier/crudifier.component';
+import { EndpointsComponent } from './components/endpoints/endpoints.component';
+import { EvaluatorComponent } from './components/evaluator/evaluator.component';
 
+// Modal dialogs.
+import { LoginDialogComponent } from './components/app/login-dialog/login-dialog.component';
+
+/**
+ * The main module for your Magic Dashboard application.
+ */
 @NgModule({
   declarations: [
     AppComponent,
@@ -57,6 +69,9 @@ import { CryptoComponent } from './components/crypto/crypto.component';
     AuthComponent,
     LogComponent,
     CryptoComponent,
+    MarkedPipe,
+    DateToPipe,
+    DateFromPipe,
   ],
   imports: [
     FormsModule,
@@ -75,8 +90,10 @@ import { CryptoComponent } from './components/crypto/crypto.component';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
+    MatTableModule,
     MatAutocompleteModule,
     MatSnackBarModule,
+    MatPaginatorModule,
     MatProgressSpinnerModule,
   ],
   providers: [
