6ea2af474220d481be54f49404e12ca43f4ace00
thomas@ainiro.io <thomas@gaiasoul.com>
2020-12-03 08:04:34 +0200

    Implementing missing verbs

--- frontend2/src/app/services/http-service.ts
+++ frontend2/src/app/services/http-service.ts
@@ -4,12 +4,12 @@
  */
 
 // Angular and system imports.
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 // Application specific imports.
 import { AuthService } from './auth.service';
-import { Observable } from 'rxjs';
 
 /**
  * HTTP service for invoking endpoints towards your currently active backend.
@@ -68,4 +68,50 @@ export class HttpService {
       }
     });
   }
-}
+
+  /**
+     * Invokes the HTTP PUT verb towards your specified URL
+     * in your currently selected backend, passing in the specified
+     * payload, and returns the result.
+     * 
+   * @param url Backend URL of endpoint
+   * @param req Request payload to post
+   */
+  public put<Request, Response>(url: string, req: Request) {
+    return new Observable<Response>(observer => {
+
+      // Sanity checking invocation
+      if (!this.authService.connected) {
+        observer.error('Not connected to any backend, please choose or configure a backend before trying to invoke your backend');
+        observer.complete();
+      } else {
+        this.httpClient.put<Response>(this.authService.current.url + url, req).subscribe(res => {
+          observer.next(res);
+          observer.complete();
+        });
+      }
+    });
+  }
+
+    /**
+     * Invokes the HTTP DELETE verb towards your specified URL
+     * in your currently selected backend, and returns the result.
+     * 
+     * @param url Backend URL to endpoint
+     */
+    public delete<Response>(url: string) {
+      return new Observable<Response>(observer => {
+  
+        // Sanity checking invocation
+        if (!this.authService.connected) {
+          observer.error('Not connected to any backend, please choose or configure a backend before trying to invoke your backend');
+          observer.complete();
+        } else {
+          this.httpClient.delete<Response>(this.authService.current.url + url).subscribe(res => {
+            observer.next(res);
+            observer.complete();
+          });
+        }
+      });
+    }
+  }
