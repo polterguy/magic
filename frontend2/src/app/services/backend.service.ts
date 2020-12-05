5a827851d4c3d236572420387821f5e63573c0d2
thomas@ainiro.io <thomas@gaiasoul.com>
2020-12-05 16:28:19 +0200

    Bettering, but not done

--- frontend2/src/app/services/backend.service.ts
+++ frontend2/src/app/services/backend.service.ts
@@ -11,7 +11,10 @@ import { Backend } from '../models/backend.model';
 import { environment } from 'src/environments/environment';
 
 /**
- * Keeps track of your backend URLs.
+ * Keeps track of your backend URLs and currently selected backends.
+ * 
+ * This service will store your backends in the local storage object,
+ * to allow for easily selecting a backend you have previously connected to.
  */
 @Injectable({
   providedIn: 'root'
@@ -29,15 +32,16 @@ export class BackendService {
     // Reading persisted backends from local storage, or defaulting to whatever is in our environment.ts file.
     let backends: Backend[];
     const storage = localStorage.getItem('backends');
-    backends = storage === null ?
-      environment.defaultBackends :
-      backends = <Backend[]>JSON.parse(storage);
+    backends = storage === null ? environment.defaultBackends : backends = <Backend[]>JSON.parse(storage);
     this._backends = backends;
 
     // Checking we actually have any backends stored.
     if (this._backends.length > 0) {
 
-      // Defaulting selected backend to whatever has a JWT token value, since this was the previously selected backend.
+      /*
+       * Defaulting selected backend to whatever has a JWT token value,
+       * since this was the previously selected backend user had selected.
+       */
       const tmp = this._backends.filter(x => x.token !== null);
       this._current = tmp.length > 0 ? tmp[0] : this._backends[0];
     }
@@ -62,7 +66,11 @@ export class BackendService {
    */
   public set current(value: Backend) {
 
-    // Checking to see if the backend exists from before, and if so, updating its fields.
+    /*
+     * Checking to see if the backend exists from before,
+     * and if it does we update its fields. If not, we append
+     * a new backend to the list of backends we handle.
+     */
     const existing = this._backends.filter(x => x.url === value.url);
     if (existing.length > 0) {
 
@@ -77,6 +85,11 @@ export class BackendService {
       // Appending backend.
       this._backends.push(value);
     }
+
+    /*
+     * Persisting all backends to local storage object,
+     * and updating the currently selected backend.
+     */
     this.persistBackends();
     this._current = value;
   }
