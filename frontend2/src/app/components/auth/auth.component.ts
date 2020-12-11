54b13dd0d828f12474806647a8699a04e715bf95
thomas@ainiro.io <thomas@gaiasoul.com>
2020-12-11 15:32:54 +0200

    Continuing implementation of auth component

--- frontend2/src/app/components/auth/auth.component.ts
+++ frontend2/src/app/components/auth/auth.component.ts
@@ -4,12 +4,15 @@
  */
 
 // Angular and system imports.
+import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 // Application specific imports.
 import { User } from 'src/app/models/user.model';
 import { BaseComponent } from '../base.component';
 import { UserService } from 'src/app/services/user.service';
+import { AuthFilter } from 'src/app/models/auth-filter.model';
 import { MessageService } from 'src/app/services/message.service';
 
 /**
@@ -28,12 +31,23 @@ export class AuthComponent extends BaseComponent implements OnInit {
    */
   public users: User[] = [];
 
+  public filter: AuthFilter = {
+    limit: 20,
+    offset: 0,
+    filter: '',
+  };
+
   /**
    * What columns to display in table.
    */
   public displayedColumns: string[] = [
     'username'
   ];
+
+  /**
+   * Filter form control for filtering users to display.
+   */
+  public filterFormControl: FormControl;
 
   /**
    * Creates an instance of your component.
@@ -51,9 +65,40 @@ export class AuthComponent extends BaseComponent implements OnInit {
    */
   public ngOnInit() {
 
+    // Creating our filtering control.
+    this.filterFormControl = new FormControl('');
+    this.filterFormControl.setValue('');
+    this.filterFormControl.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe((query: string) => {
+        this.getUsers(query);
+      });
+
     // Retrieving users from backend.
-    this.userService.list().subscribe((users: User[]) => {
+    this.getUsers(this.filterFormControl.value);
+  }
+
+  /**
+   * Retrieves users from your backend.
+   */
+  public getUsers(filter: string) {
+
+    // Updating filter value.
+    this.filter.filter = filter;
+
+    // Invoking backend.
+    this.userService.list(this.filter).subscribe((users: User[]) => {
+
+      // Assigning users, triggering a re-render operation of the material table.
       this.users = users;
+
     }, (error: any) => this.showError(error));
+  }
+
+  /**
+   * Clears any filters user has applied for the users table.
+   */
+  public clearUserFilter() {
+    this.filterFormControl.setValue('');
   }
 }
