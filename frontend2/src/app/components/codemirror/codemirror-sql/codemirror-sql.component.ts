9358f03ebe3dbba3acf27f431623e28fbc55348b
Thomas Hansen <thomas@gaiasoul.com>
2020-12-14 07:43:12 +0200

    Started work on retrieving auto complete items for SQL editor

--- frontend2/src/app/components/codemirror/codemirror-sql/codemirror-sql.component.ts
+++ frontend2/src/app/components/codemirror/codemirror-sql/codemirror-sql.component.ts
@@ -6,6 +6,11 @@
 // Angular and system imports.
 import { Component, Input, OnInit } from '@angular/core';
 
+// Application specific imports.
+import { BaseComponent } from '../../base.component';
+import { SqlService } from 'src/app/services/sql.service';
+import { MessageService } from 'src/app/services/message.service';
+
 /**
  * Model class for CodeMirror instance's SQL..
  */
@@ -30,15 +35,31 @@ export class Model {
   templateUrl: './codemirror-sql.component.html',
   styleUrls: ['./codemirror-sql.component.scss']
 })
-export class CodemirrorSqlComponent implements OnInit {
+export class CodemirrorSqlComponent extends BaseComponent implements OnInit {
 
   /**
    * Model for component containing SQL that is displayed.
    */
   @Input() public model: Model;
 
-  constructor() { }
+  /**
+   * Creates an instance of your component.
+   * 
+   * @param sqlService Necessary to retrieve auto complete object
+   * @param messageService Used to signal other components
+   */
+  constructor(
+    private sqlService: SqlService,
+    protected messageService: MessageService) {
+      super(messageService);
+  }
 
+  /**
+   * Implementation of OnInit.
+   */
   ngOnInit() {
+    this.sqlService.vocabulary('mysql', 'magic').subscribe((result: any) => {
+      console.log(result);
+    });
   }
 }
