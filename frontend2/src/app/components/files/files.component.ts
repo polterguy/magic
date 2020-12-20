bf299e435e70f4adde64b2c1dd1212a28c574c6e
Thomas Hansen <thomas@gaiasoul.com>
2020-12-20 10:01:25 +0200

    Working more on files menu item

--- frontend2/src/app/components/files/files.component.ts
+++ frontend2/src/app/components/files/files.component.ts
@@ -27,7 +27,8 @@ export class FilesComponent extends BaseComponent implements OnInit {
    * Displayed columns in data table.
    */
   public displayedColumns: string[] = [
-    'path'
+    'icon',
+    'path',
   ];
 
   /**
@@ -66,9 +67,60 @@ export class FilesComponent extends BaseComponent implements OnInit {
    * 
    * @param path Path to return object name for
    */
-  public getObjectName(path: string) {
+  public getItemName(path: string) {
     const elements = path.split('/').filter(x => x !== '');
     return elements[elements.length - 1];
+  }
+
+  /**
+   * Returns true if given path is a folder
+   * 
+   * @param path What path to check
+   */
+  public isFolder(path: string) {
+    return path.endsWith('/');
+  }
+
+  /**
+   * Invoked when a path item, such as a file or folder is clicked.
+   * 
+   * @param path Item that was clicked
+   */
+  public itemClicked(path: string) {
+
+    // Checking what type of item that was clicked.
+    if (this.isFolder(path)) {
+
+      // Open the specified folder.
+      this.currentFolder = path;
+      this.getFolderContent();
+
+    } else {
+
+      // Open the specified file.
+      this.editItem(path);
+    }
+  }
+
+  /**
+   * Invoked when user wants to go up one level from his current folder.
+   */
+  public up() {
+
+    // Fiding all folders we're inside of.
+    const elements = this.currentFolder.split('/').filter(x => x !== '');
+
+    // Removing last folder, and prepending '/'.
+    let newFolder = '/' + elements.slice(0, elements.length - 1).join('/');
+
+    // Checking if this is not root folder, at which point we append another '/' at the end.
+    if (newFolder !== '/') {
+      newFolder += '/';
+    }
+
+    // Updating current folder, and re-retrieving items for folder.
+    this.currentFolder = newFolder;
+    this.getFolderContent();
   }
 
   /*
@@ -89,5 +141,12 @@ export class FilesComponent extends BaseComponent implements OnInit {
       this.items = (res[0] || []).concat(res[1] || []);
 
     }, (error: any) => this.showError(error));
+  }
+
+  /*
+   * Invoked when user wants to edit an item
+   */
+  private editItem(path: string) {
+    console.log(path);
   }
 }
