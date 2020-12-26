c9ba9e07c55b92d7d8daf9b1d9d0da5ca534f62c
thomas@ainiro.io <thomas@gaiasoul.com>
2020-12-26 08:56:37 +0200

    Implementing paging and sorting

--- frontend2/src/app/services/crypto.service.ts
+++ frontend2/src/app/services/crypto.service.ts
@@ -8,7 +8,9 @@ import { Injectable } from '@angular/core';
 
 // Application specific imports.
 import { HttpService } from './http.service';
+import { Count } from '../models/count.model';
 import { Affected } from '../models/affected.model';
+import { Response } from '../models/response.model';
 import { PublicKey } from '../models/public-key.model';
 import { PublicKeyFull } from '../models/public-key-full.model';
 
@@ -54,7 +56,7 @@ export class CryptoService {
 
       // Applying filter parts, if given.
       if (filter.filter && filter.filter !== '') {
-        query += '&username.like=' + encodeURIComponent(filter.filter + '%');
+        query += '&email.like=%' + encodeURIComponent(filter.filter + '%');
       }
     }
 
@@ -63,11 +65,61 @@ export class CryptoService {
   }
 
   /**
+   * Returns public keys from your backend.
+   * 
+   * @param filter Filter for which keys to return
+   */
+  public countPublicKeys(filter: any) {
+
+    // Dynamically building our query parameters.
+    let query = '';
+    if (filter !== null && filter.filter && filter.filter !== '') {
+      query += '?email.like=%' + encodeURIComponent(filter.filter + '%');
+    }
+
+    // Invoking backend and returning observable.
+    return this.httpService.get<Count>('/magic/modules/magic/crypto_keys-count' + query);
+  }
+
+  /**
    * Deletes a public key from your backend.
    * 
    * @param id Unique ID of public key to delete
    */
   public deletePublicKey(id: number) {
+
+    // Invoking backend and returning observable.
     return this.httpService.get<Affected>('/magic/modules/magic/crypto_keys?id=' + id);
+  }
+
+  /**
+   * Saves the public key declaration by invoking backend.
+   * 
+   * @param key Key caller wants to save
+   */
+  public savePublicKey(key: PublicKey) {
+    return this.httpService.put<Affected>('/magic/modules/magic/crypto_keys', {
+      id: key.id,
+      subject: key.subject,
+      email: key.email,
+      domain: key.domain,
+      fingerprint: key.fingerprint,
+      content: key.content,
+      vocabulary: key.vocabulary,
+      enabled: key.enabled,
+    });
+  }
+
+  /**
+   * Invokes server to find the fingerprint of a public key.
+   * 
+   * @param key Key to retrieve fingerprint for
+   */
+  public getFingerprint(key: string) {
+
+    // Invoking backend and returning observable.
+    return this.httpService.get<Response>(
+      '/magic/modules/system/crypto/get-fingerprint?key=' +
+      encodeURIComponent(key));
   }
 }
