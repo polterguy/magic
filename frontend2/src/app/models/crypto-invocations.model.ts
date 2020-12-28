9a48243691e5804da7de2dd1d115b9c945346b41
thomas@ainiro.io <thomas@gaiasoul.com>
2020-12-28 12:26:49 +0200

    Continuing work on crypto invocations

---
+++ frontend2/src/app/models/crypto-invocations.model.ts
@@ -0,0 +1,40 @@
+
+/*
+ * Copyright(c) Thomas Hansen thomas@servergardens.com, all right reserved
+ */
+
+/**
+ * Model for cryptographically signed invocations towards your backend.
+ */
+export class CryptoInvocation {
+
+  /**
+   * Unique ID of invocation.
+   */
+  id: number;
+
+  /**
+   * What key was used to cryptographically sign the invocation.
+   */
+  crypto_key: number;
+
+  /**
+   * Date and time of invocation.
+   */
+  created: Date;
+
+  /**
+   * Hyperlambda content of request.
+   */
+  request: string;
+
+  /**
+   * Raw request, including signature of request
+   */
+  request_raw: Date;
+
+  /**
+   * Response server generated and returned to caller.
+   */
+  response: Date;
+}
