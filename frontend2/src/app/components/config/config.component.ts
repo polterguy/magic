46203807ab5305a3616fd2e2bdab09bb72c70a4d
thomas@ainiro.io <thomas@gaiasoul.com>
2021-01-02 07:49:55 +0200

    Started working on license editor

--- frontend2/src/app/components/config/config.component.ts
+++ frontend2/src/app/components/config/config.component.ts
@@ -4,16 +4,12 @@
  */
 
 // Angular and system imports.
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 // Application specific imports.
-import { FeedbackService } from '../../services/feedback.service';
 import { Status } from 'src/app/models/status.model';
-import { Response } from 'src/app/models/response.model';
 import { ConfigService } from 'src/app/services/config.service';
-
-// CodeMirror options.
-import json from '../codemirror/options/json.json'
+import { FeedbackService } from '../../services/feedback.service';
 
 /**
  * Setup component allowing you to setup and modify your system's configuration.
@@ -29,18 +25,6 @@ export class ConfigComponent implements OnInit {
    * Status of setup process.
    */
   public status: Status = null;
-
-  /**
-   * CodeMirror options object, taken from common settings.
-   */
-  public cmOptions = {
-    json: json,
-  };
-
-  /**
-   * Raw configuration settings as returned from backend.
-   */
-  public config: string = null;
 
   /**
    * Creates an instance of your component.
@@ -59,36 +43,12 @@ export class ConfigComponent implements OnInit {
 
     // Checking status of backend.
     this.configService.status().subscribe((res: Status) => {
-      this.status = res;
-      if (res.magic_crudified && res.server_keypair && res.config_done) {
 
-        // Fetching raw config from backend.
-        this.configService.loadConfig().subscribe((res: any) => {
-          this.config = JSON.stringify(res, null, 2);
-        });
-      }
+      // Assigning returned value to status.
+      this.status = res;
+
     }, (error: any) => {
       this.feedbackService.showError(error);
     });
-  }
-
-  /**
-   * Saves your configuration.
-   */
-  public save() {
-
-    // Converting configuration to JSON.
-    const config = JSON.parse(this.config);
-
-    // Saving config by invoking backend.
-    this.configService.saveConfig(config).subscribe((res: Response) => {
-
-      // Sanity checking result of invocation.
-      if (res.result === 'success') {
-        this.feedbackService.showInfo('Configuration was successfully saved');
-      } else {
-        this.feedbackService.showError('Unspecified error when saving configuration');
-      }
-    }, (error: any) => this.feedbackService.showError(error));
   }
 }
