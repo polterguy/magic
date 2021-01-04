f493735c2cb1e34fca24dc5dcddedbf6029acda2
Thomas Hansen <thomas@gaiasoul.com>
2021-01-04 14:25:34 +0200

    Adding unhandled exceptions chart meta information

--- frontend2/src/app/components/health/health-restarts/health-restarts.component.ts
+++ frontend2/src/app/components/health/health-restarts/health-restarts.component.ts
@@ -66,6 +66,68 @@ export class HealthRestartsComponent implements OnInit {
     ]}];
 
   /**
+   * Labels for log items per day bar chart.
+   */
+  public taskLabels: Label[] = [];
+
+  /**
+   * Dataset for log items per day bar chart.
+   */
+  public taskData: SingleDataSet = null;
+
+  /**
+   * Colors for log items per day bar chart.
+   */
+  public taskColors = [{
+    backgroundColor: [
+      'rgba(200,200,200,0.6)',
+      'rgba(190,190,190,0.6)',
+      'rgba(180,180,180,0.6)',
+      'rgba(170,170,170,0.6)',
+      'rgba(160,160,160,0.6)',
+      'rgba(150,150,150,0.6)',
+      'rgba(140,140,140,0.6)',
+      'rgba(130,130,130,0.6)',
+      'rgba(120,120,120,0.6)',
+      'rgba(110,110,110,0.6)',
+      'rgba(100,100,100,0.6)',
+      'rgba(90,90,90,0.6)',
+      'rgba(80,80,80,0.6)',
+      'rgba(70,70,70,0.6)',
+    ]}];
+
+  /**
+   * Labels for log items per day bar chart.
+   */
+  public unhandledLabels: Label[] = [];
+
+  /**
+   * Dataset for log items per day bar chart.
+   */
+  public unhandledData: SingleDataSet = null;
+
+  /**
+   * Colors for log items per day bar chart.
+   */
+  public unhandledColors = [{
+    backgroundColor: [
+      'rgba(200,200,200,0.6)',
+      'rgba(190,190,190,0.6)',
+      'rgba(180,180,180,0.6)',
+      'rgba(170,170,170,0.6)',
+      'rgba(160,160,160,0.6)',
+      'rgba(150,150,150,0.6)',
+      'rgba(140,140,140,0.6)',
+      'rgba(130,130,130,0.6)',
+      'rgba(120,120,120,0.6)',
+      'rgba(110,110,110,0.6)',
+      'rgba(100,100,100,0.6)',
+      'rgba(90,90,90,0.6)',
+      'rgba(80,80,80,0.6)',
+      'rgba(70,70,70,0.6)',
+    ]}];
+
+  /**
    * Creates an instance of your component.
    * 
    * @param healthService Needed to retrieve healt diagnostic data from backend
@@ -77,10 +139,22 @@ export class HealthRestartsComponent implements OnInit {
    */
   public ngOnInit() {
 
-    // Retrieving logins per day type of statistics.
+    // Retrieving restarts per day type of statistics.
     this.healthService.statisticsDays('Magic was successfully started').subscribe((res: any[]) => {
       this.restartData = res.map(x => x.count);
       this.restartLabels = res.map(x => moment(new Date(x.date)).format("D. MMM"));
+    });
+
+    // Retrieving task executions per day type of statistics.
+    this.healthService.statisticsDays('Preparing to execute task with id of \'').subscribe((res: any[]) => {
+      this.taskData = res.map(x => x.count);
+      this.taskLabels = res.map(x => moment(new Date(x.date)).format("D. MMM"));
+    });
+
+    // Retrieving unhandled exceptions per day type of statistics.
+    this.healthService.statisticsDays('Unhandled exception occurred \'').subscribe((res: any[]) => {
+      this.unhandledData = res.map(x => x.count);
+      this.unhandledLabels = res.map(x => moment(new Date(x.date)).format("D. MMM"));
     });
   }
 }
