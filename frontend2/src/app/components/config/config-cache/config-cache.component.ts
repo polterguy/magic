47b377fa243999b00f945a847e678957f6f789bd
Thomas Hansen <thomas@gaiasoul.com>
2021-01-05 09:56:49 +0200

    Started working on viewing cache item values

--- frontend2/src/app/components/config/config-cache/config-cache.component.ts
+++ frontend2/src/app/components/config/config-cache/config-cache.component.ts
@@ -25,6 +25,11 @@ export class ConfigCacheComponent implements OnInit {
   public cacheItems: any[] = [];
 
   /**
+   * Currently selected items.
+   */
+  public selectedCacheItems: string[] = [];
+
+  /**
    * Creates an instance of your component.
    * 
    * @param cacheService Needed to read, remove and purge cache
@@ -34,19 +39,49 @@ export class ConfigCacheComponent implements OnInit {
   /**
    * Implementation of OnInit.
    */
-  ngOnInit() {
+  public ngOnInit() {
     this.cacheService.list().subscribe((items: any[]) => {
 
       // Dynamically building our dataset.
       const arr = [];
       for(const idx in items) {
-        arr.push(idx);
+        arr.push({
+          key: idx,
+          value: items[idx]
+        });
       }
-      this.cacheItems = arr.map(x => {
-        return {
-          key: x
-        };
-      });
+      this.cacheItems = arr;
     });
+  }
+
+  /**
+   * Toggles the details view for a single cache item.
+   * 
+   * @param item Cache item to toggle details for
+   */
+  public toggleDetails(item: any) {
+
+    // Checking if we're already displaying details for current item.
+    const idx = this.selectedCacheItems.indexOf(item.key);
+    if (idx !== -1) {
+
+      // Hiding item.
+      this.selectedCacheItems.splice(idx, 1);
+    } else {
+
+      // Displaying item.
+      this.selectedCacheItems.push(item.key);
+    }
+  }
+
+  /**
+   * Returns true if we should display the details view for specified cache item.
+   * 
+   * @param item Item to check if we should display details for
+   */
+  public shouldDisplayDetails(item: any) {
+
+    // Returns true if we're currently displaying this particular item.
+    return this.selectedCacheItems.filter(x => x === item.key).length > 0;
   }
 }
