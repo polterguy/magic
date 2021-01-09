963cee16f2b8f475e6b564593069ee0f9931766b
Thomas Hansen <thomas@gaiasoul.com>
2021-01-09 09:12:03 +0200

    Started working on endpoints

--- frontend2/src/app/components/endpoints/endpoints.component.ts
+++ frontend2/src/app/components/endpoints/endpoints.component.ts
@@ -6,14 +6,56 @@
 // Angular and system imports.
 import { Component, OnInit } from '@angular/core';
 
+// Application specific imports.
+import { Endpoint } from 'src/app/models/endpoint.model';
+import { EndpointService } from './services/endpoint.service';
+
+/**
+ * Endpoints component allowing user to see his endpoints, and
+ * meta data about his endpoints, in addition to invoking endpoints
+ * and see the result of the invocation.
+ */
 @Component({
   selector: 'app-endpoints',
   templateUrl: './endpoints.component.html'
 })
 export class EndpointsComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Model describing endpoints in your installation.
+   */
+  public endpoints: Endpoint[];
 
-  ngOnInit() {
+  /**
+   * Creates an instance of your component.
+   * 
+   * @param endpointService Endpoint service required to retrieve meta information about endpoints, and invoke them generically
+   */
+  constructor(private endpointService: EndpointService) { }
+
+  /**
+   * Implementation of OnInit.
+   */
+  public ngOnInit() {
+
+    // Retrieving endpoints initially.
+    this.getEndpoints();
+  }
+
+  /*
+   * Private helper methods.
+   */
+
+  /*
+   * Invokes backend to retrieve meta data about endpoints.
+   */
+  private getEndpoints() {
+
+    // Invoking backend to retrieve endpoints.
+    this.endpointService.endpoints().subscribe((endpoints: Endpoint[]) => {
+
+      // Assigning model to result of invocation.
+      this.endpoints = endpoints;
+    });
   }
 }
