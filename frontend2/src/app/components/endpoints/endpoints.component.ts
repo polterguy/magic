e9787575630effe859a7382fbc1d9ac3416fe3e6
Thomas Hansen <thomas@gaiasoul.com>
2021-01-09 09:34:24 +0200

    Started working on showing grid of endpoints

--- frontend2/src/app/components/endpoints/endpoints.component.ts
+++ frontend2/src/app/components/endpoints/endpoints.component.ts
@@ -4,27 +4,44 @@
  */
 
 // Angular and system imports.
+import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 // Application specific imports.
 import { Endpoint } from 'src/app/models/endpoint.model';
 import { EndpointService } from './services/endpoint.service';
 
 /**
- * Endpoints component allowing user to see his endpoints, and
- * meta data about his endpoints, in addition to invoking endpoints
- * and see the result of the invocation.
+ * Endpoints component allowing user to see and invoke his endpoints.
  */
 @Component({
   selector: 'app-endpoints',
-  templateUrl: './endpoints.component.html'
+  templateUrl: './endpoints.component.html',
+  styleUrls: ['./endpoints.component.scss']
 })
 export class EndpointsComponent implements OnInit {
+
+  // List of log item IDs that we're currently viewing details for.
+  private displayDetails: string[] = [];
+
+  // Filter for which items to display.
+  private filter: string = '';
+
+  /**
+   * Columns to display in table.
+   */
+  public displayedColumns: string[] = ['path', 'verb'];
 
   /**
    * Model describing endpoints in your installation.
    */
   public endpoints: Endpoint[];
+
+  /**
+   * Filter form control for filtering log items to display.
+   */
+  public filterFormControl: FormControl;
 
   /**
    * Creates an instance of your component.
@@ -38,8 +55,70 @@ export class EndpointsComponent implements OnInit {
    */
   public ngOnInit() {
 
+    // Creating our filter form control, with debounce logic.
+    this.filterFormControl = new FormControl('');
+    this.filterFormControl.setValue('');
+    this.filterFormControl.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe((query: any) => {
+        this.displayDetails = [];
+        this.filter = query;
+      });
+
     // Retrieving endpoints initially.
     this.getEndpoints();
+  }
+
+  /**
+   * Returns items matching currently applied filter.
+   */
+  public filteredItems() {
+    if (this.filter === '') {
+      return this.endpoints;
+    } else {
+      return this.endpoints.filter(x => x.verb === this.filter || x.path.indexOf(this.filter) !== -1);
+    }
+  }
+
+  /**
+   * Clears the current filter.
+   */
+  public clearFilter() {
+
+    // Resetting filter form control's value.
+    this.displayDetails = [];
+    this.filterFormControl.setValue('');
+  }
+
+  /**
+   * Toggles details about one specific endpoint item.
+   * 
+   * @param el Log item to toggle details for
+   */
+  public toggleDetails(el: Endpoint) {
+
+    // Checking if we're already displaying details for current item.
+    const idx = this.displayDetails.indexOf(el.verb + el.path);
+    if (idx !== -1) {
+
+      // Hiding item.
+      this.displayDetails.splice(idx, 1);
+    } else {
+
+      // Displaying item.
+      this.displayDetails.push(el.verb + el.path);
+    }
+  }
+
+  /**
+   * Returns true if details for specified endpoint item should be displayed.
+   * 
+   * @param el Endpoint item to display details for
+   */
+  public shouldDisplayDetails(el: Endpoint) {
+
+    // Returns true if we're currently displaying this particular item.
+    return this.displayDetails.filter(x => x === el.verb + el.path).length > 0;
   }
 
   /*
