61054f54e74b4118c345b4de7b3750d24da4f95f
thomas@ainiro.io <thomas@gaiasoul.com>
2021-01-10 09:20:15 +0200

    Started working on parametrizing invocations

--- frontend2/src/app/components/endpoints/endpoint-details/endpoint-details.component.ts
+++ frontend2/src/app/components/endpoints/endpoint-details/endpoint-details.component.ts
@@ -4,13 +4,18 @@
  */
 
 // Angular and system imports.
-import { Component, Input } from '@angular/core';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { Component, Input, OnInit } from '@angular/core';
 
 // Application specific imports.
-import { Endpoint } from 'src/app/models/endpoint.model';
+import { Endpoint } from '../models/endpoint.model';
+import { Argument } from '../models/argument.model';
 import { EndpointService } from '../services/endpoint.service';
 import { BackendService } from 'src/app/services/backend.service';
+
+// CodeMirror options.
+import json from '../../codemirror/options/json.json'
+import { FeedbackService } from 'src/app/services/feedback.service';
 
 /**
  * Endpoint details component, showing information specific to a single
@@ -21,7 +26,24 @@ import { BackendService } from 'src/app/services/backend.service';
   templateUrl: './endpoint-details.component.html',
   styleUrls: ['./endpoint-details.component.scss']
 })
-export class EndpointDetailsComponent {
+export class EndpointDetailsComponent implements OnInit {
+
+  /**
+   * CodeMirror options object, taken from common settings.
+   */
+  public cmOptions = {
+    json: json,
+  };
+
+  /**
+   * Payload example for JSON type of endpoints.
+   */
+  public payload: string = null;
+
+  /**
+   * URL model for invoking endpoint.
+   */
+  public url: string = null;
 
   /**
    * Model for instance.
@@ -38,10 +60,50 @@ export class EndpointDetailsComponent {
   constructor(
     private clipboard: Clipboard,
     private backendService: BackendService,
+    private feedbackService: FeedbackService,
     private endpointService: EndpointService) { }
 
   /**
-   * Returns string representing authorization requirements for endpoint.
+   * Implementation of OnInit.
+   */
+  public ngOnInit() {
+
+    // Model for URL that invokes endpoint.
+    this.url = this.backendService.current.url + '/' + this.endpoint.path;
+
+    // Checking if this is a JSON payload or not.
+    if (this.endpoint.verb !== 'get' && this.endpoint.verb !== 'delete') {
+
+      // JSON payload type.
+      let payload = {};
+      for (var idx of this.endpoint.input) {
+        let type: any = idx.type;
+        switch (type) {
+
+          case "long":
+            type = 1;
+            break;
+
+          case "date":
+            type = new Date().toISOString();
+            break;
+
+          case "bool":
+            type = true;
+            break;
+
+          case "string":
+            type = "foo bar";
+            break;
+        }
+        payload[idx.name] = type;
+      }
+      this.payload = JSON.stringify(payload, null, 2);
+    }
+  }
+
+  /**
+   * Returns string representation of authorization requirements for endpoint.
    * 
    * @param auth List of roles
    */
@@ -56,5 +118,28 @@ export class EndpointDetailsComponent {
 
     // Copies the currently edited endpoint's URL prepended by backend root URL.
     this.clipboard.copy(this.backendService.current.url + '/' + this.endpoint.path);
+  }
+
+  /**
+   * Invoked when a query parameter chip item is clicked.
+   */
+  public selectQueryParameter(arg: Argument) {
+
+    // Appending query parameter to URL.
+    if (this.url.indexOf('?') === -1) {
+
+      // First query parameter.
+      this.url += '?';
+    } else {
+
+      // Consecutive query parameters.
+      this.url += '&';
+    }
+
+    // Appending actual argument.
+    this.url += arg.name + '=';
+
+    // Creating a default value for argument.
+    //switch (arg.type)
   }
 }
