9b8852d786dfe2f9aab43f190e6082c69b6f9bd3
thomas@ainiro.io <thomas@gaiasoul.com>
2021-01-10 08:09:11 +0200

    Implementing copying of URL

--- frontend2/src/app/components/endpoints/endpoint-details/endpoint-details.component.ts
+++ frontend2/src/app/components/endpoints/endpoint-details/endpoint-details.component.ts
@@ -5,10 +5,12 @@
 
 // Angular and system imports.
 import { Component, Input } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 // Application specific imports.
 import { Endpoint } from 'src/app/models/endpoint.model';
 import { EndpointService } from '../services/endpoint.service';
+import { BackendService } from 'src/app/services/backend.service';
 
 /**
  * Endpoint details component, showing information specific to a single
@@ -29,9 +31,14 @@ export class EndpointDetailsComponent {
   /**
    * Creates an instance of your component.
    * 
+   * @param clipboard Needed to copy URL of endpoint
+   * @param backendService Needed to retrieve base root URL for backend
    * @param endpointService Needed to be able to invoke endpoint
    */
-  constructor(private endpointService: EndpointService) { }
+  constructor(
+    private clipboard: Clipboard,
+    private backendService: BackendService,
+    private endpointService: EndpointService) { }
 
   /**
    * Returns string representing authorization requirements for endpoint.
@@ -40,5 +47,14 @@ export class EndpointDetailsComponent {
    */
   public getAuth(auth: string[]) {
     return auth.join(', ');
+  }
+
+  /**
+   * Invoked when user wants to copy the full URL of the endpoint.
+   */
+  public copyUrl() {
+
+    // Copies the currently edited endpoint's URL prepended by backend root URL.
+    this.clipboard.copy(this.backendService.current.url + '/' + this.endpoint.path);
   }
 }
