b0e4ba798023ce65431590a75f3ab45a87ce536e
thomas@ainiro.io <thomas@gaiasoul.com>
2021-01-10 11:11:49 +0200

    Improved query parameter handling

--- frontend2/src/app/components/endpoints/endpoint-details/add-query-parameter-dialog/add-query-parameter-dialog.component.ts
+++ frontend2/src/app/components/endpoints/endpoint-details/add-query-parameter-dialog/add-query-parameter-dialog.component.ts
@@ -4,8 +4,11 @@
  */
 
 // Angular and system imports.
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+// Application specific imports.
+import { Argument } from '../../models/argument.model';
 
 /**
  * Dialog component for adding a query parameter to the URL of endpoint.
@@ -20,14 +23,40 @@ export class AddQueryParameterComponentDialog {
   /**
    * Model for value of query parameter.
    */
-  public value: string;
+  public value: any;
 
   /**
    * Creates an instance of your component.
    * 
    * @param dialogRef Needed to be able to close dialog
    */
-  constructor(private dialogRef: MatDialogRef<AddQueryParameterComponentDialog>) { }
+  constructor(
+    private dialogRef: MatDialogRef<AddQueryParameterComponentDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: Argument)
+  {
+    switch (data.type) {
+
+      case 'bool':
+        this.value = true;
+        break;
+
+      case 'string':
+        this.value = '';
+        break;
+
+      case 'long':
+      case 'int':
+      case 'uint':
+      case 'short':
+      case 'ushort':
+        this.value = 42;
+        break;
+
+      case 'date':
+        this.value = new Date().toISOString();
+        break;
+    }
+  }
 
   /**
    * Invoked when user wants to create a new query parameter value.
