4db3ff053736081ad58229140af0f1cd12fda8a0
thomas@ainiro.io <thomas@gaiasoul.com>
2021-01-25 10:14:29 +0200

    Updating Angular to version 11

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -52,7 +52,7 @@ const routes: Routes = [
  * Main module for application.
  */
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
