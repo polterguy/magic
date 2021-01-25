43f48caf1e160ad8edfbdd9c51b1a3a6b88074e3
thomas@ainiro.io <thomas@gaiasoul.com>
2021-01-25 08:50:40 +0200

    Updating template from Angular 9 to Angular 10

--- backend/files/misc/templates/angular-dark/templates/main/src/polyfills.ts
+++ backend/files/misc/templates/angular-dark/templates/main/src/polyfills.ts
@@ -2,9 +2,3 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-import 'hammerjs/hammer';
