dfaaf4a97621cea6be8896ebfb6725bcf9a36294
thomas@ainiro.io <thomas@gaiasoul.com>
2021-01-28 09:20:36 +0200

    Adding Sakila actor/movie count statistics

---
+++ backend/files/misc/mysql/templates/sakila-actor-movie-count.sql
@@ -0,0 +1,6 @@
+select concat(a.first_name, ' ', a.last_name) as name, count(*) as value
+  from actor a
+  inner join film_actor fa on fa.actor_id = a.actor_id
+  group by a.actor_id
+  order by value desc
+  limit 25
