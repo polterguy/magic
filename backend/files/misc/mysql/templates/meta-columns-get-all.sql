69fd509d388a8db820f6f5a923a2300f5a0ff22d
Thomas Hansen <thomas@gaiasoul.com>
2021-01-28 14:07:15 +0200

    Adding META SQL examples

---
+++ backend/files/misc/mysql/templates/meta-columns-get-all.sql
@@ -0,0 +1,8 @@
+
+/*
+ * Selects all columns from log_entries database table.
+ *
+ * Exchange "log_entries" with a different table, and the currently
+ * selected database, to find columns from other tables.
+ */
+describe log_entries
