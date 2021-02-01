44ca8094ccee49b9229ee9a232c5785d2dc5a73d
thomas@ainiro.io <thomas@gaiasoul.com>
2021-02-01 11:29:41 +0200

    Adding missing interface

--- backend/files/misc/templates/angular-dark/templates/main/src/app/services/http-service.ts
+++ backend/files/misc/templates/angular-dark/templates/main/src/app/services/http-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { ICrudEntity, ICrdEntity, ICrEntity, IREntity } from './interfaces/crud-interfaces'
+import { ICrudEntity, ICrdEntity, ICrEntity, IREntity, IRdEntity } from './interfaces/crud-interfaces'
 import { ILog } from './interfaces/log-interface'
 import { StatusResponse } from './models/status-response';
 import { CreateResponse } from './models/create-response';
