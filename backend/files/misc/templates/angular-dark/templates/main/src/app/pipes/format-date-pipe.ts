b07586f7f1770a3362a86f0deba703140c3672dc
thomas@ainiro.io <thomas@gaiasoul.com>
2021-02-08 16:39:10 +0200

    Fixing compiler error that only shows during AOT builds

--- backend/files/misc/templates/angular-dark/templates/main/src/app/pipes/format-date-pipe.ts
+++ backend/files/misc/templates/angular-dark/templates/main/src/app/pipes/format-date-pipe.ts
@@ -9,7 +9,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'formatDate'
 })
 export class FormatDatePipe implements PipeTransform {
-  transform(value: any, args: string[]): any {
+  transform(value: any): any {
     const date = new Date(value);
     return date.toLocaleString();
   }
