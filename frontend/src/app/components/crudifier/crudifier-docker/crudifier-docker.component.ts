24efe1a8576f0340742914ace0418940d9d7f4a0
thomas@ainiro.io <thomas@gaiasoul.com>
2021-02-15 13:58:47 +0200

    Sanity checking domains before generating template file

--- frontend/src/app/components/crudifier/crudifier-docker/crudifier-docker.component.ts
+++ frontend/src/app/components/crudifier/crudifier-docker/crudifier-docker.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 
 // Application specific imports.
 import { CrudifyService } from '../services/crudify.service';
+import { FeedbackService } from 'src/app/services/feedback.service';
 
 /**
  * Docker component to generate a docker-compose.yml template file, allowing you to deploy
@@ -35,12 +36,28 @@ export class CrudifierDockerComponent {
    * 
    * @param crudifierService Needed to actually generate your docker-compose file.
    */
-  constructor(private crudifierService: CrudifyService) { }
+  constructor(
+    private feedbackService: FeedbackService,
+    private crudifierService: CrudifyService) { }
 
   /**
    * Invoked when user wants to generate his docker-compose file.
    */
   public generate() {
+
+    // Sanity checking domain and api-domain before invoking backend.
+    for (let idxDomain of [this.domain, this.apiDomain]) {
+      for (let idx = 0; idx < idxDomain.length; idx++) {
+        if ("abcdefghijklmnopqrstuvwxyz-_.1234567890".indexOf(idxDomain[idx]) === -1) {
+          this.feedbackService.showError("Only alphanumeric characters in lowercase and '.', '-' or '_' are allowed");
+          return;
+        }
+      }
+    }
+    if (this.domain.indexOf('.') === -1 || this.apiDomain.indexOf('.') === -1) {
+      this.feedbackService.showError("A domain needs to have at least one '.' in it to be considered valid");
+      return;
+    }
     
     // Invoking backend to generate docker-compose.yml file, downloaded to client.
     this.crudifierService.generateDockerComposeFile(this.domain, this.apiDomain);
