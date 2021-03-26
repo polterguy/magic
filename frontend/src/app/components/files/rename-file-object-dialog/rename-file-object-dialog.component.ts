d3f700a8c4798b31cc08b00ba4e01eaa3ac53f32
Thomas Hansen <thomas@gaiasoul.com>
2021-03-26 13:55:50 +0200

    Implementing renaming of file or folder

---
+++ frontend/src/app/components/files/rename-file-object-dialog/rename-file-object-dialog.component.ts
@@ -0,0 +1,55 @@
+
+/*
+ * Copyright(c) Thomas Hansen thomas@servergardens.com, all right reserved
+ */
+
+// Angular and system imports.
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+/**
+ * Helper class for passing parameters in and out of modal dialog.
+ */
+export class RenameFileObject {
+
+  /**
+   * If true, the user wants to rename a folder, otherwise a file.
+   */
+  isFolder: boolean;
+
+  /**
+   * Old name of file object user wants to rename.
+   */
+  path: string;
+
+  /**
+   * New name of file object
+   */
+  newName: string;
+}
+
+/**
+ * Component for renaming a file system object, either a folder or a file.
+ */
+@Component({
+  selector: 'app-rename-file-object-dialog',
+  templateUrl: './rename-file-object-dialog.component.html'
+})
+export class RenameFileObjectDialogComponent implements OnInit {
+
+  /**
+   * Creates an instance of your component.
+   * 
+   * @param data File object type and name.
+   */
+  constructor(@Inject(MAT_DIALOG_DATA) public data: RenameFileObject) { }
+
+  /**
+   * Implementation of OnInit.
+   */
+  public ngOnInit() {
+
+    const entities = this.data.path.split('/').filter(x => x !== '');
+    this.data.newName = entities[entities.length - 1];
+  }
+}
