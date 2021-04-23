117edcd533e52ba69d1f29fb6f0f33f369ba7b21
Thomas Hansen <thomas@gaiasoul.com>
2021-04-23 09:02:41 +0300

    Code formatting

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,21 +7,19 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-// CodeMirror imports for addons.
-import 'codemirror/addon/selection/active-line';
-
 // CodeMirror mode imports.
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/markdown/markdown';
-import 'codemirror/mode/shell/shell';
-import 'codemirror/mode/sass/sass';
-import 'codemirror/mode/yaml/yaml';
 import 'codemirror/mode/sql/sql';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/css/css';
+import 'codemirror/mode/sass/sass';
+import 'codemirror/mode/yaml/yaml';
+import 'codemirror/mode/shell/shell';
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/javascript/javascript';
 
-// Then addons.
+// CodeMirror addons.
 import 'codemirror/addon/display/fullscreen';
+import 'codemirror/addon/selection/active-line';
 
 // Application specific imports.
 import { AppModule } from './app/app.module';
