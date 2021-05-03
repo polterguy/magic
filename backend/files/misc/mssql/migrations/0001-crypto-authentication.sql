28ef4d25f553427c6c2a7915422a928de30fc36b
Thomas Hansen <thomas@gaiasoul.com>
2021-05-03 14:36:46 +0300

    Adding db migration scripts for users/crypt_keys association

---
+++ backend/files/misc/mssql/migrations/0001-crypto-authentication.sql
@@ -0,0 +1,46 @@
+
+/*
+ * This MS SQL script migrates the database to accommodate for the
+ * ability to associate a public cryptography key with a user record.
+ */
+use magic;
+
+
+/*
+ * Updating our database version.
+ */
+delete from magic_version;
+insert into magic_version(db_version) values ('009.001.007');
+
+go
+
+/*
+ * Creating association table between users and crypto_keys.
+ */
+create table users_crypto_keys (
+  username nvarchar(128) not null,
+  key_id int not null,
+  constraint pk_users_crypto_keys primary key clustered(username, key_id)
+)
+
+go
+
+/*
+ * Creating foreign key towards users.
+ */
+alter table users_crypto_keys
+  add foreign key (username)
+  references users(username)
+  on delete cascade;
+
+go
+
+/*
+ * Creating foreign key towards crypto_keys.
+ */
+alter table users_crypto_keys
+  add foreign key (key_id)
+  references crypto_keys(id)
+  on delete cascade;
+
+go
---
+++ backend/files/misc/mysql/migrations/0001-crypto-authentication.sql
@@ -0,0 +1,25 @@
+
+/*
+ * This MySQL script migrates the database to accommodate for the
+ * ability to associate a public cryptography key with a user record.
+ */
+use magic;
+
+
+/*
+ * Updating our database version.
+ */
+delete from magic_version;
+insert into magic_version(db_version) values ('009.001.007');
+
+
+/*
+ * Creating association table between users and crypto_keys.
+ */
+create table `users_crypto_keys` (
+  `username` varchar(256) not null,
+  `key_id` int(11) not null,
+  primary key (`username`, `key_id`),
+  constraint `username_fky` foreign key (`username`) references `users` (`username`) on delete cascade,
+  constraint `key_id_fky` foreign key (`key_id`) references `crypto_keys` (`id`) on delete cascade
+);
