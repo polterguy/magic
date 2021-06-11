0e84f8036cb6d7614b36262795cdff6f428f7107
Thomas Hansen <thomas@gaiasoul.com>
2021-06-11 12:17:19 +0300

    Adding not exists condition to SQL server migration script

--- backend/files/misc/mssql/migrations/0001-crypto-authentication.sql
+++ backend/files/misc/mssql/migrations/0001-crypto-authentication.sql
@@ -17,31 +17,31 @@ go
 /*
  * Creating association table between users and crypto_keys.
  */
-create table users_crypto_keys (
-  username nvarchar(128) not null,
-  key_id int not null,
-  unique(key_id),
-  constraint pk_users_crypto_keys primary key clustered(username, key_id)
-)
+if not exists (select * from sysobjects where name='users_crypto_keys' and xtype='U') begin
 
-go
+  create table users_crypto_keys (
+    username nvarchar(128) not null,
+    key_id int not null,
+    unique(key_id),
+    constraint pk_users_crypto_keys primary key clustered(username, key_id)
+  );
 
-/*
- * Creating foreign key towards users.
- */
-alter table users_crypto_keys
-  add foreign key (username)
-  references users(username)
-  on delete cascade;
+  /*
+   * Creating foreign key towards users.
+   */
+  alter table users_crypto_keys
+    add foreign key (username)
+    references users(username)
+    on delete cascade;
 
-go
+  /*
+   * Creating foreign key towards crypto_keys.
+   */
+  alter table users_crypto_keys
+    add foreign key (key_id)
+    references crypto_keys(id)
+    on delete cascade;
 
-/*
- * Creating foreign key towards crypto_keys.
- */
-alter table users_crypto_keys
-  add foreign key (key_id)
-  references crypto_keys(id)
-  on delete cascade;
+end
 
 go
