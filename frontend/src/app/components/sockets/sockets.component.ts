a46452d5dc42340d6ff54bc5be7a7b26f7611d95
thomas@ainiro.io <thomas@gaiasoul.com>
2021-06-12 16:36:43 +0300

    Started work on explicit Sockets menu item
    Creating more socket features

---
+++ frontend/src/app/components/sockets/sockets.component.ts
@@ -0,0 +1,14 @@
+
+// Angular specific imports.
+import { Component } from '@angular/core';
+
+/**
+ * Wrapper component for SignalR web socket connections.
+ */
+@Component({
+  selector: 'app-sockets',
+  templateUrl: './sockets.component.html',
+  styleUrls: ['./sockets.component.scss']
+})
+export class SocketsComponent {
+}
