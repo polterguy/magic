cdf2cf6a89b9bfa558a37ffc59a448b82eb8f165
Thomas Hansen <thomas@gaiasoul.com>
2021-06-15 14:25:31 +0300

    Making sure we fetch production URL from JSON file to more easily allow for transformations in CI pipelines

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,11 +2,15 @@
 /*
  * Copyright(c) Thomas Hansen thomas@servergardens.com, all right reserved
  */
+
+// This little trick makes pipeline transformations slightly simpler in Azure at least.
+import prod_url from "./environment.prod.json";
+
 export const environment = {
   production: true,
   defaultBackends: [
     {
-      url: 'http://localhost:4444',
+      url: prod_url.url,
       username: 'root',
       password: 'root',
      },
