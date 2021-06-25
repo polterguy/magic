39ed80cec21780bb64247fd788473c40a15224e6
thomas@ainiro.io <thomas@gaiasoul.com>
2021-06-25 08:51:45 +0300

    Supporting templates when creating new files in Hyper IDE

---
+++ backend/files/misc/ide/README.md
@@ -0,0 +1,4 @@
+
+# Helper files for the IDE component
+
+Contains helper files for Hyper IDE.
--- frontend/src/app/components/ide/new-file-folder-dialog/new-file-folder-dialog.component.ts
+++ frontend/src/app/components/ide/new-file-folder-dialog/new-file-folder-dialog.component.ts
@@ -4,11 +4,14 @@
  */
 
 // Angular and system imports.
-import { Component, Inject } from '@angular/core';
+import { ElementRef } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 
-// CodeMirror options according to file extensions.
-import fileTypes from '../../files/file-editor/file-types.json'
+// Application specific imports.
+import { Template } from '../../../models/template.model';
+import { FileService } from '../../files/services/file.service';
+import { FeedbackService } from 'src/app/services/feedback.service';
 
 /**
  * Helper class for passing parameters in and out of modal dialog.
@@ -31,6 +34,11 @@ export class FileObject {
   path: string;
 
   /**
+   * Template file's content when creating a file.
+   */
+  template: string;
+
+  /**
    * All existing folders in system.
    */
    folders: string[];
@@ -49,17 +57,69 @@ export class FileObject {
   templateUrl: './new-file-folder-dialog.component.html',
   styleUrls: ['./new-file-folder-dialog.component.scss']
 })
-export class NewFileFolderDialogComponent {
+export class NewFileFolderDialogComponent implements OnInit {
 
-  // Known file extensions we've got editors for.
-  private extensions = fileTypes;
+  /**
+   * Templates user can create file from.
+   */
+  public templates: Template[] = [];
+
+  /**
+   * Which template user has currently selected.
+   */
+  public activeTemplate: Template = null;
+
+  /**
+   * Element wrapping name input.
+   */
+  @ViewChild('fileName') fileName: ElementRef;
 
   /**
    * Creates an instance of your component.
    * 
-   * @param data File object type and name.
+   * @param fileService Needed to retrieve templates from backend
+   * @param feedbackService Needed to display errors and such to the user
+   * @param data File object type and name
    */
-  constructor(@Inject(MAT_DIALOG_DATA) public data: FileObject) { }
+  constructor(
+    private fileService: FileService,
+    private feedbackService: FeedbackService,
+    @Inject(MAT_DIALOG_DATA) public data: FileObject) { }
+
+  /**
+   * Implementation of OnInit.
+   */
+  public ngOnInit() {
+
+    // Invoking backend to retrieve templates.
+    this.fileService.listFiles('/misc/ide/templates/').subscribe((result: string[]) => {
+
+      // Assigning result to model.
+      this.templates = result.map(x => {
+        return {
+          name: x,
+        }
+      });
+    });
+  }
+
+  /**
+   * Invoked when selected template is changed.
+   */
+  public templateChanged() {
+
+    // Invoking backend to retrieve file's content.
+    this.fileService.loadFile(this.activeTemplate.name).subscribe((result: string) => {
+
+      // Assigning model.
+      this.data.template = result;
+      this.data.name = this.activeTemplate.name.substr(this.activeTemplate.name.lastIndexOf('/') + 1);
+
+      // For simplicity reasons we assign focus to file name input.
+      this.fileName.nativeElement.focus();
+
+    }, (error: any) => this.feedbackService.showError(error));
+  }
 
   /**
    * Returns true if the filename is valid, otherwise false.
