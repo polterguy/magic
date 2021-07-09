2a618211108a9e64c9c5da926289e63d5a21edc3
Thomas Hansen <thomas@gaiasoul.com>
2021-07-09 09:33:25 +0300

    Bumping version getting ready for release

--- backend/slots/Version.cs
+++ backend/slots/Version.cs
@@ -21,7 +21,7 @@ namespace magic.backend.slots
         /// <param name="input">Parameters passed from signaler</param>
         public void Signal(ISignaler signaler, Node input)
         {
-            input.Value = "9.4.9";
+            input.Value = "9.5.0";
         }
     }
 }
