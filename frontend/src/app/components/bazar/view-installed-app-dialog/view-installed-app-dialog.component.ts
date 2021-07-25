6c1c3ba337c5fe1f0a2ede17a484e58775408a4c
Thomas Hansen <thomas@gaiasoul.com>
2021-07-25 10:03:07 +0300

    Update view-installed-app-dialog.component.ts

--- frontend/src/app/components/bazar/view-installed-app-dialog/view-installed-app-dialog.component.ts
+++ frontend/src/app/components/bazar/view-installed-app-dialog/view-installed-app-dialog.component.ts
@@ -90,7 +90,7 @@ export class ViewInstalledAppDialogComponent implements OnInit {
           if (install.result === 'success') {
 
             // Application was successfully initialised.
-            this.feedbackService.showInfo('Application was successfully updated');
+            this.feedbackService.showInfo('Application was successfully updated. You probably want to store the ZIP file later.');
 
             // This will signal parent form that app was updated, triggering refreshing of manifests.
             this.dialogRef.close(this.data);
@@ -98,6 +98,9 @@ export class ViewInstalledAppDialogComponent implements OnInit {
         }, (error: any) => this.feedbackService.showError(error));
       }
     }, (error: any) => this.feedbackService.showError(error));
+
+    // Downloading module to local computer.
+    this.bazarService.downloadLocally(this.data.token);
   }
 
   /**
