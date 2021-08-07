30ef800abcb4f2b6c35a5099f896632864b5eb49
Thomas Hansen <thomas@gaiasoul.com>
2021-08-07 20:10:25 +0300

    Support for deleting old file in image upload endpoint

--- backend/files/misc/templates/angular/templates/main/src/app/services/http-service.ts
+++ backend/files/misc/templates/angular/templates/main/src/app/services/http-service.ts
@@ -106,14 +106,16 @@ export class HttpService {
    *
    * @param content Base64 encoded image data
    * @param type Type of image, such as 'jpeg', 'png', etc
+   * @param old_file Optional name of old file, which if existing and specified will be deleted
    */
-   public uploadImage(content: string, type: string) {
+   public uploadImage(content: string, type: string, old_file?: string) {
 
     // Invoking backend with a form data object containing file.
     // TODO: If you want to handle images, you'll need a backend endpoint, and change the URL below.
     return this.httpClient.put<any>('magic/modules/MODULE_NAME_HERE/upload-image', {
       content,
       type,
+      old_file,
     });
   }
 
