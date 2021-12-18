aba8d7500e0a8e176cbed9ab3ab2e0b43fd17371
Thomas Hansen <thomas@gaiasoul.com>
2021-12-18 12:34:25 +0200

    Using "Path.Combine" to make code more easily understood

--- backend/slots/ConfigLoad.cs
+++ backend/slots/ConfigLoad.cs
@@ -3,6 +3,7 @@
  */
 
 using System.IO;
+using System.Threading.Tasks;
 using magic.node;
 using magic.signals.contracts;
 
@@ -12,7 +13,7 @@ namespace magic.backend.slots
     /// [config.load] slot returning the entire contents of appsettings.json file raw to caller.
     /// </summary>
     [Slot(Name = "config.load")]
-    public class LoadAppSettings : ISlot
+    public class LoadAppSettings : ISlot, ISlotAsync
     {
         /// <summary>
         /// Implementation of signal
@@ -22,8 +23,24 @@ namespace magic.backend.slots
         public void Signal(ISignaler signaler, Node input)
         {
             input.Value = File.ReadAllText(
-                Directory.GetCurrentDirectory().Replace("\\", "/").TrimEnd('/') +
-                "/config/appsettings.json");
+                Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "config",
+                    "appsettings.json"));
+        }
+
+        /// <summary>
+        /// Implementation of signal
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal</param>
+        /// <param name="input">Parameters passed from signaler</param>
+        public async Task SignalAsync(ISignaler signaler, Node input)
+        {
+            input.Value = await File.ReadAllTextAsync(
+                Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "config",
+                    "appsettings.json"));
         }
     }
 }
