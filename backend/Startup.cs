6dbb3b731cc032a74d1f99a3b1d440610b3a47e2
thomas@ainiro.io <thomas@gaiasoul.com>
2021-12-21 14:50:59 +0200

    Cleanups

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -5,10 +5,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Server.IISIntegration;
 using magic.library;
-using magic.lambda.sockets;
-using magic.lambda.logging.helpers;
 
 namespace magic.backend
 {
@@ -30,13 +27,6 @@ namespace magic.backend
             services.AddSingleton<IConfiguration>(p => Configuration);
 
             /*
-             * Checking if we should add IIS authentication scheme, which we
-             * only do if configuration has declared an "auto login slot".
-             */
-            if (!string.IsNullOrEmpty(Configuration["magic:auth:auto-auth"]))
-                services.AddAuthentication(IISDefaults.AuthenticationScheme);
-
-            /*
              * Initializing Magic.
              *
              * Notice, must be done AFTER you invoke "AddControllers".
@@ -52,35 +42,6 @@ namespace magic.backend
              * Notice, must be done BEFORE you invoke "UseEndpoints".
              */
             app.UseMagic(Configuration);
-
-            app.UseHttpsRedirection();
-
-            // Applying CORS.
-            var origins = Configuration["magic:frontend:urls"];
-            if (!string.IsNullOrEmpty(origins))
-                app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(origins.Split(',')));
-            else
-                app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-
-            app.UseAuthentication();
-            app.UseRouting().UseEndpoints(conf =>
-            {
-                conf.MapControllers();
-
-                /*
-                 * Checking if SignalR is enabled, and if so, making sure we
-                 * resolve the "/sockets" endpoint as SignalR invocations.
-                 */
-                if (Configuration["magic:sockets:url"] != null)
-                    conf.MapHub<MagicHub>("/sockets");
-            });
-
-            // Creating a log entry for having started application, but only if system has beeen setup.
-            if (Configuration["magic:auth:secret"] != "THIS-IS-NOT-A-GOOD-SECRET-PLEASE-CHANGE-IT")
-            {
-                var logger = app.ApplicationServices.GetService(typeof(ILogger)) as ILogger;
-                logger.Info("Magic was successfully started");
-            }
         }
     }
 }
