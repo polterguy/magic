83ddbe9161e24a0e74ed8941a796165f8d2fec16
thomas@ainiro.io <thomas@gaiasoul.com>
2021-12-21 15:29:39 +0200

    Cleanups

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -5,42 +5,31 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Server.IISIntegration;
 using magic.library;
 
 namespace magic.backend
 {
     public class Startup
     {
-        public Startup()
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder();
-            builder.AddJsonFile("config/appsettings.json");
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
         IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(Configuration);
-            services.AddControllers().AddNewtonsoftJson();
             services.AddSingleton<IConfiguration>(p => Configuration);
 
-            /*
-             * Initializing Magic.
-             *
-             * Notice, must be done AFTER you invoke "AddControllers".
-             */
+            // Initializing Magic.
             services.AddMagic(Configuration);
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            /*
-             * Initializing Magic.
-             *
-             * Notice, must be done BEFORE you invoke "UseEndpoints".
-             */
+            //Initializing Magic.
             app.UseMagic(Configuration);
         }
     }
