17ca409935b734a6f140d92cd8626311cec4407c
Thomas Hansen <thomas@gaiasoul.com>
2022-03-04 07:18:33 +0200

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,28 +8,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Application specific imports.
+import { AccessGuard } from './access.guard';
 import { IdeComponent } from './components/tools/ide/ide.component';
-import { LogComponent } from './components/analytics/log/log.component';
-import { SqlComponent } from './components/tools/sql/sql.component';
-import { AuthComponent } from './components/management/auth/auth.component';
-import { TasksComponent } from './components/tools/tasks/tasks.component';
-import { BazarComponent } from './components/management/bazar/bazar.component';
 import { AboutComponent } from './components/about/about.component';
+import { SqlComponent } from './components/tools/sql/sql.component';
+import { LogComponent } from './components/analytics/log/log.component';
+import { TasksComponent } from './components/tools/tasks/tasks.component';
+import { AuthComponent } from './components/management/auth/auth.component';
+import { BazarComponent } from './components/management/bazar/bazar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ConfigComponent } from './components/management/config/config.component';
 import { CryptoComponent } from './components/management/crypto/crypto.component';
-import { ProfileComponent } from './components/management/profile/profile.component';
 import { TerminalComponent } from './components/tools/terminal/terminal.component';
+import { SocketsComponent } from './components/analytics/sockets/sockets.component';
+import { ProfileComponent } from './components/management/profile/profile.component';
 import { CrudifierComponent } from './components/tools/crudifier/crudifier.component';
-import { EndpointsComponent } from './components/analytics/endpoints/endpoints.component';
 import { EvaluatorComponent } from './components/tools/evaluator/evaluator.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DiagnosticsCacheComponent } from './components/analytics/cache/cache.component';
+import { EndpointsComponent } from './components/analytics/endpoints/endpoints.component';
 import { RegisterComponent } from './components/management/auth/register/register.component';
+import { DiagnosticsTestsComponent } from './components/analytics/assumptions/assumptions.component';
 import { ConfirmEmailComponent } from './components/management/auth/confirm-email/confirm-email.component';
 import { ChangePasswordComponent } from './components/management/auth/change-password/change-password.component';
-import { DiagnosticsTestsComponent } from './components/analytics/assumptions/assumptions.component';
-import { DiagnosticsCacheComponent } from './components/analytics/cache/cache.component';
-import { SocketsComponent } from './components/analytics/sockets/sockets.component';
-import { AccessGuard } from './access.guard';
 
 /**
  * Routes for application.
