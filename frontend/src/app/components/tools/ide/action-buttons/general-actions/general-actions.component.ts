b6b33d715d808327785371cb5d39d9814187300e
thomas@ainiro.io <thomas@gaiasoul.com>
2022-03-09 07:25:14 +0200

    Ensuring we're closing Crudifier when done

--- frontend/src/app/components/tools/ide/action-buttons/general-actions/general-actions.component.ts
+++ frontend/src/app/components/tools/ide/action-buttons/general-actions/general-actions.component.ts
@@ -1,16 +1,30 @@
-import { Component, EventEmitter, NgZone, OnInit, Output } from '@angular/core';
+
+/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+// Angular and system imports.
+import { Subscription } from 'rxjs/internal/Subscription';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Component, EventEmitter, NgZone, OnDestroy, OnInit, Output } from '@angular/core';
+
+// Application specific imports.
+import { Message } from 'src/app/models/message.model';
 import { AuthService } from 'src/app/services/auth.service';
-import { FeedbackService } from 'src/app/services/feedback.service';
+import { MessageService } from 'src/app/services/message.service';
 import { FileService } from 'src/app/services/tools/file.service';
+import { FeedbackService } from 'src/app/services/feedback.service';
 import { GenerateCrudAppComponent } from '../../generate-crud-app/generate-crud-app.component';
 
+/**
+ * General action button component for executing general actions in Hyper IDE.
+ */
 @Component({
   selector: 'app-general-actions',
   templateUrl: './general-actions.component.html',
   styleUrls: ['./general-actions.component.scss']
 })
-export class GeneralActionsComponent implements OnInit {
+export class GeneralActionsComponent implements OnInit, OnDestroy {
 
   @Output() updateFiles: EventEmitter<any> = new EventEmitter();
 
@@ -22,7 +36,12 @@ export class GeneralActionsComponent implements OnInit {
   /**
    * Needed to be kept around such that we can explicitly close it after having CRUDified some database/table.
    */
-   private generateCrudDialog: MatDialogRef<GenerateCrudAppComponent> = null;
+  private generateCrudDialog: MatDialogRef<GenerateCrudAppComponent> = null;
+
+  /*
+   * Subscription allowing us to subscribe to messages relevant for component.
+   */
+  private subscription: Subscription;
 
   /**
    * Creates an instance of your component.
@@ -30,19 +49,64 @@ export class GeneralActionsComponent implements OnInit {
    * @param dialog Needed to create modal dialogs
    * @param authService Needed to verify access to components
    * @param fileService Needed to load and save files.
+   * @param messageService Needed to subscribe to relevant messages published by other components
    * @param feedbackService Needed to display feedback to user
-   * @param evaluatorService Needed to retrieve vocabulary from backend, in addition to executing Hyperlambda files
    * @param ngZone Needed to make sure dialogs popup inside the ngZone
    */
    constructor(
     private dialog: MatDialog,
     public authService: AuthService,
     private fileService: FileService,
+    private messageService: MessageService,
     private feedbackService: FeedbackService,
     readonly ngZone: NgZone
   ) { }
 
-  ngOnInit(): void {
+  /**
+   * Implementation of OnInit
+   */
+  public ngOnInit() {
+
+    /*
+     * Subscribing to relevant messages.
+     * Notice, this is necessary to make sure we're able to close modal Crudifier dialog
+     * once the user performs some sort of "crudify action".
+     */
+    this.subscription = this.messageService.subscriber().subscribe((msg: Message) => {
+
+      if (msg.name === 'magic.folders.update') {
+
+        // Closing dialog if it is open.
+        if (this.generateCrudDialog) {
+          this.generateCrudDialog.close();
+        }
+
+      } else if (msg.name === 'magic.crudifier.frontend-generated-locally') {
+
+        // Closing dialog if it is open.
+        if (this.generateCrudDialog) {
+          this.generateCrudDialog.close();
+        }
+
+      } else if (msg.name === 'magic.crudifier.frontend-generated') {
+
+        // Closing dialog if it is open.
+        if (this.generateCrudDialog) {
+          this.generateCrudDialog.close();
+        }
+      }
+    });
+  }
+
+  /**
+   * Implementation of OnDestroy.
+   */
+   public ngOnDestroy() {
+
+    // Making sure we unsubscribe to our subscription.
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   /**
