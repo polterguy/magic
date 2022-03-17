84acf7fb87a311d0ae80cc635072b0d2428cf83f
Thomas Hansen <thomas@gaiasoul.com>
2022-03-17 18:11:16 +0200

    Cleaning up cache

--- frontend/src/app/components/analytics/cache/cache.component.ts
+++ frontend/src/app/components/analytics/cache/cache.component.ts
@@ -15,9 +15,9 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 import { Count } from 'src/app/models/count.model';
 import { CacheItem } from '../../../models/cache-item.model';
 import { AuthService } from '../../../services/auth.service';
+import { BackendService } from 'src/app/services/backend.service';
 import { FeedbackService } from 'src/app/services/feedback.service';
 import { CacheService } from 'src/app/services/analytics/cache.service';
-import { BackendService } from 'src/app/services/backend.service';
 
 /**
  * Cache component for allowing user to inspect, remove, and purge cache items.
@@ -39,28 +39,23 @@ export class DiagnosticsCacheComponent implements OnInit {
   /**
    * Filter for what items to display.
    */
-  public filter: any = {};
+  filter: any = {};
 
   /**
    * List of cache items as returned from backend.
    */
-  public cacheItems: any[] = [];
-  public expandedElement: any;
+  cacheItems: any[] = [];
+  expandedElement: any;
 
   /**
    * Number of users matching filter in the backend.
    */
-  public count: number = 0;
-
-  /**
-   * Currently selected items.
-   */
-  public selectedCacheItems: string[] = [];
+  count: number = 0;
 
   /**
    * Filter form control for filtering users to display.
    */
-  public filterFormControl: FormControl;
+  filterFormControl: FormControl;
 
   /**
    * Paginator for paging table.
@@ -73,6 +68,7 @@ export class DiagnosticsCacheComponent implements OnInit {
    * @param clipboard Used to copy content of cache item to clipboard
    * @param authService Needed to verify user has access to components
    * @param cacheService Needed to read, remove and clear cache
+   * @param backendService Needed to figure out if user has access to specific actions related to the cache or not
    * @param feedbackService Needed to display feedback information to user
    */
   constructor(
@@ -85,9 +81,7 @@ export class DiagnosticsCacheComponent implements OnInit {
   /**
    * Implementation of OnInit.
    */
-  public ngOnInit() {
-
-    // Creating our filtering control.
+  ngOnInit() {
     this.filterFormControl = new FormControl('');
     this.filterFormControl.setValue('');
     this.filterFormControl.valueChanges
@@ -97,27 +91,18 @@ export class DiagnosticsCacheComponent implements OnInit {
         this.getItems();
       });
 
-    // Retrieving initial items from backend.
     this.getItems();
   }
 
   /**
    * Retrieves items from backend and databinds table.
    */
-  public getItems() {
-
-    // Updating filter value.
+  getItems() {
     this.filter.filter = this.filterFormControl.value;
     this.filter.offset = this.paginator.pageIndex * this.paginator.pageSize;
     this.filter.limit = this.paginator.pageSize;
-
-    // Invoking backend to retrieve cache items.
     this.cacheService.list(this.filter).subscribe((items: CacheItem[]) => {
-
-      // Assigning result of backend invocation to model.
       this.cacheItems = items || [];
-
-      // Invoking backend to count items matching filter.
       this.cacheService.count(this.filter.filter).subscribe((count: Count) => {
         this.count = count.count;
       });
@@ -127,48 +112,15 @@ export class DiagnosticsCacheComponent implements OnInit {
   /**
    * Invoked when cache items are paged.
    */
-  public paged() {
+  paged() {
     this.getItems();
   }
 
   /**
    * Invoked when user wants to clear current filter condition.
    */
-  public clearFilter() {
-
-    // Setting value of form control will automatically retrieve items.
+  clearFilter() {
     this.filterFormControl.setValue('');
-  }
-
-  /**
-   * Toggles the details view for a single cache item.
-   * 
-   * @param item Cache item to toggle details for
-   */
-  public toggleDetails(item: any) {
-
-    // Checking if we're already displaying details for current item.
-    const idx = this.selectedCacheItems.indexOf(item.key);
-    if (idx !== -1) {
-
-      // Hiding item.
-      this.selectedCacheItems.splice(idx, 1);
-    } else {
-
-      // Displaying item.
-      this.selectedCacheItems.push(item.key);
-    }
-  }
-
-  /**
-   * Returns true if we should display the details view for specified cache item.
-   * 
-   * @param item Item to check if we should display details for
-   */
-  public shouldDisplayDetails(item: any) {
-
-    // Returns true if we're currently displaying this particular item.
-    return this.selectedCacheItems.filter(x => x === item.key).length > 0;
   }
 
   /**
@@ -176,9 +128,7 @@ export class DiagnosticsCacheComponent implements OnInit {
    * 
    * @param value Value to copy
    */
-  public copyContent(value: string) {
-
-    // Copying specified string to clipboard and gives user some information.
+  copyContent(value: string) {
     this.clipboard.copy(value);
     this.feedbackService.showInfoShort('Value was copied to your clipboard');
   }
@@ -188,15 +138,9 @@ export class DiagnosticsCacheComponent implements OnInit {
    * 
    * @param id ID or key of item to delete
    */
-  public delete(event: any, id: string) {
-
-    // Making sure the event doesn't propagate upwards, which would trigger the row click event.
+  delete(event: any, id: string) {
     event.stopPropagation();
-
-    // Invoking backend to delete item.
     this.cacheService.delete(id).subscribe(() => {
-
-      // Showing some information to user, and re-databinding table.
       this.feedbackService.showInfoShort('Cache item deleted');
       this.getItems();
     });
@@ -205,9 +149,7 @@ export class DiagnosticsCacheComponent implements OnInit {
   /**
    * Purges cache completely, deleting all items from cache.
    */
-  public deleteAll() {
-
-    // Getting confirmation from user that he really wants to purge cache.
+  deleteAll() {
     this.feedbackService.confirm(
       'Please confirm action',
       `Are you sure you want to purge your cache? Purging your cache will delete ${this.count} items.`,
@@ -220,6 +162,6 @@ export class DiagnosticsCacheComponent implements OnInit {
           this.feedbackService.showInfoShort('Cache successfully purged');
           this.filterFormControl.setValue('');
         });
-      });
+    });
   }
 }
