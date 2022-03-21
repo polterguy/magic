510c04e394e00d8eca2cfbe56395baebdc48c7b9
thomas@ainiro.io <thomas@gaiasoul.com>
2022-03-21 15:14:49 +0200

    Update token.model.ts

--- frontend/src/app/models/token.model.ts
+++ frontend/src/app/models/token.model.ts
@@ -3,7 +3,7 @@
  * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
  */
 
-import { Buffer } from "Buffer";
+import { Buffer } from 'buffer';
 
 /**
  * Wrapper class for a JWT token with helper methods to calculate expiration, etc.
