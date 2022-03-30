c9cb6e2e8b2b67f6857767e56c8f94c3f7dc11d7
Thomas Hansen <thomas@gaiasoul.com>
2022-03-30 22:17:10 +0300

    Continuing on SQLite support

--- backend/files/misc/sqlite/magic.sql
+++ backend/files/misc/sqlite/magic.sql
@@ -14,11 +14,10 @@
  * Creating users table.
  */
 create table "users" (
-  "username" varchar(256) not null,
-  "password" varchar(256) not null,
+  "username" text not null primary key,
+  "password" text not null,
   "locked" boolean not null default false,
-  "created" timestamptz not null default now(),
-  primary key ("username")
+  "created" timestamptz not null default current_timestamp
 );
 
 
@@ -26,9 +25,8 @@ create table "users" (
  * Creating roles table.
  */
 create table "roles" (
-  "name" varchar(45) not null,
-  "description" varchar(256) null,
-  primary key ("name")
+  "name" text not null primary key,
+  "description" text null
 );
 
 
@@ -36,11 +34,11 @@ create table "roles" (
  * Creating association between roles and users through users_roles table.
  */
 create table "users_roles" (
-  "role" varchar(45) not null,
-  "user" varchar(256) not null,
-  primary key ("role", "user"),
+  "role" text not null,
+  "user" text not null,
   constraint "roles_fky" foreign key ("role") references "roles" ("name") on delete cascade,
-  constraint "users_fky" foreign key ("user") references "users" ("username") on delete cascade
+  constraint "users_fky" foreign key ("user") references "users" ("username") on delete cascade,
+  primary key("role", "user")
 );
 create index "users_roles_user_idx" on "users_roles" ("user");
 
@@ -65,11 +63,10 @@ insert into roles (name, description) values ('blocked', 'A user that has been b
  * Creating tasks table.
  */
 create table tasks (
-  id varchar(256) not null,
-  description varchar(1024) null,
+  id text not null primary key,
+  description text null,
   hyperlambda text not null,
-  created timestamptz not null default now(),
-  primary key (id)
+  created timestamptz not null default current_timestamp
 );
 create unique index "tasks_id_idx" on "tasks" ("id");
 
@@ -78,12 +75,11 @@ create unique index "tasks_id_idx" on "tasks" ("id");
  * Creating task_due table.
  */
 create table task_due (
-  id serial not null,
-  task varchar(256) not null,
+  id integer not null primary key autoincrement,
+  task text not null,
   due timestamptz not null,
-  repeats varchar(128) null,
-  constraint task_due_task_fky foreign key (task) references tasks (id) on delete cascade,
-  primary key (id)
+  repeats text null,
+  constraint task_due_task_fky foreign key (task) references tasks (id) on delete cascade
 );
 create index "task_due_task_idx" on "task_due" ("task");
 
@@ -97,13 +93,12 @@ create index "task_due_task_idx" on "task_due" ("task");
  * Creating log table.
  */
 create table log_entries (
-  id serial not null,
-  created timestamptz not null default now(),
-  type varchar(10) not null,
+  id integer not null primary key autoincrement,
+  created timestamptz not null default current_timestamp,
+  type text not null,
   content text not null,
   meta text null,
-  exception text null,
-  primary key (id)
+  exception text null
 );
 
 
@@ -116,17 +111,16 @@ create table log_entries (
  * Creating crypto_keys table for holding public cryptography keys.
  */
 create table crypto_keys (
-  id serial not null,
-  subject varchar(120) not null, /* Typically the name of the owner of the key */
-  email varchar(120) not null, /* Email address of owner */
-  domain varchar(250) null, /* The base URL of the subject */
-  type varchar(20) not null, /* Typically 'RSA' or something */
-  fingerprint varchar(120) not null, /* Public key's SHA256 value, in 'fingerprint' format */
-  content text not null, /* Actual public key */
-  vocabulary text not null, /* The vocabulary the key is allowed to evaluate */
-  imported timestamptz not null default now(),
-  enabled boolean not null default false, /* If true, the owner is allowed to invoke cryptographically secured endpoints */
-  primary key (id)
+  id integer primary key autoincrement not null,
+  subject text not null, 
+  email text not null, 
+  domain text null, 
+  type text not null, 
+  fingerprint text not null, 
+  content text not null, 
+  vocabulary text not null, 
+  imported timestamptz not null default current_timestamp,
+  enabled boolean not null default false
 );
 create index "crypto_keys_subject_idx" on "crypto_keys" ("subject");
 create index "crypto_keys_email_idx" on "crypto_keys" ("email");
@@ -137,14 +131,13 @@ create index "crypto_keys_email_idx" on "crypto_keys" ("email");
  * with some public key.
  */
 create table crypto_invocations (
-  id serial not null,
-  crypto_key integer not null, /* A reference to the crypto key associated with the evaluation */
-  request_id varchar(250) not null, /* The ID of the request - Ensures idempotency if caller specifies an ID */
-  request text not null, /* The request payload supplied by the caller */
-  request_raw text not null, /* The request payload supplied by the caller */
-  response text not null, /* The response payload returned to the caller */
-  created timestamptz not null default now(),
-  primary key (id),
+  id integer primary key autoincrement not null,
+  crypto_key integer not null,
+  request_id text not null,
+  request text not null,
+  request_raw text not null,
+  response text not null,
+  created timestamptz not null default current_timestamp,
   constraint "request_id_UNIQUE" unique ("request_id"),
   constraint "crypto_key_fky" foreign key ("crypto_key") references "crypto_keys" ("id") on delete cascade
 );
@@ -155,11 +148,11 @@ create index "crypto_invocations_crypto_key_idx" on "crypto_invocations" ("crypt
  * Creating association table between users and crypto_keys.
  */
 create table "users_crypto_keys" (
-  "username" varchar(256) not null,
+  "username" text not null,
   "key_id" integer not null,
-  primary key ("username", "key_id"),
   constraint "username_fky" foreign key ("username") references "users" ("username") on delete cascade,
-  constraint "key_id_fky" foreign key ("key_id") references "crypto_keys" ("id") on delete cascade
+  constraint "key_id_fky" foreign key ("key_id") references "crypto_keys" ("id") on delete cascade,
+  primary key("username", "key_id")
 );
 create index "users_crypto_keys_username_idx" on "users_crypto_keys" ("username");
 create index "users_crypto_keys_key_id_idx" on "users_crypto_keys" ("key_id");
