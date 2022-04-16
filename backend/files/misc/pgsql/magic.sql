ac285a45e49ada2323e1dbbfb18711e55200b8dd
Thomas Hansen <thomas@gaiasoul.com>
2022-04-16 11:00:39 +0300

    Adding users_extra table to Magic main database

--- backend/files/misc/pgsql/magic.sql
+++ backend/files/misc/pgsql/magic.sql
@@ -55,6 +55,18 @@ insert into roles (name, description) values ('reset-password', 'A special role 
 insert into roles (name, description) values ('unconfirmed', 'An unconfirmed user that has severely restricted access');
 insert into roles (name, description) values ('blocked', 'A user that has been blocked from site entirely');
 
+/*
+ * Creating users' extra table.
+ */
+create table "users_extra" (
+  "user" varchar(256) not null,
+  "type" varchar(45) not null,
+  "value" varchar(1024) not null,
+  primary key ("user", "type"),
+  constraint "users_extra_fky" foreign key ("user") references "users" ("username") on delete cascade
+);
+create index "users_extra_fky_idx" on "users_extra" ("user");
+
 
 /*
  * Tasks and task scheduler tables
--- backend/files/misc/sqlite/magic.sql
+++ backend/files/misc/sqlite/magic.sql
@@ -55,6 +55,19 @@ insert into roles (name, description) values ('blocked', 'A user that has been b
 
 
 /*
+ * Creating association between roles and users through users_roles table.
+ */
+create table "users_extra" (
+  "user" varchar(256) not null,
+  "type" varchar(45) not null,
+  "value" varchar(1024) not null,
+  constraint "users_extra_fky" foreign key ("user") references "users" ("username") on delete cascade,
+  primary key("user", "type")
+);
+create index "users_extra_user_idx" on "users_extra" ("user");
+
+
+/*
  * Tasks and task scheduler tables
  */
 
