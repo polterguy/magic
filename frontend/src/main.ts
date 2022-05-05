75c10a79987f47fcc3e80103de193a7bb7fe80ba
julia <julia@horusys.com>
2022-05-05 10:27:28 +0300

    pwaUpdate

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -32,8 +32,14 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+  platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
+    if ('serviceWorker' in navigator && environment.production) {
+      navigator.serviceWorker.register('ngsw-worker.js');
+    }
+  }).catch(err => console.log(err));
 
 
 RecaptchaComponent.prototype.ngOnDestroy = function() {
