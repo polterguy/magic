ce2831d5358727a5e0a92f4f7654b74f9c0f973d
julia <julia@horusys.com>
2022-05-11 06:21:02 +0300

    pwa update

--- frontend/src/app/services/update-pwa.service.ts
+++ frontend/src/app/services/update-pwa.service.ts
@@ -4,10 +4,10 @@
  */
 
 // Angular and system imports.
-import { Injectable } from '@angular/core';
+import { ApplicationRef, Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SwUpdate } from '@angular/service-worker';
-import { switchMap, interval } from 'rxjs';
+import { switchMap, interval, concat, first } from 'rxjs';
 import { PwaUpdateSnackbarComponent } from '../components/utilities/pwa-update-snackbar/pwa-update-snackbar.component';
 
 /**
@@ -19,30 +19,57 @@ import { PwaUpdateSnackbarComponent } from '../components/utilities/pwa-update-s
 export class UpdatePwaService {
 
   constructor(
+    appRef: ApplicationRef,
     private swUpdate: SwUpdate,
-    private snackbar: MatSnackBar
-  ) {
-    // if (swUpdate.isEnabled) {
-    //   interval(6 * 60 * 60).subscribe(() => swUpdate.checkForUpdate()
-    //     .then(() => { 
-    //       console.log('checking for updates'); 
-    //     }));
-    // }
+    private snackbar: MatSnackBar) {
+    if (swUpdate.isEnabled) {
+      // Allow the app to stabilize first, before starting
+      // polling for updates with `interval()`.
+      const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
+      const everySixHours$ = interval(6 * 60 * 60);
+      const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+      everySixHoursOnceAppIsStable$.subscribe(() => swUpdate.checkForUpdate());
+    }
   }
 
   public checkForUpdates(): void {
-    this.swUpdate.versionUpdates.subscribe(() => {
-      const snack = this.snackbar.openFromComponent(PwaUpdateSnackbarComponent, {
-        duration: -1
-      });
-      snack.afterDismissed().subscribe(res => {
-        return;
-      })
+    this.swUpdate.versionUpdates.subscribe(evt => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version`);
+          break;
+        case 'VERSION_READY':
+          // console.log(`Current app version: ${evt.currentVersion.hash}`);
+          // console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+
+          const snack = this.snackbar.openFromComponent(PwaUpdateSnackbarComponent, {
+            duration: -1
+          });
+          snack.afterDismissed().subscribe(res => {
+            return;
+          })
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(`Failed to install app version`);
+          break;
+      }
+    });
+
+
+
+    // this.swUpdate.versionUpdates.subscribe(() => {
+      // const snack = this.snackbar.openFromComponent(PwaUpdateSnackbarComponent, {
+      //   duration: -1
+      // });
+      // snack.afterDismissed().subscribe(res => {
+      //   return;
+      // })
       // snack.onAction().pipe(switchMap(() => this.swUpdate.activateUpdate())).subscribe((res) => {
       //   console.log(res)
       //   // this.reloadPage();
       // });
-    });
+    // });
   }
 
   // private reloadPage(): void {
