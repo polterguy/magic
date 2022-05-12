ec83265e881a0906b4a414262855ea7bc5b10f31
julia <julia@horusys.com>
2022-05-12 14:03:51 +0300

    pwa update test

--- frontend/src/app/components/utilities/pwa-update-dialog/pwa-update-dialog.component.ts
+++ frontend/src/app/components/utilities/pwa-update-dialog/pwa-update-dialog.component.ts
@@ -22,20 +22,20 @@ export class PwaUpdateDialogComponent {
   public reloadPage(): void {
     this.swUpdate.activateUpdate().then(() => {
       // caches.keys().then(cs=>cs.forEach(c=>caches.delete(c)));
-      // this.dialogRef.close('install');
-      document.location.reload();
+      this.dialogRef.close();
+      window.location.href = window.location.href;
     });
   }
 
   public remindLater() {
     this.dialogRef.close('later');
-    setTimeout(() => {
-      this.matDialog.open(PwaUpdateDialogComponent, {
-        position: {top: '7px'},
-        width: '500px',
-        panelClass: ['pwa-update-panel'],
-        hasBackdrop: false
-      })
-    }, 120000);
+    // setTimeout(() => {
+    //   this.matDialog.open(PwaUpdateDialogComponent, {
+    //     position: {top: '7px'},
+    //     width: '500px',
+    //     panelClass: ['pwa-update-panel'],
+    //     hasBackdrop: false
+    //   })
+    // }, 120000);
   }
 }
