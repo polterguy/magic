3c2a467ff8a5f6fe323795e6940a6c3f99324dd7
julia <julia@horusys.com>
2022-05-12 12:44:32 +0300

    pwa update test

--- frontend/src/app/components/utilities/pwa-update-dialog/pwa-update-dialog.component.ts
+++ frontend/src/app/components/utilities/pwa-update-dialog/pwa-update-dialog.component.ts
@@ -1,4 +1,11 @@
+
+/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-pwa-update-dialog',
@@ -7,9 +14,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PwaUpdateDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private swUpdate: SwUpdate,
+    private dialogRef: MatDialogRef<PwaUpdateDialogComponent>,
+    private matDialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  public reloadPage(): void {
+    this.swUpdate.activateUpdate().then(() => {
+      caches.keys().then(cs=>cs.forEach(c=>caches.delete(c)));
+      this.dialogRef.close('install');
+      document.location.reload();
+    });
+  }
+
+  public remindLater() {
+    this.dialogRef.close('later');
+    setTimeout(() => {
+      this.matDialog.open(PwaUpdateDialogComponent, {
+        position: {top: '7px'},
+        width: '500px',
+        panelClass: ['pwa-update-panel'],
+        hasBackdrop: false
+      })
+    }, 120000);
+  }
 }
