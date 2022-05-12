6f14110b1f4bbc2a90336fc33d75d1dfef57d091
julia <julia@horusys.com>
2022-05-12 11:14:49 +0300

    pwa update dialog

---
+++ frontend/src/app/components/utilities/pwa-update-dialog/pwa-update-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pwa-update-dialog',
+  templateUrl: './pwa-update-dialog.component.html',
+  styleUrls: ['./pwa-update-dialog.component.scss']
+})
+export class PwaUpdateDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
