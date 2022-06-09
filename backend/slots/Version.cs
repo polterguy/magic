d979ad3c52b503ad96a1f78125681d1eab6488a7
thomas@ainiro.io <th@aista.com>
2022-06-09 11:25:20 +0300

    Update Version.cs

--- backend/slots/Version.cs
+++ backend/slots/Version.cs
@@ -20,7 +20,7 @@ namespace magic.backend.slots
         /// <param name="input">Parameters passed from signaler</param>
         public void Signal(ISignaler signaler, Node input)
         {
-            input.Value = "v13.4.21";
+            input.Value = "v13.4.22";
         }
     }
 }
