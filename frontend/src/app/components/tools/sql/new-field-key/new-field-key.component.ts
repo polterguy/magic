d1c802d126dee941ec457f945b10e7f3e2cafe68
Mohsen Shafiey Kasrineh <mshafiey@users.noreply.github.com>
2022-07-15 17:48:04 +0300

    Revert "mssql/pgsql common datatype"
    This reverts commit ab1f46332c4c7956346325259cf5cc1e1145501c.

--- frontend/src/app/components/tools/sql/new-field-key/new-field-key.component.ts
+++ frontend/src/app/components/tools/sql/new-field-key/new-field-key.component.ts
@@ -82,22 +82,13 @@ export class NewFieldKeyComponent {
   private pgsqlDataTypes = [
     {name: 'Numeric', defaultValue: 'Numeric'},
     {name: 'text', defaultValue: 'string'},
-    {name: 'date', defaultValue: 'date'},
-    {name: 'datetime', defaultValue: 'datetime'},
-    {name: 'time', defaultValue: 'time'},
-    { name: 'blob', defaultValue: false },
+    {name: 'blob', defaultValue: false},
     {name: 'nvarchar', size: {min: 0, max: 65535}, defaultValue: false},
     {name: 'varchar', size: {min: 0, max: 65535}, defaultValue: 'string'},
   ];
   private mssqlDataTypes = [
     {name: 'integer', defaultValue: 'integer'},
-    {name: 'float', defaultValue: 'float'},
-    {name: 'real', defaultValue: 'real'},
-    {name: 'date', defaultValue: 'date'},
-    {name: 'datetime', defaultValue: 'datetime'},
-    {name: 'time', defaultValue: 'time'},
     {name: 'text', defaultValue: 'string'},
-    {name: 'ntext', defaultValue: 'string'},
     {name: 'blob', defaultValue: false},
     {name: 'nvarchar', size: {min: 0, max: 65535}, defaultValue: false},
     {name: 'varchar', size: {min: 0, max: 65535}, defaultValue: 'string'},
