d7292d5e77da628ccacd2cc09bfee6230cc0fc4b
thomas@ainiro.io <th@aista.com>
2022-08-12 13:49:55 +0300

    Better wizard

--- frontend/src/app/components/utilities/intro-guide/intro-guide.component.ts
+++ frontend/src/app/components/utilities/intro-guide/intro-guide.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
 
+/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+// Angular and system imports.
+import { Component } from '@angular/core';
+
+/**
+ * Wizard card help component to get started with Magic.
+ */
 @Component({
   selector: 'app-intro-guide',
   templateUrl: './intro-guide.component.html',
   styleUrls: ['./intro-guide.component.scss']
 })
-export class IntroGuideComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class IntroGuideComponent {
 }
