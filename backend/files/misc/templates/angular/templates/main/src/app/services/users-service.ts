526e4d282f1bfc6015dde3a3bbe0384f454d1b80
julia <julia@horusys.com>
2022-09-16 17:09:52 +0300

    user-management in backend folder

--- backend/files/misc/templates/angular/templates/main/src/app/services/users-service.ts
+++ backend/files/misc/templates/angular/templates/main/src/app/services/users-service.ts
@@ -6,10 +6,50 @@ import { environment } from 'src/environments/environment';
 /**
  * User model encapsulating a user from backend.
  */
-export class User {
+ export class User {
   username: string;
-  locked?: boolean;
+  locked?: number; 
   created?: Date;
+  role?: any; // In-app use only
+  extra?: any; // In-app use only
+}
+
+export class UpdateUser {
+  username: string;
+  locked?: boolean; 
+}
+
+/**
+ * Role model encapsulating a user's role from backend.
+ */
+export class Role {
+  user: string;
+  role: string
+}
+
+/**
+ * Extra details model encapsulating a user's Extra details from backend.
+ */
+export class Extra {
+  type: string;
+  value: string;
+  user: string
+}
+
+/**
+ * Extra details model encapsulating a user's Extra details from backend.
+ */
+export class Roles {
+  name: string;
+  description: string;
+}
+
+/**
+ * Create a new user's model.
+ */
+export class NewUser {
+  username: string;
+  password: string;
 }
 
 /**
@@ -28,9 +68,113 @@ export class UsersService {
    */
   constructor(private httpClient: HttpClient) { }
 
-  users() {
+  /**
+   * Invokes users's endpoint to fetch the list of users.
+   * @param params QueryParameters.
+   * @returns List of users.
+   */
+  users(params: string) {
     return this.httpClient.get<User[]>(
       environment.apiUrl +
-      'magic/modules/magic/users');
+      'magic/modules/magic/users' + params);
   }
+
+  /**
+   * Invokes users's endpoint to update a user.
+   * @param params data.
+   * @returns Result of the operation.
+   */
+  updateUser(data: UpdateUser) {
+    return this.httpClient.put<UpdateUser>(
+      environment.apiUrl +
+      'magic/modules/magic/users', data);
+  }
+
+  /**
+   * Invokes users's endpoint to create a new user.
+   * @param params data.
+   * @returns Result of the operation.
+   */
+  createUser(data: NewUser) {
+    return this.httpClient.post<NewUser>(
+      environment.apiUrl +
+      'magic/modules/magic/users', data);
+  }
+
+  /**
+   * Invokes users_count's endpoint to get the total number of users.
+   * @param params queryParameters.
+   * @returns Count of the users.
+   */
+  usersCount(params: string) {
+    return this.httpClient.get<any>(
+      environment.apiUrl +
+      'magic/modules/magic/users-count' + params);
+  }
+
+  /**
+   * Invokes users's endpoint to delete a user.
+   * @param params queryParameters.
+   * @returns Result of the operation.
+   */
+  deleteUser(params: string) {
+    return this.httpClient.delete<User>(
+      environment.apiUrl +
+      'magic/modules/magic/users' + params);
+  }
+
+  /**
+   * Invokes users_roles' endpoint to get all roles assigned to the user.
+   * @param params queryParameters.
+   * @returns list of roles assigned to the user.
+   */
+  getUserRole(params: string) {
+    return this.httpClient.get<Role[]>(
+      environment.apiUrl +
+      'magic/modules/magic/users_roles' + params);
+  }
+
+  /**
+   * Invokes users_roles' endpoint to add a new role.
+   * @param params data.
+   * @returns Result of the operation.
+   */
+  addUserRole(data: Role) {
+    return this.httpClient.post<any>(
+      environment.apiUrl +
+      'magic/modules/magic/users_roles', data);
+  }
+
+  /**
+   * Invokes users_roles' endpoint to remove a role from the user's roles.
+   * @param params queryParameters.
+   * @returns Result of the operation.
+   */
+  removeUserRole(params: string) {
+    return this.httpClient.delete<any>(
+      environment.apiUrl +
+      'magic/modules/magic/users_roles' + params);
+  }
+
+  /**
+   * Invokes users_extra's endpoint to get all details stored in extra field.
+   * @param params queryParameters.
+   * @returns List of extra details.
+   */
+  getUserExtras(params: string) {
+    return this.httpClient.get<Extra[]>(
+      environment.apiUrl +
+      'magic/modules/magic/users_extra' + params);
+  }
+
+  /**
+   * Invokes roles's endpoint to get the list of all available roles.
+   * @returns List of roles.
+   */
+  getRoles() {
+    return this.httpClient.get<Roles[]>(
+      environment.apiUrl +
+      'magic/modules/magic/roles');
+  }
+
 }
