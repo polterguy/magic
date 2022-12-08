19f994448db52e6439a7583bf29912418b146557
Aista, Ltd <th@aista.com>
2022-12-08 13:44:46 +0200

    Adding theme support

--- frontend/src/app/_protected/pages/dashboard/components/overview/overview.component.ts
+++ frontend/src/app/_protected/pages/dashboard/components/overview/overview.component.ts
@@ -4,21 +4,28 @@
 
 import { Component, Input, OnInit } from '@angular/core';
 import { SystemReport } from 'src/app/models/dashboard.model';
+import { ThemeService } from 'src/app/_general/services/theme.service';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent {
+
+  // Available themes.
+  themes: string[] = [
+    'default',
+    'neomorphism'
+  ];
+
+  // Selected theme.
+  public selectedTheme: string = null;
 
   /**
    * Data coming from the parent component.
    */
   @Input() data: SystemReport;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(public themeService: ThemeService) { }
 }
