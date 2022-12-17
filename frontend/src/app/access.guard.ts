ace7b9f1ddfae9ff7d934072b425496b020f71e3
Aista, Ltd <th@aista.com>
2022-12-17 09:04:26 +0200

    Removing promise

--- frontend/src/app/access.guard.ts
+++ frontend/src/app/access.guard.ts
@@ -40,10 +40,7 @@ export class AccessGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    console.log('CanActivate called');
     return (async () => {
-      while ((this.backendService?.active?.access && Object.keys(this.backendService?.active?.access?.auth ?? {}).length === 0))
-        await new Promise(resolve => setTimeout(resolve, 100));
       if (this.backendService?.active?.access && Object.keys(this.backendService?.active?.access?.auth).length) {
         const notAuthorized: boolean = Object.values(this.backendService.active.access.auth).every((item: any) => { return item === false })
 
@@ -55,6 +52,8 @@ export class AccessGuard implements CanActivate {
         } else {
           return true
         }
+      } else {
+        return false;
       }
     })();
   }
