7a6fa046b86fd00f8e217012fc0c1c68614dc3df
Aista, Ltd <th@aista.com>
2022-12-28 08:34:44 +0200

    Subscribing to backend socket messages

--- frontend/src/app/_layout/core/core.component.ts
+++ frontend/src/app/_layout/core/core.component.ts
@@ -3,20 +3,23 @@
  * Copyright (c) Aista Ltd, 2021 - 2022 info@aista.com, all rights reserved.
  */
 
-import { ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { UpdatePwaService } from 'src/app/_general/services/update-pwa.service';
 import { GeneralService } from 'src/app/_general/services/general.service';
 import { BackendService } from 'src/app/_general/services/backend.service';
+import { HttpTransportType, HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
 
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html'
 })
-export class CoreComponent implements OnInit {
-
-  public largeScreen!: boolean;
+export class CoreComponent implements OnInit, OnDestroy {
 
   private largeScreenStatus!: boolean;
+
+  largeScreen!: boolean;
+  hubConnection: HubConnection;
+
   // Needed to be able to figure out if we're on a small screen or not.
   @HostListener('window:resize', ['$event'])
   private onWindowResize() {
@@ -50,5 +53,32 @@ export class CoreComponent implements OnInit {
     this.backendService.versionRetrieved.subscribe((res: any) => {
       this.cdr.detectChanges();
     });
+
+    // Creating socket channel
+    let builder = new HubConnectionBuilder();
+    this.hubConnection = builder.withUrl(this.backendService.active.url + '/sockets', {
+      accessTokenFactory: () => this.backendService.active.token.token,
+      skipNegotiation: true,
+      transport: HttpTransportType.WebSockets,
+    }).build();
+
+    this.hubConnection.start();
+    this.hubConnection.on('magic.backend.message', (args) => {
+      args = JSON.parse(args);
+      switch (args.type) {
+
+        case 'success':
+          this.generalService.showFeedback(args.message, 'successMessage', 'OK');
+          break;
+
+        case 'error':
+          this.generalService.showFeedback(args.message, 'errorMessage', 'OK');
+          break;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.hubConnection?.stop();
   }
 }
