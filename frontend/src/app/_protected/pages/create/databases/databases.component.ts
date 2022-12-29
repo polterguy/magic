0a681b996707139c854b88dccf7bf64c8250841f
thomas@ainiro.io <th@aista.com>
2022-12-29 10:55:19 +0200

    Further cleanups

--- frontend/src/app/_protected/pages/create/databases/databases.component.ts
+++ frontend/src/app/_protected/pages/create/databases/databases.component.ts
@@ -3,10 +3,7 @@
  * Copyright (c) Aista Ltd, 2021 - 2022 info@aista.com, all rights reserved.
  */
 
-import { Component, OnInit } from '@angular/core';
-import { DefaultDatabaseType } from 'src/app/_general/models/default-database-type.model';
-import { GeneralService } from 'src/app/_general/services/general.service';
-import { SqlService } from 'src/app/_general/services/sql.service';
+import { Component } from '@angular/core';
 
 /**
  * Primary databases component allowing user to create a new database,
@@ -16,24 +13,5 @@ import { SqlService } from 'src/app/_general/services/sql.service';
   selector: 'app-databases',
   templateUrl: './databases.component.html'
 })
-export class DatabasesComponent implements OnInit{
-
-  databaseTypes: DefaultDatabaseType = null;
-
-  constructor(
-    private sqlService: SqlService,
-    private generalService: GeneralService) { }
-
-  ngOnInit() {
-    this.getDefaultDbType();
-  }
-
-  private getDefaultDbType() {
-    this.sqlService.defaultDatabaseType().subscribe({
-      next: (result: DefaultDatabaseType) => {
-        this.databaseTypes = result;
-      },
-      error: (error: any) => this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage')
-    });
-  }
+export class DatabasesComponent {
 }
