67f33d0a7f9c03c7c1a1e3b0dce416ad2e462a20
Aista, Ltd <th@aista.com>
2022-12-29 15:33:46 +0200

    More refactoring

--- frontend/src/app/_protected/pages/create/endpoint-generator/generator-base.ts
+++ frontend/src/app/_protected/pages/create/endpoint-generator/generator-base.ts
@@ -34,9 +34,45 @@ export abstract class GeneratorBase {
     protected activatedRoute: ActivatedRoute,
     protected sqlService: SqlService) { }
 
-  /**
-   * Initialises component.
+  getConnectionString(init: boolean = false) {
+    this.isLoading = true;
+    this.sqlService.connectionStrings(this.selectedDbType).subscribe({
+      next: (connectionStrings: any) => {
+        this.connectionStrings = Object.keys(connectionStrings || {});
+        if (!init) {
+          this.selectedConnectionString = this.connectionStrings[0];
+        }
+        this.getDatabases(init);
+      },
+      error: (error: any) => this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage', 'Ok', 5000)
+    });
+  }
+
+  getDatabases(init: boolean = false) {
+    this.isLoading = true;
+    this.sqlService.getDatabaseMetaInfo(
+      this.selectedDbType,
+      this.selectedConnectionString).subscribe({
+        next: (res: Databases) => {
+          this.databases = res.databases || [];
+          if (this.databases.length > 0) {
+            if (!init) {
+              this.selectedDatabase = this.databases[0].name;
+            }
+            this.databaseLoaded();
+          }
+          this.isLoading = false;
+        },
+        error: (error: any) => this.generalService.showFeedback(error?.error?.message, 'errorMessage', 'Ok', 5000)
+      });
+  }
+
+  /*
+   * Protected and abstract methods.
    */
+
+  protected abstract databaseLoaded(): void;
+
   protected init() {
 
     // Checking if we've got query parameters.
@@ -77,50 +113,10 @@ export abstract class GeneratorBase {
     });
   }
 
-  /**
-   * Retrieves the connection strings of the active database type.
-   */
-  public getConnectionString(init: boolean = false) {
-    this.isLoading = true;
-    this.sqlService.connectionStrings(this.selectedDbType).subscribe({
-      next: (connectionStrings: any) => {
-        this.connectionStrings = Object.keys(connectionStrings || {});
-        if (!init) {
-          this.selectedConnectionString = this.connectionStrings[0];
-        }
-        this.getDatabases(init);
-      },
-      error: (error: any) => this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage', 'Ok', 5000)
-    });
-  }
-
-  /**
-   * Retrieves a list of databases already available on the user's backend.
-   */
-  public getDatabases(init: boolean = false) {
-    this.isLoading = true;
-    this.sqlService.getDatabaseMetaInfo(
-      this.selectedDbType,
-      this.selectedConnectionString).subscribe({
-        next: (res: Databases) => {
-          this.databases = res.databases || [];
-          if (this.databases.length > 0) {
-            if (!init) {
-              this.selectedDatabase = this.databases[0].name;
-            }
-            this.databaseLoaded();
-          }
-          this.isLoading = false;
-        },
-        error: (error: any) => this.generalService.showFeedback(error?.error?.message, 'errorMessage', 'Ok', 5000)
-      });
-  }
-
-  protected abstract databaseLoaded(): void;
-
   /*
-   * Returns friendly name for database type.
+   * Private helper methods.
    */
+
   private getDatabaseName(type: string) {
     switch (type) {
       case 'mssql': return 'SQL Server';
