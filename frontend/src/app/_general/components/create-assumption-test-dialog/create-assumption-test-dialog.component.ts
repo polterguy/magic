de51bceebab10c81764cb1cb700c6fd07e6584a7
Thomas Hansen <thomas@gaiasoul.com>
2023-01-01 08:37:35 +0200

    Formatting code

--- frontend/src/app/_general/components/create-assumption-test-dialog/create-assumption-test-dialog.component.ts
+++ frontend/src/app/_general/components/create-assumption-test-dialog/create-assumption-test-dialog.component.ts
@@ -47,33 +47,22 @@ export class CreateAssumptionTestDialogComponent implements OnInit {
 
   private files: string[] = [];
 
-  /**
-   * Test model to use when saving test.
-   */
   data: TestModel = {
     filename: '',
     description: '',
     matchResponse: false,
   };
 
-  public CommonRegEx = CommonRegEx;
-  public CommonErrorMessages = CommonErrorMessages;
+  CommonRegEx = CommonRegEx;
+  CommonErrorMessages = CommonErrorMessages;
 
-  /**
-   * Creates an instance of your component.
-   *
-   * @param generalService Needed to show user feedback
-   * @param assumptionService Needed to be able tolist and execute assumptions
-   */
   constructor(
     private dialogRef: MatDialogRef<CreateAssumptionTestDialogComponent>,
     private generalService: GeneralService,
     private assumptionService: AssumptionService) { }
 
-  /**
-   * Implementation of OnInit.
-   */
   ngOnInit() {
+
     this.assumptionService.list().subscribe({
       next: (files: string[]) => {
         this.files = files.filter(x => x.endsWith('.hl'));
@@ -82,35 +71,24 @@ export class CreateAssumptionTestDialogComponent implements OnInit {
     });
   }
 
-  /**
-   * Returns only the filename parts from the given full path and filename.
-   *
-   * @param path Complete path of file
-   */
   getFilename(path: string) {
     const result = path.substring(path.lastIndexOf('/') + 1);
     return result.substring(0, result.lastIndexOf('.'));
   }
 
-  /**
-   * Returns filtered files according to what user has typed.
-   */
   getFiltered() {
     return this.files.filter((idx: string) => {
       return this.getFilename(idx).indexOf(this.data.filename) !== -1;
     });
   }
 
-  /**
-   * Returns true if filename is a valid filename for snippet.
-   */
   filenameValid() {
     return this.CommonRegEx.appNamesWithDot.test(this.data.filename);
   }
 
-  public create() {
+  create() {
     if (!this.filenameValid()) {
-      this.generalService.showFeedback('Test name is not valid.', 'errorMessage');
+      this.generalService.showFeedback('Test name is not valid', 'errorMessage');
       return;
     }
     this.dialogRef.close(this.data)
