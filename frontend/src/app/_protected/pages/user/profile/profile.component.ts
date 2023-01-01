e314e6aa954613cb030f258359883e59c1d5cbcf
thomas@ainiro.io <th@aista.com>
2023-01-01 18:35:42 +0200

    Validators

--- frontend/src/app/_protected/pages/user/profile/profile.component.ts
+++ frontend/src/app/_protected/pages/user/profile/profile.component.ts
@@ -22,17 +22,13 @@ import { UserService } from '../../manage/user-and-roles/_services/user.service'
 })
 export class ProfileComponent implements OnInit {
 
-  // Selected theme.
-  public selectedTheme: string = null;
-
-  public user: any = {};
-
-  public password: string = '';
-  public confirmPassword: string = '';
-  public showPassword: boolean = false;
-
-  public CommonRegEx = CommonRegEx;
-  public CommonErrorMessages = CommonErrorMessages;
+  selectedTheme: string = null;
+  user: any = {};
+  password: string = '';
+  confirmPassword: string = '';
+  showPassword: boolean = false;
+  CommonRegEx = CommonRegEx;
+  CommonErrorMessages = CommonErrorMessages;
 
   constructor(
     private router: Router,
@@ -44,6 +40,64 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.getUsername();
   }
+
+  save() {
+
+    if (this.user.name === '') {
+      this.generalService.showFeedback('Please provide a full name', 'errorMessage');
+      return;
+    }
+    const data: any = {
+      user: this.user.username,
+      type: 'name',
+      value: this.user.name
+    }
+
+    this.userService.editExtra(data).subscribe({
+      next: (res: any) => {
+        this.generalService.showFeedback('Details saved successfully', 'successMessage');
+      },
+      error: (error: any) => {
+        this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage');
+      }
+    })
+  }
+
+  savePassword() {
+
+    if (this.password === '') {
+      this.generalService.showFeedback('Please provide a new password', 'errorMessage');
+      return;
+    }
+
+    if (this.password.length < 12) {
+      this.generalService.showFeedback('Not a valid password', 'errorMessage');
+      return;
+    }
+
+    if (this.confirmPassword === '') {
+      this.generalService.showFeedback('Please confirm the new password', 'errorMessage');
+      return;
+    }
+
+    if (this.confirmPassword !== this.password) {
+      this.generalService.showFeedback('Passwords do not match', 'errorMessage');
+      return;
+    }
+
+    this.backendService.changePassword(this.password).subscribe({
+      next: () => {
+        this.generalService.showFeedback('Please sign in with your new password', 'successMessage', 'Ok', 5000);
+        this.backendService.logout(false);
+        this.router.navigate(['/authentication']);
+      },
+      error: (error: any) => this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage')
+    });
+  }
+
+  /*
+   * Private helper methods.
+   */
 
   private getUsername() {
     this.user.username = this.backendService.active.token['_username'];
@@ -68,56 +122,5 @@ export class ProfileComponent implements OnInit {
       },
       error: (error: any) => { }
     })
-  }
-
-  public save() {
-    if (this.user.name === '') {
-      this.generalService.showFeedback('Please provide a full name', 'errorMessage');
-      return;
-    }
-    const data: any = {
-      user: this.user.username,
-      type: 'name',
-      value: this.user.name
-    }
-    this.userService.editExtra(data).subscribe({
-      next: (res: any) => {
-        this.generalService.showFeedback('Details saved successfully', 'successMessage');
-      },
-      error: (error: any) => {
-        this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage');
-      }
-    })
-  }
-
-  /**
-   * Invoked when user wants to change his password.
-   */
-  public savePassword() {
-    if (this.password === '') {
-      this.generalService.showFeedback('Please provide a new password', 'errorMessage');
-      return;
-    }
-    if (this.password.length < 12) {
-      this.generalService.showFeedback('Not a valid password', 'errorMessage');
-      return;
-    }
-    if (this.confirmPassword === '') {
-      this.generalService.showFeedback('Please confirm the new password', 'errorMessage');
-      return;
-    }
-    if (this.confirmPassword !== this.password) {
-      this.generalService.showFeedback('Passwords do not match', 'errorMessage');
-      return;
-    }
-
-    this.backendService.changePassword(this.password).subscribe({
-      next: () => {
-        this.generalService.showFeedback('Please sign in with your new password', 'successMessage', 'Ok', 5000);
-        this.backendService.logout(false);
-        this.router.navigate(['/authentication']);
-      },
-      error: (error: any) => this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage')
-    });
   }
 }
