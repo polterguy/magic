242c1fafa3c732c69887575465bb9455d9c0f134
Thomas Hansen <th@aista.com>
2023-01-01 16:58:56 +0200

    Code formatting

--- frontend/src/app/_protected/pages/manage/sockets/socket-list/socket-list.component.ts
+++ frontend/src/app/_protected/pages/manage/sockets/socket-list/socket-list.component.ts
@@ -6,7 +6,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { GeneralService } from 'src/app/_general/services/general.service';
-import { Message } from 'src/app/_protected/models/common/message.model';
 import { BackendService } from 'src/app/_general/services/backend.service';
 import { SocketUser } from '../../endpoints/_models/socket-user.model';
 import { MessageWrapper, PublishDialogComponent } from '../components/publish-dialog/publish-dialog.component';
@@ -34,15 +33,12 @@ export class SocketListComponent {
     public backendService: BackendService,
     private generalService: GeneralService) { }
 
-  public subscribeToPublishedMessage(itemName: string) {
+  subscribeToPublishedMessage(itemName: string) {
     this.subscribe.emit(itemName);
   }
 
-  /**
-   * Invoked when user wants to generically post a message, to for instance
-   * one or more groups, or one or more roles, instead of a single connection.
-   */
-  public publish(item: any) {
+  publish(item: any) {
+
     this.dialog.open(PublishDialogComponent, {
       width: '550px',
       panelClass: 'light',
@@ -57,6 +53,7 @@ export class SocketListComponent {
       }
     }).afterClosed().subscribe((data: MessageWrapper) => {
       if (data) {
+
         this.socketService.publishMessage(data.message, data.client, data.roles, data.groups).subscribe({
           next: () => this.generalService.showFeedback('Message was successfully published', 'successMessage'),
           error: (error: any) => this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage')
