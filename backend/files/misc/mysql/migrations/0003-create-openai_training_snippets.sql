c9e28e3cf5edb57cdc87eb8129cdc0105c5d88b7
thomas@ainiro.io <th@aista.com>
2023-01-08 17:13:05 +0200

    Migration scripts for all database types

---
+++ backend/files/misc/mysql/migrations/0003-create-openai_training_snippets.sql
@@ -0,0 +1,10 @@
+create table openai_training_snippets(
+  id int(11) not null auto_increment,
+  created timestamp not null default current_timestamp,
+  type varchar(256) not null default 'hyperlambda',
+  pushed smallint not null default 0,
+  uri varchar(1024) null,
+  prompt text not null,
+  completion text not null,
+  primary key (id)
+);
---
+++ backend/files/misc/pgsql/migrations/0003-create-openai_training_snippets.sql
@@ -0,0 +1,9 @@
+create table openai_training_snippets(
+  "id" serial not null,
+  "created" timestamptz not null default now(),
+  "type" varchar(256) not null default 'hyperlambda',
+  "pushed" smallint not null default 0,
+  "uri" varchar(1024) null,
+  "prompt" text not null,
+  "completion" text not null
+);
