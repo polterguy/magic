dafa8001cb5f470347d7435a2d8d81e28c6e9551
Thomas Hansen <th@aista.com>
2023-01-08 15:47:13 +0200

    Better UI

--- backend/files/misc/sqlite/migrations/0003-create-openai_training_snippets.sql
+++ backend/files/misc/sqlite/migrations/0003-create-openai_training_snippets.sql
@@ -2,6 +2,7 @@ create table openai_training_snippets(
   "openai_training_snippet_id" integer not null primary key autoincrement,
   "created" timestamp not null default current_timestamp,
   "type" text not null default 'hyperlambda',
+  "pushed" int not null default 0,
   "filename" text null,
   "description" text not null,
   "content" text not null
