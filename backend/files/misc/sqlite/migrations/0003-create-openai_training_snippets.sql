9658e4ceaeabb9b7d4cbd367586cfa43b5053945
Thomas Hansen <th@aista.com>
2023-01-08 16:39:33 +0200

    Simplifications

--- backend/files/misc/sqlite/migrations/0003-create-openai_training_snippets.sql
+++ backend/files/misc/sqlite/migrations/0003-create-openai_training_snippets.sql
@@ -1,9 +1,9 @@
 create table openai_training_snippets(
-  "openai_training_snippet_id" integer not null primary key autoincrement,
+  "id" integer not null primary key autoincrement,
   "created" timestamp not null default current_timestamp,
   "type" text not null default 'hyperlambda',
   "pushed" int not null default 0,
-  "filename" text null,
-  "description" text not null,
-  "content" text not null
+  "uri" text null,
+  "prompt" text not null,
+  "completion" text not null
 );
