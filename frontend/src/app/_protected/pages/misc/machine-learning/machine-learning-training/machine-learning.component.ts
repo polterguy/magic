e3d48237e63b00273d2f1f7395e601fef8243168
thomas@ainiro.io <th@aista.com>
2023-01-09 16:13:20 +0200

    Improvements

--- frontend/src/app/_protected/pages/misc/machine-learning/machine-learning-training/machine-learning.component.ts
+++ frontend/src/app/_protected/pages/misc/machine-learning/machine-learning-training/machine-learning.component.ts
@@ -38,6 +38,8 @@ export class MachineLearningTrainingComponent implements OnInit {
   selectedModel: any = null;
   models: any[] = [];
   types: string[] = null;
+  max_tokens: number = 2000;
+  temperature: number = 0.1;
 
   constructor(
     private generalService: GeneralService,
@@ -48,6 +50,7 @@ export class MachineLearningTrainingComponent implements OnInit {
 
     this.isLoadingGrid = true;
     this.isLoadingConfig = true;
+    this.generalService.showLoading();
 
     this.machineLearningTrainingService.types().subscribe({
       next: (types: any[]) => {
@@ -56,7 +59,9 @@ export class MachineLearningTrainingComponent implements OnInit {
         this.getItems();
       },
       error: (error: any) => {
+
         this.generalService.showFeedback(error, 'errorMessage', 'Ok');
+        this.generalService.hideLoading();
       }
     });
 
@@ -67,7 +72,36 @@ export class MachineLearningTrainingComponent implements OnInit {
         this.getModels();
       },
       error: (error: any) => {
+
         this.generalService.showFeedback(error, 'errorMessage', 'Ok');
+        this.generalService.hideLoading();
+      }
+    });
+  }
+
+  apiKeyChanged() {
+
+    this.generalService.showLoading();
+    this.openAIService.configure(
+      this.key,
+      this.selectedModel.name,
+      this.max_tokens,
+      this.temperature).subscribe({
+      next: () => {
+
+        this.generalService.showFeedback('Your OpenAI API key was saved to your configuration', 'successMessage');
+
+        // To allow for configuration to kick in we need to sleep for 1 second
+        setTimeout(() => {
+
+          this.getTrainingStatus();
+
+        }, 1000);
+      },
+      error: (error: any) => {
+
+        this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage');
+        this.generalService.hideLoading();
       }
     });
   }
@@ -77,11 +111,13 @@ export class MachineLearningTrainingComponent implements OnInit {
   }
 
   page(event: PageEvent) {
+
     this.filter.offset = event.pageIndex * event.pageSize;
     this.getItems(false);
   }
 
   filterList(event: { searchKey: string, type?: string }) {
+
     this.filter = {
       limit: this.filter.limit,
       offset: 0,
@@ -99,7 +135,7 @@ export class MachineLearningTrainingComponent implements OnInit {
    * Private helper methods.
    */
 
-  getItems(count: boolean = true) {
+  private getItems(count: boolean = true) {
 
     this.machineLearningTrainingService.list(this.filter).subscribe({
       next: (result: any[]) => {
@@ -121,8 +157,10 @@ export class MachineLearningTrainingComponent implements OnInit {
               this.isLoadingGrid = false;
             },
             error: (error: any) => {
+
               this.generalService.showFeedback(error, 'errorMessage', 'Ok');
               this.isLoadingGrid = false;
+              this.generalService.hideLoading();
             }
           });
         }
@@ -131,44 +169,82 @@ export class MachineLearningTrainingComponent implements OnInit {
 
         this.generalService.showFeedback(error, 'errorMessage', 'Ok');
         this.isLoadingGrid = false;
+        this.generalService.hideLoading();
       }
     });
   }
 
   private getModels() {
+
     this.openAIService.base_models().subscribe({
       next: (result: any[]) => {
 
         this.models = result;
 
         if (!this.key) {
+
+          this.generalService.hideLoading();
+          this.selectedModel = this.models[0];
           this.isLoadingConfig = false;
           return;
         }
 
-        this.openAIService.get_training_status().subscribe({
-          next: (result: any[]) => {
-
-            this.models = result.map(x => {
-              return {
-                name: x.fine_tuned_model,
-                description: 'Fine tuned custom model',
-              };
-            }).concat(this.models);
-
-            this.isLoadingConfig = false;
-          },
-          error: (error: any) => {
-
-            this.generalService.showFeedback(error, 'errorMessage', 'Ok');
-            this.isLoadingConfig = false;
-          }
-        });
+        this.getTrainingStatus();
       },
       error: (error: any) => {
 
         this.generalService.showFeedback(error, 'errorMessage', 'Ok');
         this.isLoadingConfig = false;
+        this.generalService.hideLoading();
+      }
+    });
+  }
+
+  private getTrainingStatus() {
+
+    this.openAIService.get_training_status().subscribe({
+      next: (result: any[]) => {
+
+        this.generalService.hideLoading();
+        result = result || [];
+        if (result.length > 0) {
+
+          // Verifying we've got trained models.
+          result.sort((lhs, rhs) => {
+            if (lhs.created > rhs.created) {
+              return -1;
+            }
+            if (lhs.created < rhs.created) {
+              return 1;
+            }
+            return 0;
+          });
+
+          // Filtering in only models that succeeded their training.
+          const succeeded = result.filter(x => x.status === 'succeeded');
+
+          // Checking if there are models being trained.
+          if (result.filter(x => x.status === 'running').length > 0) {
+            this.generalService.showFeedback('You have models currently in training');
+          }
+
+          if (succeeded.length > 0) {
+            this.models = succeeded
+              .map(x => {
+                return {
+                  name: x.fine_tuned_model,
+                  description: 'Fine tuned model',
+                }
+              })
+              .concat(this.models);
+            this.selectedModel = this.models[0];
+          }
+        }
+      },
+      error: (error: any) => {
+
+        this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage');
+        this.generalService.hideLoading();
       }
     });
   }
