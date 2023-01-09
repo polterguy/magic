da2d5ae7edd25570e2e8bf1c76e2e653bed7ad1c
Thomas Hansen <th@aista.com>
2023-01-09 14:13:14 +0200

    Getting rid of custom SQL snippet filtering

--- frontend/src/app/_protected/pages/create/sql-studio/components/sql-view/components/load-sql-snippet-dialog/load-sql-snippet-dialog.component.ts
+++ frontend/src/app/_protected/pages/create/sql-studio/components/sql-view/components/load-sql-snippet-dialog/load-sql-snippet-dialog.component.ts
@@ -17,15 +17,8 @@ import { SqlService } from '../../../../../../../../_general/services/sql.servic
 })
 export class SqlSnippetDialogComponent implements OnInit {
 
-  /**
-   * Snippet files as returned from backend.
-   */
   files: string[] = [];
-
-  /**
-   * Filter for filtering files to display.
-   */
-  filter: string = '';
+  filter: string = null;
 
   constructor(
     private dialogRef: MatDialogRef<SqlSnippetDialogComponent>,
@@ -34,23 +27,24 @@ export class SqlSnippetDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: string) { }
 
   ngOnInit() {
+
     this.sqlService.listSnippets(this.data).subscribe((files: string[]) => {
       this.files = files.filter(x => x.endsWith('.sql'));
     }, (error: any) => this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage'));
   }
 
-  /**
-   * Returns only the filename parts from the given full path and filename.
-   */
+  filterList(event: { searchKey: string }) {
+    this.filter = event.searchKey;
+  }
+
   getFilename(path: string) {
+
     const result = path.substring(path.lastIndexOf('/') + 1);
     return result.substring(0, result.lastIndexOf('.'));
   }
 
-  /**
-   * Invoked when user selects a file.
-   */
   select(filename: string) {
+
     this.dialogRef.close(this.getFilename(filename));
   }
 }
