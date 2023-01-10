22fba96f26c61811d79b096060eeb1928ead063c
Thomas Hansen <th@aista.com>
2023-01-10 19:47:24 +0200

    Update 0003-create-openai_training_snippets.sql

--- backend/files/misc/mysql/migrations/0003-create-openai_training_snippets.sql
+++ backend/files/misc/mysql/migrations/0003-create-openai_training_snippets.sql
@@ -1,10 +1,10 @@
 
 create table ml_types(
-  "type" varchar(256) not null,
-  "model" varchar(1024) not null,
-  "max_tokens" int not null,
-  "temperature" decimal(3,2) not null,
-  primary key ("type")
+  type varchar(256) not null,
+  model varchar(1024) not null,
+  max_tokens int not null,
+  temperature decimal(3,2) not null,
+  primary key (type)
 );
 
 insert into ml_types(type, model, max_tokens, temperature) values ('hl', 'curie', 2000, 0.1);
@@ -12,11 +12,11 @@ insert into ml_types(type, model, max_tokens, temperature) values ('hl', 'curie'
 create table ml_training_snippets(
   id int(11) not null auto_increment,
   created timestamp not null default current_timestamp,
-  "type" varchar(256) not null default 'hl',
+  type varchar(256) not null default 'hl',
   pushed int not null default 0,
   uri varchar(1024) null,
   prompt text not null,
   completion text not null,
   primary key (id),
-  constraint ml_training_snippets_type_fky foreign key ("type") references ml_types ("type") on delete cascade
+  constraint ml_training_snippets_type_fky foreign key (type) references ml_types (type) on delete cascade
 );
