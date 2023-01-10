62e90913645c41bdf6a6a4b6973a26e0bad2fbde
thomas@ainiro.io <th@aista.com>
2023-01-10 16:55:14 +0200

    [console.log] slot for logging to console

---
+++ backend/slots/ConsoleLog.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Copyright (c) Aista Ltd, 2021 - 2023 info@aista.com, all rights reserved.
+ */
+
+using System;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.backend.slots
+{
+    /// <summary>
+    /// [console.log] for writing entries to the console.
+    /// </summary>
+    [Slot(Name = "console.log")]
+    public class ConsoleLog : ISlot
+    {
+        /// <summary>
+        /// Implementation of signal
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal</param>
+        /// <param name="input">Parameters passed from signaler</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            Console.WriteLine(input.GetEx<string>());
+        }
+    }
+}
