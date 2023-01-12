c2f937204148577f1a17879cf4271f9f4105212d
Thomas Hansen <th@aista.com>
2023-01-12 11:54:13 +0200

    Started working on types tab

---
+++ frontend/src/app/_protected/pages/misc/machine-learning/components/machine-learning-types/machine-learning-types.component.ts
@@ -0,0 +1,24 @@
+
+/*
+ * Copyright (c) Aista Ltd, 2021 - 2023 info@aista.com, all rights reserved.
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * Helper component to manage machine learning types, different models, and configurations
+ * for your types.
+ */
+@Component({
+  selector: 'app-machine-learning-types',
+  templateUrl: './machine-learning-types.component.html',
+  styleUrls: ['./machine-learning-types.component.scss']
+})
+export class MachineLearningTypesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
