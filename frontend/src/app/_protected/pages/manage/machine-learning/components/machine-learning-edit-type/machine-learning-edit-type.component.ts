38d76e2db7cfd97a1094c418fbbdec8a23dd2683
thomas@ainiro.io <th@aista.com>
2023-01-13 12:30:21 +0200

    More options for configuration

--- frontend/src/app/_protected/pages/manage/machine-learning/components/machine-learning-edit-type/machine-learning-edit-type.component.ts
+++ frontend/src/app/_protected/pages/manage/machine-learning/components/machine-learning-edit-type/machine-learning-edit-type.component.ts
@@ -7,6 +7,8 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { GeneralService } from 'src/app/_general/services/general.service';
 import { OpenAIModel, OpenAIService } from 'src/app/_general/services/openai.service';
+import { Role } from '../../../user-and-roles/_models/role.model';
+import { RoleService } from '../../../user-and-roles/_services/role.service';
 
 /**
  * Helper component to create or edit existing Machine Learning type.
@@ -21,13 +23,17 @@ export class MachineLearningEditTypeComponent implements OnInit {
   type: string = null;
   temperature: string = null;
   max_tokens: string = null;
+  recaptcha: 0;
+  auth: string = null;
   model: OpenAIModel = null;
   models: OpenAIModel[] = [];
+  roles: Role[] = [];
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private generalService: GeneralService,
     private openAIService: OpenAIService,
+    private roleService: RoleService,
     private dialogRef: MatDialogRef<MachineLearningEditTypeComponent>,) { }
 
   ngOnInit() {
@@ -35,19 +41,32 @@ export class MachineLearningEditTypeComponent implements OnInit {
     this.type = this.data?.type;
     this.max_tokens = this.data?.max_tokens ?? 2000;
     this.temperature = this.data?.temperature ?? 0.5;
+    this.recaptcha = this.data?.recaptcha ?? 0.3;
 
     this.generalService.showLoading();
 
-    this.openAIService.models().subscribe({
-      next: (models: OpenAIModel[]) => {
+    this.roleService.list('?limit=-1').subscribe({
+      next: (roles: Role[]) => {
 
-        this.models = models;
-        if (this.data?.model) {
-          this.model = this.models.filter(x => x.id === this.data.model)[0];
-        } else {
-          this.model = this.models.filter(x => x.id === 'curie')[0];
-        }
-        this.generalService.hideLoading();
+        this.roles = roles;
+
+        this.openAIService.models().subscribe({
+          next: (models: OpenAIModel[]) => {
+    
+            this.models = models;
+            if (this.data?.model) {
+              this.model = this.models.filter(x => x.id === this.data.model)[0];
+            } else {
+              this.model = this.models.filter(x => x.id === 'curie')[0];
+            }
+            this.generalService.hideLoading();
+          },
+          error: () => {
+    
+            this.generalService.showFeedback('Something went wrong as we tried to create your snippet', 'errorMessage');
+            this.generalService.hideLoading();
+          }
+        });
       },
       error: () => {
 
