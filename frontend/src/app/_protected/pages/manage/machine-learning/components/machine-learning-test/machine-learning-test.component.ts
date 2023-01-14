99e47245086263c29106661869c13ed533ae92de
Thomas Hansen <th@aista.com>
2023-01-14 17:52:24 +0200

    Hyperlambda view during test

--- frontend/src/app/_protected/pages/manage/machine-learning/components/machine-learning-test/machine-learning-test.component.ts
+++ frontend/src/app/_protected/pages/manage/machine-learning/components/machine-learning-test/machine-learning-test.component.ts
@@ -3,11 +3,12 @@
  * Copyright (c) Aista Ltd, 2021 - 2023 info@aista.com, all rights reserved.
  */
 
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Response } from 'src/app/models/response.model';
 import { GeneralService } from 'src/app/_general/services/general.service';
 import { OpenAIService } from 'src/app/_general/services/openai.service';
+import { CodemirrorActionsService } from 'src/app/_protected/pages/create/hyper-ide/services/codemirror-actions.service';
 
 /**
  * Helper component to test model.
@@ -17,16 +18,37 @@ import { OpenAIService } from 'src/app/_general/services/openai.service';
   templateUrl: './machine-learning-test.component.html',
   styleUrls: ['./machine-learning-test.component.scss']
 })
-export class MachineLearningTestComponent {
+export class MachineLearningTestComponent implements OnInit {
 
   prompt: string = '';
   completion: string = 'Result ...';
   isLoading: boolean = false;
+  hlReady: boolean = false;
+  hlModel: HlModel;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private generalService: GeneralService,
+    private codemirrorActionsService: CodemirrorActionsService,
     private openAIService: OpenAIService,) { }
+
+  ngOnInit() {
+
+    // Checking if training snippet is Hyperlambda, at which point we display CodeMirror editor for content.
+    if (this.data.type === 'hl') {
+      const res = this.codemirrorActionsService.getActions(null, 'hl');
+      res.autofocus = false;
+      this.hlModel = {
+        hyperlambda: this.data.completion,
+        options: res,
+      }
+      setTimeout(() => {
+        this.hlReady = true;
+      }, 500);
+    } else {
+      this.hlReady = false;
+    }
+  }
 
   submit() {
 
@@ -38,6 +60,12 @@ export class MachineLearningTestComponent {
         this.generalService.hideLoading();
         this.isLoading = false;
         this.completion = result.result;
+        this.hlModel.hyperlambda = result.result;
+        setTimeout(() => {
+          const el = <any>document.getElementsByName('prompt')[0];
+          el.focus();
+          el.select();
+        }, 100);
       },
       error: () => {
 
@@ -47,4 +75,9 @@ export class MachineLearningTestComponent {
       }
     });
   }
+}
+
+interface HlModel {
+  hyperlambda: string,
+  options: any
 }
