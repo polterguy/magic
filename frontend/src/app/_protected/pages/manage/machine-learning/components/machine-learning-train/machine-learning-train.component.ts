09b3d49840e2ef7794229e06469191cf9ec8de10
thomas@ainiro.io <th@aista.com>
2023-01-15 08:10:05 +0200

    Started work on better training

--- frontend/src/app/_protected/pages/manage/machine-learning/components/machine-learning-train/machine-learning-train.component.ts
+++ frontend/src/app/_protected/pages/manage/machine-learning/components/machine-learning-train/machine-learning-train.component.ts
@@ -7,6 +7,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { GeneralService } from 'src/app/_general/services/general.service';
 import { MachineLearningTrainingService } from 'src/app/_general/services/machine-learning-training.service';
+import { OpenAIModel, OpenAIService } from 'src/app/_general/services/openai.service';
 import { Count } from 'src/app/_protected/models/common/count.model';
 
 /**
@@ -22,10 +23,15 @@ export class MachineLearningTrainComponent implements OnInit {
   count: number = 0;
   epochs: number = 4;
   batch_size: number = 0;
+  learning_rate_multiplier: number = 0.02;
+  prompt_loss_weight: number = 0.01;
+  model: OpenAIModel = null;
+  models: OpenAIModel[] = [];
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private generalService: GeneralService,
+    private openAIService: OpenAIService,
     private machineLearningTrainingService: MachineLearningTrainingService,
     private dialogRef: MatDialogRef<MachineLearningTrainComponent>,) { }
 
@@ -38,9 +44,32 @@ export class MachineLearningTrainComponent implements OnInit {
     }).subscribe({
       next: (result: Count) => {
 
-        this.generalService.hideLoading();
         this.count = result.count;
+
+        if (this.count === 0) {
+          this.generalService.hideLoading();
+          return;
+        }
+
         this.batch_size = Math.round(Math.min(Math.max(this.count / 500, 1), 256));
+
+        this.openAIService.models().subscribe({
+          next: (models: OpenAIModel[]) => {
+    
+            this.models = models;
+            if (this.data?.model) {
+              this.model = this.models.filter(x => x.id === this.data.model)[0];
+            } else {
+              this.model = this.models.filter(x => x.id === 'curie')[0];
+            }
+            this.generalService.hideLoading();
+          },
+          error: () => {
+    
+            this.generalService.showFeedback('Something went wrong as we tried to create your snippet', 'errorMessage');
+            this.generalService.hideLoading();
+          }
+        });
       },
       error: () => {
 
