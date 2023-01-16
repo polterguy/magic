785284bab8fd323a3a12eff526f5180c16fc0c01
thomas@ainiro.io <th@aista.com>
2023-01-16 15:10:11 +0200

    Better scraping

--- frontend/src/app/_protected/pages/manage/machine-learning/components/machine-learning-types/machine-learning-types.component.ts
+++ frontend/src/app/_protected/pages/manage/machine-learning/components/machine-learning-types/machine-learning-types.component.ts
@@ -45,7 +45,6 @@ export class MachineLearningTypesComponent implements OnInit {
     'action',
   ];
   types: any[] = null;
-  seenUrls: string[] = [];
 
   constructor(
     private dialog: MatDialog,
@@ -140,9 +139,18 @@ export class MachineLearningTypesComponent implements OnInit {
         if (result?.crawl) {
 
           this.generalService.showLoading();
-          this.importing = true;
-          this.seenUrls = [];
-          this.crawl([result.crawl], el.type);
+          this.openAIService.importUrl(result.crawl, el.type).subscribe({
+            next: () => {
+
+              this.generalService.hideLoading();
+              this.generalService.showFeedback('Crawling started, you will be notified when it is finished', 'successMessage');
+            },
+            error: () => {
+
+              this.generalService.hideLoading();
+              this.generalService.showFeedback('Something went wrong as we tried to start training', 'errorMessage');
+            }
+          });
         }
       });
   }
@@ -319,35 +327,5 @@ export class MachineLearningTypesComponent implements OnInit {
         this.generalService.hideLoading();
       }
     });
-  }
-
-  private crawl(urls: string[], type: string) {
-
-    const upcoming = urls.filter(x => !this.seenUrls.includes(x));
-
-    const observables = upcoming.map((url: string) => {
-      return this.openAIService.importUrl(url, type);
-    });
-    this.seenUrls.push(...upcoming);
-
-    from(observables)
-      .pipe(bufferCount(4),
-        concatMap((result: any) => forkJoin(result))).subscribe({
-          next: (results: any[]) => {
-            const toBe = [];
-            for (const idx of results) {
-              if (idx.urls) {
-                toBe.push(...idx.urls.filter(x => !toBe.includes(x)));
-              }
-            }
-            this.crawl(toBe, type);
-          },
-          error: (error: any) => {
-
-            console.log(error);
-          },
-          complete: () => {
-          },
-        });
   }
 }
