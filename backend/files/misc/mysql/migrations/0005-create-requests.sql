15f07a21374de981036c4ff5240d69377af00bec
Thomas Hansen <th@aista.com>
2023-01-23 14:33:20 +0200

    Changing to longtext

--- backend/files/misc/mysql/migrations/0005-create-requests.sql
+++ backend/files/misc/mysql/migrations/0005-create-requests.sql
@@ -4,7 +4,7 @@ create table ml_requests(
   created timestamp not null default current_timestamp,
   type varchar(256) not null,
   prompt text not null,
-  completion text not null,
+  completion longtext not null,
   finish_reason varchar(100) not null,
   primary key (id),
   constraint ml_requests_types_fky foreign key (type) references ml_types (type) on delete cascade
