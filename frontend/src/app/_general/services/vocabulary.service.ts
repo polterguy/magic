90d3538f10e71d13fcb9e01fe62b3967f446ed3c
Thomas Hansen <th@aista.com>
2023-02-05 07:51:19 +0200

    Cleanups

--- frontend/src/app/_general/services/vocabulary.service.ts
+++ frontend/src/app/_general/services/vocabulary.service.ts
@@ -6,7 +6,7 @@
 // Angular and system imports.
 import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { HttpService } from './http.service';
+import { HttpService } from 'src/app/_general/services/http.service';
 
 // Application specific imports.
 
@@ -20,20 +20,41 @@ export class VocabularyService {
 
   private _vocabulary: string[] = [];
 
+  /**
+   * Creates an instance of your service.
+   *
+   * @param httpService HTTP service to use for backend invocations
+   */
   constructor(private httpService: HttpService) { }
 
+  /**
+   * Returns all Hyperlambda keywords in context to caller.
+   */
+  get words() {
+
+    return this._vocabulary;
+  }
+
+  /**
+   * Returns server's Hyperlambda vocabulary to caller.
+   */
   vocabulary() {
+
     if (this._vocabulary.length > 0) {
       of(this._vocabulary);
     }
+
     return new Observable<string[]>((observer) => {
+
       this.httpService.get<string[]>('/magic/system/evaluator/vocabulary').subscribe({
         next: (vocabulary: string[]) => {
+
           this._vocabulary = vocabulary;
           observer.next(vocabulary);
           observer.complete();
         },
         error: (error: any) => {
+
           observer.error(error);
           observer.complete();
         }
