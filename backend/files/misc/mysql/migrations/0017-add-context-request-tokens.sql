09219c8867ee07e7a02f150bab5d5875dcd16533
Thomas Hansen <th@aista.com>
2023-03-20 15:23:27 +0200

    b

--- backend/files/misc/mysql/migrations/0017-add-context-request-tokens.sql
+++ backend/files/misc/mysql/migrations/0017-add-context-request-tokens.sql
@@ -1,4 +1,4 @@
 
-alter table ml_training_snippets add column max_context_tokens int;
-alter table ml_training_snippets add column max_request_tokens int;
+alter table ml_types add column max_context_tokens int;
+alter table ml_types add column max_request_tokens int;
 
