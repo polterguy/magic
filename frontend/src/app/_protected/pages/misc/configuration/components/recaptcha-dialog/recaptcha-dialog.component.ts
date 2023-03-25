8562bb21b69e6bbd92dba78e4847d7e0db50f795
thomas@ainiro.io <th@aista.com>
2023-03-25 08:32:29 +0200

    reCAPTCHA value validators

--- frontend/src/app/_protected/pages/misc/configuration/components/recaptcha-dialog/recaptcha-dialog.component.ts
+++ frontend/src/app/_protected/pages/misc/configuration/components/recaptcha-dialog/recaptcha-dialog.component.ts
@@ -6,6 +6,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { UntypedFormBuilder } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CommonRegEx } from 'src/app/_general/classes/common-regex';
+import { CommonErrorMessages } from 'src/app/_general/classes/common-error-messages';
+import { GeneralService } from 'src/app/_general/services/general.service';
 
 /**
  * Helper component to allow for configuring reCAPTCHA settings.
@@ -22,8 +25,12 @@ export class RecaptchaDialogComponent implements OnInit {
     secret: [''],
   });
 
+  CommonRegEx = CommonRegEx;
+  CommonErrorMessages = CommonErrorMessages;
+
   constructor(
     private formBuilder: UntypedFormBuilder,
+    private generalService: GeneralService,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<RecaptchaDialogComponent>) { }
 
@@ -38,6 +45,13 @@ export class RecaptchaDialogComponent implements OnInit {
   }
 
   submit() {
+
+    if (!this.CommonRegEx.recaptcha.test(this.captchaForm.value.key) ||
+      !this.CommonRegEx.recaptcha.test(this.captchaForm.value.secret)) {
+
+        this.generalService.showFeedback('Not valid values for reCAPTCHA', 'errorMessage');
+        return;
+    }
 
     const data = {
       key: this.captchaForm.value.key,
