4d6311449e2ff22118bc6a42a8c67b4a27959a49
Thomas Hansen <th@aista.com>
2023-04-13 15:11:05 +0300

    b

--- backend/files/system/openai/front.files/search/search.js
+++ backend/files/system/openai/front.files/search/search.js
@@ -4,7 +4,7 @@
  */
 (function() {
 
-fetch('https://docs.aista.com/assets/css/icofont.min.css')
+fetch('https://polterguy.github.io/assets/css/icofont.min.css')
   .then(res => {
     return res.text()
   })
