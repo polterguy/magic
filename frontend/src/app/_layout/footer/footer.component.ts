eabd6d8e67f76726accb48c69952df323923a3bc
thomas@ainiro.io <th@aista.com>
2023-04-24 17:21:59 +0300

    Adding link

--- frontend/src/app/_layout/footer/footer.component.ts
+++ frontend/src/app/_layout/footer/footer.component.ts
@@ -3,7 +3,7 @@
  * Copyright (c) Aista Ltd, 2021 - 2023 team@ainiro.io.
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PrivacyModalComponent } from 'src/app/_general/components/privacy-modal/privacy-modal.component';
 import { TermsModalComponent } from 'src/app/_general/components/terms-modal/terms-modal.component';
@@ -18,16 +18,9 @@ declare function aista_show_chat_window(): any;
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
-  currentYear: number = 0;
+export class FooterComponent {
 
   constructor(private dialog: MatDialog) { }
-
-  ngOnInit() {
-
-    this.currentYear = new Date().getFullYear();
-  }
 
   termsModal() {
 
