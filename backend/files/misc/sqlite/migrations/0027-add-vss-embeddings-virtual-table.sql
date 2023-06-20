968c8ed8a0d054fc4c5b39781d88a83f95dd4915
Thomas Hansen <th@aista.com>
2023-06-20 13:17:13 +0300

    Getting index sized correctly

--- backend/files/misc/sqlite/migrations/0027-add-vss-embeddings-virtual-table.sql
+++ backend/files/misc/sqlite/migrations/0027-add-vss-embeddings-virtual-table.sql
@@ -1,5 +1,5 @@
 
 create virtual table vss_ml_training_snippets using vss0(
-  embedding_vss(1536) factory="PCA384,Flat,IDMap2"
+  embedding_vss(384) factory="PCA4,Flat,IDMap2",
+  constraint vss_ml_training_snippets_ml_training_snippets_fky foreign key (rowid) references ml_training_snippets (id) on delete cascade
 );
-
