c17cc306cf0a71cdd9b247915a713babc702c4d9
Thomas Hansen <th@aista.com>
2023-06-20 13:28:33 +0300

    Update 0027-add-vss-embeddings-virtual-table.sql

--- backend/files/misc/sqlite/migrations/0027-add-vss-embeddings-virtual-table.sql
+++ backend/files/misc/sqlite/migrations/0027-add-vss-embeddings-virtual-table.sql
@@ -1,5 +1,5 @@
 
 create virtual table vss_ml_training_snippets using vss0(
-  embedding_vss(384) factory="PCA4,Flat,IDMap2",
+  embedding_vss(768) factory="PCA2,Flat,IDMap2",
   constraint vss_ml_training_snippets_ml_training_snippets_fky foreign key (rowid) references ml_training_snippets (id) on delete cascade
 );
