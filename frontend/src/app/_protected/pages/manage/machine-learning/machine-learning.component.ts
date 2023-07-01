4bb34fe8c16813f0e0afc0355a931ffc49b6ac84
thomas@ainiro.io <th@aista.com>
2023-07-01 11:12:01 +0300

    Making the machine learning component invisible if not SQLite database

--- frontend/src/app/_protected/pages/manage/machine-learning/machine-learning.component.ts
+++ frontend/src/app/_protected/pages/manage/machine-learning/machine-learning.component.ts
@@ -3,7 +3,9 @@
  * Copyright (c) Aista Ltd, and Thomas Hansen - For license inquiries you can contact thomas@ainiro.io.
  */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/app/_general/services/config.service';
+import { GeneralService } from 'src/app/_general/services/general.service';
 
 /**
  * Helper component to manage Machine Learning training snippets, and types.
@@ -13,7 +15,32 @@ import { Component } from '@angular/core';
   templateUrl: './machine-learning.component.html',
   styleUrls: ['./machine-learning.component.scss']
 })
-export class MachineLearningComponent {
+export class MachineLearningComponent implements OnInit {
 
   isConfigured: boolean = false;
+  sqlIte: boolean = false;
+
+  constructor(
+    private configService: ConfigService,
+    private generalService: GeneralService) { }
+
+  ngOnInit() {
+
+    // Checking if cloudlet is using SQLite as default database.
+    this.generalService.showLoading();
+    this.configService.getDatabases().subscribe({
+
+      next: (result: any) => {
+
+        this.sqlIte = result.default === 'sqlite';
+        this.generalService.hideLoading();
+      },
+
+      error: (error: any) => {
+
+        this.generalService.hideLoading();
+        this.generalService.showFeedback(error?.error?.message ?? error, 'errorMessage');
+      }
+    });
+  }
 }
