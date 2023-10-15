a1fdbf9e2d689e04c6694b9c1aa5045236bd0e3f
Thomas Hansen <th@aista.com>
2023-10-15 08:55:29 +0300

    Turning of CORS when fetching icofont.min.css

--- backend/files/system/openai/front.files/chat/default.js
+++ backend/files/system/openai/front.files/chat/default.js
@@ -34,7 +34,11 @@ let ainiroChatbotType = '[[type]]';
 // Downloading icofont, making sure we only download it once.
 if (!window.ainiroHasDownloadIcofont) {
   window.ainiroHasDownloadIcofont = true;
-  fetch('https://ainiro.io/assets/css/icofont.min.css')
+  fetch('https://ainiro.io/assets/css/icofont.min.css', {
+    headers: {
+      'mode': 'no-cors'
+    }
+  })
     .then(res => {
       return res.text()
     })
