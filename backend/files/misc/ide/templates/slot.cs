17fbc50e56fb673f935c9ad2ddc11177c77db8b2
thomas@ainiro.io <th@aista.com>
2023-11-12 08:23:57 +0200

    Update slot.cs

--- backend/files/misc/ide/templates/slot.cs
+++ backend/files/misc/ide/templates/slot.cs
@@ -12,10 +12,13 @@ using magic.node.contracts;
 public class Foo : ISlot
 {
     /*
-     * Notice, because the DI container is built during startup, we cannot
-     * use IoC on dynamically compiled code since we can't rebuild the IoC container.
+     * Notice, since the DI container is built during startup, we cannot
+     * use standard IoC and DI on dynamically compiled code since we can't rebuild
+     * the IoC container.
+     *
      * This implies we'll have to use the "service locator" pattern to resolve services
      * in dynamically compiled C# code.
+     *
      * It's a drag, but we have no choice really.
      */
     readonly IServiceProvider _services;
