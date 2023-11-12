1536721c5023264fba50d9acce6cb070e5b67cb1
Thomas Hansen <th@aista.com>
2023-11-12 08:22:19 +0200

    Update slot.cs

--- backend/files/misc/ide/templates/slot.cs
+++ backend/files/misc/ide/templates/slot.cs
@@ -1,3 +1,4 @@
+
 // Example C# code creating a Hyperlambda slot.
 using System;
 
@@ -10,6 +11,13 @@ using magic.node.contracts;
 [Slot(Name = "foo")]
 public class Foo : ISlot
 {
+    /*
+     * Notice, because the DI container is built during startup, we cannot
+     * use IoC on dynamically compiled code since we can't rebuild the IoC container.
+     * This implies we'll have to use the "service locator" pattern to resolve services
+     * in dynamically compiled C# code.
+     * It's a drag, but we have no choice really.
+     */
     readonly IServiceProvider _services;
 
     public Foo(IServiceProvider services)
