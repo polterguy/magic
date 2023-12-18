0d6e1543bac3f62196d23d2dd0dda01a47d7a533
Thomas Hansen <th@aista.com>
2023-12-18 15:19:00 +0200

    b

--- frontend/src/app/_protected/models/common/setup.model.ts
+++ frontend/src/app/_protected/models/common/setup.model.ts
@@ -5,9 +5,6 @@
 
 export interface SetupModel {
   password: string | null,
-  connectionString: string | null,
-  defaultTimeZone: string | null,
-  databaseType: string | null,
   name: string | null,
   email: string | null,
 }
