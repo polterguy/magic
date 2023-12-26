c0a050c787cf2714577f4b3bdd5924ba4150e7bd
thomas@ainiro.io <th@aista.com>
2023-12-26 09:10:06 +0200

    b

--- frontend/src/app/services/codemirror-actions.service.ts
+++ frontend/src/app/services/codemirror-actions.service.ts
@@ -29,92 +29,117 @@ export class CodemirrorActionsService {
     let options = [];
 
     if (path) {
+
       const extension = path.substring(path.lastIndexOf('.') + 1).toLowerCase();
       options = this.extensions.filter(x => x.extensions.indexOf(extension) !== -1);
+
     } else if (type) {
+
       options = this.extensions.filter(x => x.extensions.indexOf(type) !== -1);
+
     }
 
     if (options.length === 0) {
+
       return null;
+
     } else {
+
       const res = this.clone(options[0]);
       res.options.theme = this.themeService.codeTheme;
+
       if (res.options.extraKeys) {
+
         res.options.extraKeys['Alt-M'] = (cm: any) => {
           cm.setOption('fullScreen', !cm.getOption('fullScreen'));
         };
+
         res.options.extraKeys['Alt-S'] = () => {
           this.ngZone.run(() => {
             this.action.next('save')
           })
         };
+
         res.options.extraKeys['Alt-D'] = () => {
           this.ngZone.run(() => {
             this.action.next('deleteFile');
           })
         };
+
         res.options.extraKeys['Alt-F'] = () => {
           this.ngZone.run(() => {
             this.action.next('find');
           })
         };
+
         res.options.extraKeys['Alt-C'] = () => {
           this.ngZone.run(() => {
             this.action.next('close');
           })
         };
+
         res.options.extraKeys['Alt-R'] = () => {
           this.ngZone.run(() => {
             this.action.next('renameFile');
           })
         };
+
         res.options.extraKeys['Alt-L'] = () => {
           this.ngZone.run(() => {
             this.action.next('renameFolder');
           })
         };
+
         res.options.extraKeys['Alt-V'] = () => {
           this.ngZone.run(() => {
             this.action.next('insertSnippet');
           })
         };
+
         res.options.extraKeys['Alt-O'] = () => {
           this.ngZone.run(() => {
             this.action.next('macro');
           })
         };
+
         res.options.extraKeys['Alt-A'] = () => {
           this.ngZone.run(() => {
             this.action.next('newFile');
           })
         };
+
         res.options.extraKeys['Alt-B'] = () => {
           this.ngZone.run(() => {
             this.action.next('newFolder');
           })
         };
+
         res.options.extraKeys['Alt-X'] = () => {
           this.ngZone.run(() => {
             this.action.next('deleteFolder');
           })
         };
+
         res.options.extraKeys['Alt-P'] = () => {
           this.ngZone.run(() => {
             this.action.next('preview');
           })
         };
+
         res.options.extraKeys['F5'] = () => {
           this.ngZone.run(() => {
             this.action.next('execute');
           })
         };
+
         res.options.extraKeys['F1'] = () => {
           this.ngZone.run(() => {
             this.action.next('prompt');
           })
         };
+
         if (type === 'sql') {
+
           res.options.extraKeys['Ctrl-Space'] = (cm: any) => {
             this.ngZone.run(() => {
               cm.showHint({
@@ -123,6 +148,7 @@ export class CodemirrorActionsService {
               });
             })
           };
+          
         }
       }
       return res.options;
