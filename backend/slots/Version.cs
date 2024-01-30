27a93e64732067492d1668b43c45f4996769ccc2
Thomas Hansen <th@aista.com>
2024-01-30 09:58:42 +0200

    version

--- backend/slots/Version.cs
+++ backend/slots/Version.cs
@@ -20,7 +20,7 @@ namespace magic.backend.slots
         /// <param name="input">Parameters passed from signaler</param>
         public void Signal(ISignaler signaler, Node input)
         {
-            input.Value = "v17.2.1";
+            input.Value = "v17.3.0";
         }
     }
 }
