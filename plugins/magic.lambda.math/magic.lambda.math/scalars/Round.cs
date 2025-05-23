ee03517e3a40781ec3567cd69527107d8e05da46
AINIRO.IO <thomas@ainiro.io>
2025-05-23 14:37:40 +0300

    Add rounding scalar slots

---
+++ plugins/magic.lambda.math/magic.lambda.math/scalars/Round.cs
@@ -0,0 +1,31 @@
+/*
+ * Magic Cloud, copyright (c) 2023 Thomas Hansen. See the attached LICENSE file
+ * for details. For license inquiries you can send an email to thomas@ainiro.io
+ */
+
+using System;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda.math.scalars
+{
+    /// <summary>
+    /// [math.round] slot for rounding numbers.
+    /// </summary>
+    [Slot(Name = "math.round")]
+    public class Round : ISlot
+    {
+        /// <summary>
+        /// Implementation of slot.
+        /// </summary>
+        /// <param name="signaler">Signaler used to raise the signal.</param>
+        /// <param name="input">Arguments to slot.</param>
+        /// <returns>An awaitable task.</returns>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            dynamic original = input.GetEx<dynamic>();
+            input.Value = Math.Round(original);
+        }
+    }
+}
