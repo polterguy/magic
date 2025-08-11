600c29e2fd31fe9d0a4485bc1fc96c582ad6fac4
polterguy <thomas@ainiro.io>
2025-08-11 11:51:40 +0300

    Returning token count of system message in machine learning

--- frontend/src/app/components/protected/manage/machine-learning/machine-learning-types/machine-learning-types.component.ts
+++ frontend/src/app/components/protected/manage/machine-learning/machine-learning-types/machine-learning-types.component.ts
@@ -42,6 +42,7 @@ export class MachineLearningModelsComponent implements OnInit {
   isLoadingKey: boolean = false;
   displayedColumns: string[] = [
     'type',
+    'token_count',
     'model',
     'action',
   ];
