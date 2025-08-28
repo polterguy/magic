45d1379ecde18085ed46090259739a760df6f644
polterguy <thomas@ainiro.io>
2025-08-28 16:27:15 +0300

    Changing default embeddings model to text-embedding-3-small

--- backend/files/misc/sqlite/migrations/0012-add-vector-model-to-types.sql
+++ backend/files/misc/sqlite/migrations/0012-add-vector-model-to-types.sql
@@ -1,2 +1,2 @@
 
-alter table ml_types add column vector_model varchar(1024) not null default 'text-embedding-ada-002';
+alter table ml_types add column vector_model varchar(1024) not null default 'text-embedding-3-small';
