6c3b289d49608cafb828502d2b0e8e9350d46b60
polterguy <thomas@ainiro.io>
2025-09-23 07:41:45 +0300

    Removing legacy vector lib

--- plugins/magic.library/internals/SQLiteInitializer.cs
+++ plugins/magic.library/internals/SQLiteInitializer.cs
@@ -15,16 +15,9 @@ namespace magic.library.internals
     /*
      * Internal helper class to help wire up plugins for SQLite.
      */
-     [Slot(Name = "sqlite.upgrade")]
-    internal class SQLiteInitializer : ISlot, IInitializer
+    internal class SQLiteInitializer : IInitializer
     {
-        private static bool _legacy = true;
         private static readonly SemaphoreSlim _lock = new(1, 1);
-
-        public void Signal(ISignaler signaler, Node input)
-        {
-            _legacy = false;
-        }
 
         public async Task Initialize(SqliteConnection connection)
         {
@@ -40,26 +33,7 @@ namespace magic.library.internals
             await _lock.WaitAsync().ConfigureAwait(false);
             try
             {
-                if (_legacy)
-                {
-                    if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                    {
-                        if (RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
-                        {
-                            connection.LoadExtension("./sqlite-plugins/vector0-64");
-                            connection.LoadExtension("./sqlite-plugins/vss0-64");
-                        }
-                        else
-                        {
-                            connection.LoadExtension("./sqlite-plugins/vector0");
-                            connection.LoadExtension("./sqlite-plugins/vss0");
-                        }
-                    }
-                }
-                else
-                {
-                    connection.LoadExtension("./sqlite-plugins/vector");
-                }
+                connection.LoadExtension("./sqlite-plugins/vector");
             }
             finally
             {
